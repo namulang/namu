4d9ee616714f060940c24e593894d8018110e327
kniz <kniz@kniz.net>
2014-09-13 21:10:05 +0900

    NEModuleSet의 insert안에 argument 초기화와 script 등록 기능 이전

--- Projects/Core/Sources/Kernal/Sources/Modules/NEModuleSet/NEModuleSet.hpp
+++ Projects/Core/Sources/Kernal/Sources/Modules/NEModuleSet/NEModuleSet.hpp
@@ -7,5 +7,26 @@ namespace NE
 {
 	template
 	class NE_DLL NEArrayTemplate<NEModule*, true, NEModule*, NEType::NEMODULESET>;
-	typedef NEArrayTemplate<NEModule*, true, NEModule*, NEType::NEMODULESET> NEModuleSet;
+	
+	class NEModuleSet : public NEArrayTemplate<NEModule*, true, NEModule*, NEType::NEMODULESET>
+	{
+	public:
+		typedef NEModuleSet ThisClass;
+		typedef NEArrayTemplate<NEModule*, true, NEModule*, NEType::NEMODULESET> SuperClass;
+
+	public:
+		NEModuleSet();
+		NEModuleSet(type_count size);
+		NEModuleSet(const NEModuleSet& rhs);
+
+	public:
+		virtual ~NEModuleSet();
+
+	public:
+		virtual type_index insert(type_index index, const NEModule* const source);
+		virtual type_result setElement(type_index index, const NEModule* const source);
+
+	private:
+		void _updateModule(type_index index);
+	};
 }
