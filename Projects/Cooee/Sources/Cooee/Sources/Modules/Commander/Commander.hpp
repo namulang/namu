5954c42ee7c494f45f4935a97a8e61a038bcf740
kniz <kniz@kniz.net>
2014-09-18 20:07:17 +0900

    UPDATE: Cooee Commander의 별칭 검색 기능 확장

--- Projects/Cooee/Sources/Cooee/Sources/Modules/Commander/Commander.hpp
+++ Projects/Cooee/Sources/Cooee/Sources/Modules/Commander/Commander.hpp
@@ -5,14 +5,44 @@
 class Commander : public CommandSet
 {
 public:		
-	NEString command(const NEString& command_name) {
-		for(int n=0; n < getLength() ; n++) {
-			Command& itr = getElement(n);
+	Commander()
+		: CommandSet() 
+	{
+		_initializeCommands();
+	}
+	Commander(type_count size)
+		: CommandSet(size)
+	{
+		_initializeCommands();
+	}
+	Commander(const Commander& rhs)
+		: CommandSet()
+	{
+		_initializeCommands();
+	}
 
-			if(itr.name == command_name)
-				return itr.execute();
+	NEString command(const NEString& commandline) {
+
+		NEStringSet splited;
+		commandline.split(" ", splited);
+		if(commandline.getLength() <= 0) return "";
+		NEString command_name = commandline[0];
+		commandline.remove(0);
+
+		for(int n=0; n < getLength() ; n++) {
+			Command& command = getElement(n);
+			NEStringSet& names = command.names;
+
+			if(names.find(command_name))
+				return command.execute(commandline);
 		}
 
 		return command_name + "이란 명령어는 없다구요.\n오타? 아님 착각?";
+	}
+
+private:
+	void _initializeCommands()
+	{
+
 	}
 };
