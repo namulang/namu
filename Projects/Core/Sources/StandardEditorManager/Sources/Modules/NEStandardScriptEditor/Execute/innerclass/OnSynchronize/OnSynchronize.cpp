7209267e32e5fe29e16bd118dedcb250404be22d
kniz <kniz@kniz.net>
2014-12-04 09:51:44 +0900

    UPDATE: OnSynchronize Encapsulation 강화

--- Projects/Core/Sources/StandardEditorManager/Sources/Modules/NEStandardScriptEditor/Execute/innerclass/OnSynchronize/OnSynchronize.cpp
+++ Projects/Core/Sources/StandardEditorManager/Sources/Modules/NEStandardScriptEditor/Execute/innerclass/OnSynchronize/OnSynchronize.cpp
@@ -1,21 +1,31 @@
+
 #include "../../NEStandardScriptEditor.hpp"
 
 namespace NE
 {
-	void NEStandardScriptEditor::OnSynchronize::_setScriptCode(NEStandardScriptEditor& owner, NENode& target, type_code new_scriptcode) const
+	NEStandardScriptEditor::OnSynchronize::OnSynchronize(NEStandardScriptEditor& owner, bool is_insertion_mode, NECodeType::CodeType code_type)
+		: _owner(owner), _is_insertion_mode(is_insertion_mode), _code_type(code_type) 
 	{
-		owner._setScriptCode(target, new_scriptcode);
+
 	}
-	void NEStandardScriptEditor::OnSynchronize::_setNameCode(NEStandardScriptEditor& owner, NENode& target, type_code new_namecode) const
+	void NEStandardScriptEditor::OnSynchronize::_setScriptCode(NENode& target, type_code new_scriptcode) const
 	{
-		owner._setNameCode(target, new_namecode);
+		_owner._setScriptCode(target, new_scriptcode);
 	}
-	void NEStandardScriptEditor::OnSynchronize::_setGroupCode(NEStandardScriptEditor& owner, NENode& target, const NECodeSet& new_groupcode) const
+	void NEStandardScriptEditor::OnSynchronize::_setNameCode(NENode& target, type_code new_namecode) const
 	{
-		owner._setGroupCode(target, new_groupcode);
+		_owner._setNameCode(target, new_namecode);
 	}
-	void NEStandardScriptEditor::OnSynchronize::_setPriorityCode(NEStandardScriptEditor& owner, NENode& target, type_code new_priority) const
+	void NEStandardScriptEditor::OnSynchronize::_setGroupCode(NENode& target, const NECodeSet& new_groupcode) const
 	{
-		owner._setPriorityCode(target, new_priority);
+		_owner._setGroupCode(target, new_groupcode);
+	}
+	void NEStandardScriptEditor::OnSynchronize::_setPriorityCode(NENode& target, type_code new_priority) const
+	{
+		_owner._setPriorityCode(target, new_priority);
+	}
+	void NEStandardScriptEditor::OnSynchronize::_synchronizeCodesInKey(NEKey& key, type_code new_code)
+	{
+		_owner._synchronizeCodesInKey(key, _code_type, new_code, _is_insertion_mode);
 	}
 }
--- Projects/Core/Sources/StandardEditorManager/Sources/Modules/NEStandardScriptEditor/Execute/innerclass/OnSynchronize/OnSynchronize.hpp
+++ Projects/Core/Sources/StandardEditorManager/Sources/Modules/NEStandardScriptEditor/Execute/innerclass/OnSynchronize/OnSynchronize.hpp
@@ -1,11 +1,19 @@
 class NE_DLL OnSynchronize
 {
 public:
-	virtual type_result onSynchronize(NEStandardScriptEditor& owner, NENode& node_to_be_synchronized, type_code to_be_manipulated) = 0;
-	
+	OnSynchronize(NEStandardScriptEditor& owner, bool is_insertion_mode, NECodeType::CodeType code_type);
+
+	virtual type_result onSynchronize(NENode& node_to_be_synchronized, type_code to_be_manipulated) = 0;
+
 protected:
-	void _setScriptCode(NEStandardScriptEditor& owner, NENode& target, type_code new_scriptcode) const;
-	void _setNameCode(NEStandardScriptEditor& owner, NENode& target, type_code new_namecode) const;
-	void _setGroupCode(NEStandardScriptEditor& owner, NENode& target, const NECodeSet& new_groupcode) const;
-	void _setPriorityCode(NEStandardScriptEditor& owner, NENode& target, type_code new_priority) const;
+	void _setScriptCode(NENode& target, type_code new_scriptcode) const;
+	void _setNameCode(NENode& target, type_code new_namecode) const;
+	void _setGroupCode(NENode& target, const NECodeSet& new_groupcode) const;
+	void _setPriorityCode(NENode& target, type_code new_priority) const;
+	void _synchronizeCodesInKey(NEKey& key, type_code new_code);
+
+private:
+	NEStandardScriptEditor& _owner;
+	bool _is_insertion_mode;
+	NECodeType::CodeType _code_type;
 };
