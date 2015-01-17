10631b54eb89d46a06aeb0a69e4d1849defbc191
kniz <kniz@kniz.net>
2015-01-17 21:36:40 +0900

    보존16

--- Projects/DX9Graphics/Sources/Modules/BufferClearer/BufferClearer.cpp
+++ Projects/DX9Graphics/Sources/Modules/BufferClearer/BufferClearer.cpp
@@ -10,52 +10,36 @@ namespace DX9Graphics
 
 		if(_header.isValid() != RESULT_NOTHING)
 		{
-			_header.getName() = _T(_NAME);
-			_header.getDeveloper() = _T(_DEVELOPER);
-			_header.setRevision(_REVISION);
-			_header.getComment() = _T(_COMMENT);
-			_header.getVersion()  = _T(_VERSION);
-			_header.getReleaseDate() = _T(_DATE);
-			_header.getModuleDependencies().create(_DEPENDENCIES_COUNT);
-			_header.setMaxErrorCodeCount(1);
+			_header.getName() = "BufferClear";
+			_header.getDeveloper() = "kniz";
+			_header.setRevision(1);
+			_header.getComment() = "NodeManager 영역에 있는 DX9를 찾아서, 백버퍼를 지정한 색으로 초기화 합니다.\n반드시 DX9 Module을 NodeManager 영역에 생성 후, 실행해야 합니다.";
+			_header.getVersion()  = "0.0.1a";
+			_header.getReleaseDate() = "2013-01-29";
+			NETStringSet& args = _header.getArgumentsComments();
+			args.resize(4);
+			args.push("Red\n초기화할 색의 Red 성분입니다.\n최대 255까지 입력가능합니다.");
+			args.push("Green\n초기화할 색의 Green 성분입니다.\n최대 255까지 입력가능합니다.");
+			args.push("Blue\n초기화할 색의 Blue 성분입니다.\n최대 255까지 입력가능합니다.");
+			args.push("How will it be drawn\n화면에 어떻게 그려질지 정합니다.");
 		}
 
 		return _header;
 	}
 
-	type_result BufferClearer::initialize()
-	{
-		SuperClass::initialize();
-
-		NEKeyCodeSet& keyset = getKeySet();
-		keyset.resize(keyset.getLength() + _KEY_COUNT);		
-		keyset.push(NEUIntKey(_DEFAULT_COLOR));
-		keyset.push(NEUIntKey(_DEFAULT_FLAG));
-
-		return RESULT_SUCCESS;
-	}
-
-	type_result BufferClearer::execute()
+	type_result BufferClearer::_onExecute()
 	{
 		//	main:
-		DX9& dx9 = getBinded();
+		DX9& dx9 = DX9::getInstancedDX();
 		if( ! &dx9)
-		{
-			ALERT_ERROR(" : 디바이스가 초기화 되지 않았으므로 작업을 취소합니다.");
-			return RESULT_TYPE_ERROR | RESULT_ABORT_ACTION;
-		}
-		LPDIRECT3DDEVICE9 device = dx9.getDevice();		
-		//		타겟팅:
-		NEUIntKey& color = static_cast<NEUIntKey&>(getKeySet()[1]);
-		NEUIntKey& flag = static_cast<NEUIntKey&>(getKeySet()[2]);
-		if(FAILED(device->Clear(0, NULL, flag.getValue(), color.getValue(), 1.0f, 0)))
-		{
-			ALERT_ERROR(" : 버퍼 클리어 실패");
+			return ALERT_ERROR(" : 디바이스가 초기화 되지 않았으므로 작업을 취소합니다.");
 
-			return RESULT_TYPE_ERROR;
-		}
+		LPDIRECT3DDEVICE9 device = dx9.getDevice();
+		//		타겟팅:
+		type_uint color = D3DCOLOR_XRGB(arg_red.getValue(), arg_green.getValue(), arg_blue.getValue());
 		
-		
+		if(FAILED(device->Clear(0, NULL, arg_flag.getValue(), color, 1.0f, 0)))
+			return ALERT_ERROR(" : 버퍼 클리어 실패");		
 
 		//	post:
 		return RESULT_SUCCESS;
--- Projects/DX9Graphics/Sources/Modules/BufferClearer/BufferClearer.hpp
+++ Projects/DX9Graphics/Sources/Modules/BufferClearer/BufferClearer.hpp
@@ -10,13 +10,38 @@ namespace DX9Graphics
 		typedef NEModule SuperClass;
 
 	public:
+		NETArgument<NEUByteKey>	arg_red;
+		NETArgument<NEUByteKey> arg_green;
+		NETArgument<NEUByteKey> arg_blue;
+		NETArgument<NEUIntKey>	arg_flag;
+
+	public:
 		virtual NEExportable::ModuleHeader& getHeader() const;
 
-	public:
-		virtual type_result initialize();
+	protected:
+		virtual type_result _onFetchArguments(NEArgumentList& tray)
+		{
+			tray.push(arg_red);
+			tray.push(arg_green);
+			tray.push(arg_blue);
+			tray.push(arg_flag);
 
-	public:
-		virtual type_result execute();
+			return RESULT_SUCCESS;
+		}
+		virtual type_result _onFetchModule()
+		{
+			arg_red.setPurposeLimitation(NEArgumentBase::READ_BY);
+			arg_red.setValue(0);
+			arg_green.setPurposeLimitation(NEArgumentBase::READ_BY);
+			arg_green.setValue(0);
+			arg_blue.setPurposeLimitation(NEArgumentBase::READ_BY);
+			arg_blue.setValue(0);
+			arg_flag.setPurposeLimitation(NEArgumentBase::READ_BY);
+			arg_flag.setValue(D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER);
+
+			return RESULT_SUCCESS;
+		}
+		virtual type_result _onExecute();
 
 	public:
 		virtual NEObject& clone() const;
