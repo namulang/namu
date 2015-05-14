41f15fb8ee4474f42d8e359c37b797ee7846a61e
kniz <kniz@kniz.net>
2015-05-14 23:17:11 +0900

    UPDATE: AfterBuild에 Dependecy 폴더 복사 기능 추가

--- Projects/AfterBuild/Sources/main.cpp
+++ Projects/AfterBuild/Sources/main.cpp
@@ -9,16 +9,21 @@ using std::cout;
 */
 void _tmain(int argc, TCHAR** argv)
 {
+	cout	<< "AfterBuild. Automatic worldSDK Binary Packager Version 0.0.2a\n"
+			<< "Copyrights (c) kniz, 2009-2015\n";
+
 	if(argc != 4)
 	{
-		cout	<<	"AfterBuild is a program launched after build one of the projects derived NodeEngine(Not Certified) SDK.\n"
-				<<	"this program will increase one build count and copy all of those files to \'Binaries\' directory of each projects.\n\n"
-				<<	"Usage : \n"
+		cout	<<	"\n"
+				<<	"AfterBuild is a program launched after build one of the projects derived WorldSDK. "
+				<<	"this program will increase one build count and copy all of those files to \'Binaries\' directory of each projects.\n"
+				<<	"\nHistory :\n"
+				<<	"\t0.0.2a\t2015-05-08 : function that package Dependencies(what located on './Dependencies/Libraries') has been added.\n"
+				<<	"\t0.0.1a\t2013-00-00 : Creation.\n"
+				<<	"\nUsage : \n"
 				<<	"\tAfterBuild.exe <ini path> <binary path> <the path to be copied>\n";
 		return;
 	}
-
-
 
 	//	main:
 	const int MAX_SIZE = 256;
@@ -33,12 +38,26 @@ void _tmain(int argc, TCHAR** argv)
 	_itot(build_count, buffer, 10);
 
 	WritePrivateProfileString(_T("Version"), _T("BuildNo"), buffer, ini_path);
-		
-	TCHAR commandline[256] = {0, };
-	_tcscpy(commandline, _T("xcopy /e /Y /I "));
-	_tcscat(commandline, binary_path);
-	_tcscat(commandline, _T(" "));
-	_tcscat(commandline, destination_path);
 
-	_tsystem(commandline);
+	{
+		TCHAR commandline[256] = { 0, };
+		_tcscpy(commandline, _T("xcopy /e /Y /I "));
+		_tcscat(commandline, binary_path);
+		_tcscat(commandline, _T(" "));
+		_tcscat(commandline, destination_path);
+
+		_tsystem(commandline);
+	}
+	
+	{
+		TCHAR commandline[256] = { 0, };
+		_tcscpy(commandline, _T("xcopy /e /Y /I "));
+		_tcscat(commandline, binary_path);
+		_tcscat(commandline, _T("\\..\\..\\Dependencies\\Libraries "));
+		_tcscat(commandline, destination_path);
+		_tcscat(commandline, _T("\\Dependencies"));
+	
+		_tsystem(commandline);
+	}
+
 }
