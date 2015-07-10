fc50cd1452c9e4a31bcb09224c8c90e8b94bc440
kniz <kniz@kniz.net>
2015-07-11 05:44:22 +0900

    UPDATE: 강화된 Argument 편집 인터페이스

--- Projects/Cooee/Sources/Cooee/Sources/Modules/ModuleTerminal/ModuleTerminal.cpp
+++ Projects/Cooee/Sources/Cooee/Sources/Modules/ModuleTerminal/ModuleTerminal.cpp
@@ -17,13 +17,12 @@ public:
 	{
 		ListWindow::onUpdateData();
 
-		type_index size = isSelectorArgument() ? 4 : 3;
+		type_index size = isSelectorArgument() ? 3 : 2;
 
 		list.items.create(size);
-		list.items.push("기본 값을 수정한다.");
 		list.items.push("넣을 키를 정한다.");
-		list.items.push("키를 넣지 않는다.");
-		if (size >= 4)
+		list.items.push("기본 값을 수정한다.");
+		if (size >= 3)
 			if (isWantingToLock())
 				list.items.push("BindingLock을 헤제한다.");
 			else
@@ -88,54 +87,130 @@ public:
 
 			CodeInputer(NENode& new_owner)
 				: LG::TextListWindow("새로 추가할 키를 선택하세요.",
-				25, 7, 30, 10, BLACK,LIGHTGREEN,LIGHTGREEN, BLACK),
-				owner(new_owner)
+				25, 7, 30, 10, BLACK, LIGHTGREEN, LIGHTGREEN, BLACK),
+				owner(new_owner), _local_keys_listed(false)
 			{
 				list.use_matching = true;
 			}
 
 			virtual void onFocused()
 			{
-				if (!&owner) return;
-				const NEKeyCodeSet& ks = owner.getKeySet();
-
 				NEModule& m = toCaller().toCaller().castObject();
 				NEArgumentBase& arg = m.getArguments()[toCaller().toCaller().argument_namelist.choosed];
 				list.text.text = arg.getKeyName();
 
-				list.items.create(ks.getLength());				
-				for (int n = 0; n < ks.getLength(); n++)
-					if(list.items.find(ks[n].getName()) == NE_INDEX_ERROR)
-						list.items.push(ks[n].getName() + "(" + ks[n].getTypeName() + ")");				
+				list.items = _updateList(true);
+			}
+
+			bool _findParsedKeyName(const NEStringSet& strs, const NEString& target)
+			{
+				NEString lowered = target.toLowerCase();
+				for(int n = 0; n < strs.getLength(); n++)
+				{
+					if(_extractKeyName(strs[n]).toLowerCase().isEqualElement(target))
+						return true;
+				}
+
+				return false;
 			}
 
 			virtual void onInputed(const NEString& inputed)
 			{
 				NEModule& m = toCaller().toCaller().castObject();
 				NEArgumentBase& arg = m.getArguments()[toCaller().toCaller().argument_namelist.choosed];
-				if (!&arg)
-				{
-					::Core::pushMessage("Argument를 찾지 못했습니다.");
-					return;
+				//	주어진 입력이 LocalKeySet에 있는지, KeySet에 있는지를 확인하고
+				//	양쪽 모두에 없다면, 기본값으로 집어넣고 키이름을 ""로 만든다.
+				NEString keyname = _extractKeyName(inputed);
+
+				if(	! _findParsedKeyName(_updateListFromKeySet(true), keyname)		&&
+					! _findParsedKeyName(_updateListFromLocalKeySet(true), keyname)	)
+				{					
+					arg.getDefaultKey() = NEStringKey(inputed);
+					arg.setKeyName("");
 				}
-
-				NEStringSet splited;
-				inputed.split("(", splited);
-				if(splited.getLength() > 0)
-					splited.pop();
-				NEString keyname;
-				for (int n = 0; n < splited.getLength(); n++)
-					keyname += splited[n];
-
-				if(keyname.getLength() <= 0)
-					keyname = inputed;
-
-				arg.setKeyName(keyname);
+				else
+					arg.setKeyName(keyname);
 
 				toCaller().delete_me = delete_me = true;
 			}
 
+			virtual void onKeyPressed(int inputed)
+			{
+				TextListWindow::onKeyPressed(inputed);
+
+				switch(inputed)
+				{
+				case UP: case DOWN: case LEFT: case RIGHT: 
+					break;
+
+				default:
+					list.items = _updateList(false);
+				}
+			}
+
+			NEStringSet _updateList(bool force)
+			{
+				if (list.text.text.getLength() > 0	&&
+					list.text.text[0] == '$'		)
+					return _updateListFromLocalKeySet(force);
+				else
+					return _updateListFromKeySet(force);
+			}
+
+			NEStringSet _updateListFromKeySet(bool force)
+			{
+				if (!&owner) return list.items;
+				if (!force && !_local_keys_listed) return list.items;
+				const NEKeyCodeSet& ks = owner.getKeySet();
+
+				NEStringSet to_return;
+				to_return.create(ks.getLength());
+				for (int n = 0; n < ks.getLength(); n++)
+					if (to_return.find(ks[n].getName()) == NE_INDEX_ERROR)
+						to_return.push(ks[n].getName() + "(" + ks[n].getTypeName() + ")");
+
+				if( ! force)
+					_local_keys_listed = false;
+				return to_return;
+			}		
+			NEString _extractKeyName(const NEString& full_string)
+			{
+				NEStringSet splited;
+				full_string.split("(", splited);
+				if (splited.getLength() > 0)
+					splited.pop();
+				NEString to_return;
+				for (int n = 0; n < splited.getLength(); n++)
+					to_return += splited[n];
+
+				if (to_return.getLength() <= 0)
+					to_return = full_string;
+
+				return to_return;
+			}
+			NEStringSet _updateListFromLocalKeySet(bool force)
+			{
+				if (!force && _local_keys_listed) return list.items;
+				typedef NENodeManager::LocalStack::PersistentKeySet PersistentKeySet;
+				const PersistentKeySet& ks = Kernal::getInstance().getNodeManager().getLocalStack().getLocalKeySet();
+
+				NEStringSet to_return;
+				to_return.create(ks.getLength());
+				for (int n = 0; n < ks.getLength(); n++)
+				{
+					const NEKey& k = ks[n];
+					if( ! &k) continue;
+					if(to_return.find(k.getName()) == NE_INDEX_ERROR)
+						to_return.push(k.getName() + "(" + k.getTypeName() + ")");
+				}					
+
+				if( ! force)
+					_local_keys_listed = true;
+				return to_return;
+			}
+
 			NENode& owner;
+			bool _local_keys_listed;
 		};
 
 		NEModule& m = toCaller().castObject();
@@ -150,7 +225,7 @@ public:
 
 		switch (index)
 		{
-		case 0:
+		case 1:
 			{
 				NEEventHandler& handler = Editor::getInstance().getEventHandler();
 				NEKey& dk = arg.getDefaultKey();
@@ -165,7 +240,7 @@ public:
 			}
 			break;
 
-		case 1:
+		case 0:
 			{
 				ModuleTerminal& mt = toCaller();
 				call(CodeInputer(mt.getOwnerNodeOf(mt.castObject())));
@@ -173,11 +248,6 @@ public:
 			break;
 
 		case 2:
-			arg.setKeyName(NETString());
-			delete_me = true;
-			break;
-
-		case 3:
 			if (!isSelectorArgument()) return;
 			switchWantingToLock();
 			break;
