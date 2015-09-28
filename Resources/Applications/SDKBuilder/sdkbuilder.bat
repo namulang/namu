a9ee27eadd08e271511e12a73af3149c5e75925b
kniz <kniz@kniz.net>
2015-09-28 12:20:34 +0900

    UPDATE: SDKBuilder support preventing not to remove resource files.

--- Resources/Applications/SDKBuilder/sdkbuilder.bat
+++ Resources/Applications/SDKBuilder/sdkbuilder.bat
@@ -1,8 +1,8 @@
 echo	.\
 @echo	off
 echo	*	Node SDK Builder 
-echo	*		ver 0.2.5
-echo	*		2015. 04. 20
+echo	*		ver 0.3.0
+echo	*		2015. 09. 28
 echo	*		Only windows 7, 8 tested.
 echo	*
 echo	*		by kniz. 
@@ -16,26 +16,24 @@ echo	*	You must run this after new version of Core solution has been builded.
 echo	*	this task takes some minutes till be finished.
 echo	*
 echo 	*	Updates :
+echo	*	0.3.0
+echo	*		- Now can choose whether make leave script files or not when build 
+echo	*		a sdk.
+echo	*
 echo	*	0.2.5
 echo	*		- will also package script examples.
-echo	*
 echo	*	0.2.4
 echo	*		- solved that xcopy can't be called causeof environmental variable(path).
-echo	*
 echo	*	0.2.3	2015-04-15
 echo	*		- build sdk with some sound resources as contents of sounds.
-echo	*
 echo	*	0.2.2	2013-08-14
 echo	*		- path problem issue has been solved. (now "" will be added at 
 echo	*		  the edge of path)
-echo	*
 echo	*	0.2.1
 echo	*		- fix to create sub directory which was named "Scripts" in 
 echo	*		  Library Directory.
-echo	*
 echo	*	0.2.0
 echo	*		- Manager packages will be added to ./Modules directory.
-echo	*
 echo	*	0.1.0
 echo	*		- now pdb files can be attachable with choose.
 echo	*		- MDK Templates has been seperated from Includes
@@ -43,12 +41,29 @@ echo	*		- some bug was fixed.
 echo	*	-----------------------------------------------------
 pause
 
+rem add envrionmental variables for xcopy issue.
 set Path=%Path%;%SystemRoot%\System32
-echo	*	Do you want generate SDK with PDB files?(y/n)
-set /p answer="	*	> "
+
+rem create a temporary folder
+md tmp
+set TEMP=.\tmp
 
 set target_directory=.\..\..\..\SDK
 set source_directory=.\..\..\..\Projects\Core
+
+echo	*	Do you want generate SDK with PDB files?(y/n)
+set /p answer="	*	> "
+echo	* 	Want not to remove resources("Scripts", "Images" folder) ?(y/n)
+set /p isWantingNotToRemoveResources=" *	> "
+
+if %isWantingNotToRemoveResources% == y (
+	md %TEMP%\Scripts
+	md %TEMP%\Images
+	xcopy /s /Q "%target_directory%\Libraries\Scripts\*.script" "%TEMP%\Scripts"
+	xcopy /s /Q "%target_directory%\Libraries\Images\*.*" "%TEMP%\Images"
+)
+
+
 echo	*
 echo	*
 echo	*	Cleaning previous data. . .
@@ -101,6 +116,25 @@ echo	*
 echo	*
 pause
 
+if %isWantingNotToRemoveScriptFiles% == y (
+echo	*
+echo	*
+echo	*	Preventing not to remove old resource files. . .
+echo	*
+echo	*
+
+xcopy /s /Q /Y "%TEMP%\Scripts\*.script" "%target_directory%\Libraries\Scripts\" 
+xcopy /s /Q /Y "%TEMP%\Images\*.*" "%target_directory%\Libraries\Images\" 
+
+echo	*
+echo	*
+echo	*	Completed.
+echo	*
+echo	*
+pause
+)
+
+
 echo	*
 echo	*
 echo	*	Removing useless files. . .
@@ -111,6 +145,9 @@ del /s "%target_directory%\Includes\*.cpp"
 if %answer% == n del /s "%target_directory%\Libraries\*.pdb"
 
 rd "%target_directory%\Resources\Applications\SDKBuilder" /s /q
+
+rd "%TEMP%" /s /q
+
 echo	*
 echo	*
 echo	*	Completed.
