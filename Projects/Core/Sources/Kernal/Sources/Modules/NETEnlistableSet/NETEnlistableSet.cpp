1f0b7fbb0fe87639efe2886cff8fb025617743fe
kniz <kniz@kniz.net>
2016-02-27 18:53:33 +0900

    + Move actural class template files into 'Modules' directory, + Same thing procedured in VS project file.

--- Projects/Core/Sources/Kernal/Sources/Modules/NETEnlistableSet/NETEnlistableSet.cpp
+++ Projects/Core/Sources/Kernal/Sources/Modules/NETEnlistableSet/NETEnlistableSet.cpp
@@ -3,22 +3,22 @@
 namespace NE
 {
 	typedef NENamedUnit T;
-	typedef NETEnlistableSet<NENamedUnit> ThisClass;
+	typedef NEEnlistableSetTemplate<NENamedUnit> ThisClass;
 
-	NETEnlistableSet<NENamedUnit>::NETEnlistableSet(const NECodeType& script_type, const NECodeType& name_type)
+	NEEnlistableSetTemplate<NENamedUnit>::NEEnlistableSetTemplate(const NECodeType& script_type, const NECodeType& name_type)
 		: T(script_type, name_type), _manager(&NEGlobalManagerOffer::getGlobalManager()), _is_enlisted(false)
 	{
 		//	getGlobalManagerOnCopyConstructor에 관하여:
 		//		NEIndexedModuleSet.cpp에 관련 주석을 참고하라
 	}
-	NETEnlistableSet<NENamedUnit>::NETEnlistableSet(const ThisClass& source)
+	NEEnlistableSetTemplate<NENamedUnit>::NEEnlistableSetTemplate(const ThisClass& source)
 		: T(source), _manager(&NEGlobalManagerOffer::getGlobalManager()), _is_enlisted(false)
 	{
 		//	getGlobalManagerOnCopyConstructor에 관하여:
 		//		NEIndexedModuleSet.cpp에 관련 주석을 참고하라
 	}
 
-	const ThisClass& NETEnlistableSet<NENamedUnit>::operator=(const ThisClass& source)
+	const ThisClass& NEEnlistableSetTemplate<NENamedUnit>::operator=(const ThisClass& source)
 	{
 		/*
 		_manager를 복사하지 않도록 하기 위해서 operator=를 정의한다.
@@ -28,41 +28,41 @@ namespace NE
 		return *this;
 	}
 
-	NEEnlistableManager& NETEnlistableSet<NENamedUnit>::getManager()
+	NEEnlistableManager& NEEnlistableSetTemplate<NENamedUnit>::getManager()
 	{
 		return *_manager;
 	}
-	const NEEnlistableManager& NETEnlistableSet<NENamedUnit>::getManager() const
+	const NEEnlistableManager& NEEnlistableSetTemplate<NENamedUnit>::getManager() const
 	{
 		return *_manager;
 	}
-	bool NETEnlistableSet<NENamedUnit>::isManaged() const
+	bool NEEnlistableSetTemplate<NENamedUnit>::isManaged() const
 	{
 		return _manager != NE_NULL;
 	}
-	bool NETEnlistableSet<NENamedUnit>::isEnlisted() const
+	bool NEEnlistableSetTemplate<NENamedUnit>::isEnlisted() const
 	{
 		return _is_enlisted;
 	}
-	NEBinaryFileSaver& NETEnlistableSet<NENamedUnit>::serialize(NEBinaryFileSaver& saver) const
+	NEBinaryFileSaver& NEEnlistableSetTemplate<NENamedUnit>::serialize(NEBinaryFileSaver& saver) const
 	{
 		SuperClass::serialize(saver);
 
 		return saver << _is_enlisted;
 	}
-	NEBinaryFileLoader& NETEnlistableSet<NENamedUnit>::serialize(NEBinaryFileLoader& loader)
+	NEBinaryFileLoader& NEEnlistableSetTemplate<NENamedUnit>::serialize(NEBinaryFileLoader& loader)
 	{
 		SuperClass::serialize(loader);
 
 		return loader >> _is_enlisted;
 	}
-	type_result NETEnlistableSet<NENamedUnit>::_setManager(NEEnlistableManager& manager)
+	type_result NEEnlistableSetTemplate<NENamedUnit>::_setManager(NEEnlistableManager& manager)
 	{
 		_manager = &manager;
 
 		return RESULT_SUCCESS;
 	}
-	type_result NETEnlistableSet<NENamedUnit>::_setEnlisted(bool enlisted)
+	type_result NEEnlistableSetTemplate<NENamedUnit>::_setEnlisted(bool enlisted)
 	{
 		_is_enlisted = enlisted;
 
