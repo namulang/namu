c64040f139cf68c59749dc0658d5465b9820692e
kniz <kniz@kniz.net>
2016-05-15 12:07:16 +0900

    - remove useless document files

--- Resources/Backups/MDK Templates/mine/MyModule/Project1/source/common/define.hpp
+++
@@ -1,2 +0,0 @@
-#include "Property.hpp"
-#include "EntryPoint.hpp"
--- Resources/Backups/MDK Templates/mine/MyModule/Project1/source/module/MyModule/define/define.hpp
+++
@@ -1,8 +0,0 @@
-#define _NAME					"MyModule"
-#define _AUTHOR					"개발자명 or 웹사이트"
-#define _REVISION				1 // 커널에서 판별하는 버전표기
-#define _VERSION				"1.00" // 사용자에게 보여주는 용도의 버전표기
-#define _COMMENT				"모듈에 대한 간략한 설명입니다."
-#define	_DATE					"2011-1-1 or 2011.1.1 or 2011/1/1 등등"
-#define _DEPENDENCIES_COUNT		0
-#define _KEY_NUMBER				0
