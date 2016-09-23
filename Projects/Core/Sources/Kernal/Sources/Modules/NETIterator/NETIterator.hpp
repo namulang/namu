a728bfa0981def75be6b2d9ffc695d631eb011a2
kniz <kniz@kniz.net>
2016-09-24 01:27:41 +0900

    ~Implementing NETList

--- Projects/Core/Sources/Kernal/Sources/Modules/NETIterator/NETIterator.hpp
+++ Projects/Core/Sources/Kernal/Sources/Modules/NETIterator/NETIterator.hpp
@@ -7,136 +7,37 @@ namespace NE
 	NE_DEFINE_INTERFACE_ONLY(NETCIterator<T>, template <typename T>)
 
 	template <typename T>
-	NETCIterator<T>::NETCIterator(const NEIteratorBase& origin)
-		: Super(), _proxy(origin);
-	{
-	
-	}
-
-	template <typename T>
-	NETCIterator<T>::NETCIterator(const This& rhs)
-		: Super(rhs)
-	{
-		_assign(rhs);
-	}
-
-	template <typename T>
-	NETCIterator<T>::~NETCIterator()
-	{
-		_release();
-	}
-
-	template <typename T>
-	type_result NETCIterator<T>::next(type_count step_for_next)
-	{
-		if( ! _proxy.isBinded())
-			return RESULT_TYPE_WARNING;
-
-		return _proxy->next(step_for_next);
-	}
-
-	template <typename T>
-	type_result NETCIterator<T>::back(type_count step_for_next)
-	{
-		if( ! _proxy.isBinded())
-			return RESULT_TYPE_WARNING;
-
-		return _proxy->back(step_for_next);
-	}
-
-	template <typename T>
-	const T& NETCIterator<T>::get() const
-	{
-		T* nullpointer = NE_NULL;
-		if(	! _proxy.isBinded()		||
-			this == &_proxy.get()	)
-			return *nullpointer;
-
-		return *_proxy;
-	}
-
-	template <typename T>
-	NEObject& NETCIterator<T>::clone() const
-	{
-		return *(new This(*this));
-	}
-
-	template <typename T>
-	void NETCIterator<T>::release()
-	{
-		_proxy.release();	
-	}
-
-	template <typename T>
-	NEBinaryFileSaver& NETCIterator<T>::serialize(NEBinaryFileSaver& saver) const
-	{
-		//	TODO: if we don't use binder, we can't save it.
-		return saver << _proxy;
-	}
-
-	template <typename T>
-	NEBinaryFileLoader& NETCIterator<T>::serialize(NEBinaryFileLoader& loader)
-	{
-		//	same as above.
-		return loader >> _proxy;
-	}
-
-	template <typename T>
-	type_result NETCIterator<T>::isValid() const
-	{
-		if( ! _proxy.isBinded()) return RESULT_TYPE_WARNING;
-
-		return RESULT_SUCCESS;
-	}
-
-	template <typename T>
-	NETCIterator<T>& NETCIterator<T>::operator=(const This& rhs)
-	{
-		if(this == &rhs) return *this;
-
-		Super::operator=(rhs);
-
-		return _assign(rhs);
-	}
-
-	template <typename T>
-	type_bool NETCIterator<T>::operator==(const This& rhs) const
-	{
-		type_int evaluation = _proxy.isBinded() + rhs._proxy.isBinded();
-		if(evaluation < 2)
-			return evaluation == 0;
-
-		return &_proxy.get() == &rhs._proxy.get();
-	}
-
-	template <typename T>
-	type_bool NETCIterator<T>::operator!=(const This& rhs) const
-	{
-		return ! operator==(rhs);
-	}
-
-	template <typename T>
 	NETCIterator<T>& NETCIterator<T>::operator++()
 	{
-		return static_cast<This&>(Super::operator++());
+		next();
+
+		return *this;
 	}
 
 	template <typename T>
 	NETCIterator<T>& NETCIterator<T>::operator--()
 	{
-		return static_cast<This&>(Super::operator--());
+		back();
+
+		return *this;
 	}
 
 	template <typename T>
 	NETCIterator<T> NETCIterator<T>::operator+(type_count step_for_next)
 	{
-		return static_cast<This&>(Super::operator+(step_for_next));
+		This copied(*this);
+		copied.next(step_for_next);
+
+		return copied;
 	}
 
 	template <typename T>
 	NETCIterator<T> NETCIterator<T>::operator-(type_count step_for_back)
 	{
-		return static_cast<This&>(Super::operator-(step_for_back));
+		This copied(*this);
+		copied.back(step_for_back);
+
+		return copied;
 	}
 
 	template <typename T>
@@ -151,78 +52,40 @@ namespace NE
 		return get();
 	}
 
+	NE_DEFINE_INTERFACE_ONLY(NETIterator<T>, template <typename T>)
+	
 	template <typename T>
-	NETCIterator<T>& NETCIterator<T>::_assign(const This& rhs)
+	NETIterator<T>& NETIterator<T>::operator++()
 	{
-		release();
-
-		_proxy = rhs._proxy;
+		next();
 
 		return *this;
 	}
 
-	NE_DEFINE_INTERFACE_ONLY(NETIterator<T>, template <typename T>)
-	
-	template <typename T>
-	NEIterator<T>::NEIterator()
-		: Super()
-	{
-
-	}
-
-	template <typename T>
-	NEIterator<T>::NEIterator(const NEIteratorBase& origin)
-		: Super(origin)
-	{
-
-	}
-
-	template <typename T>
-	NEIterator<T>::NEIterator(const This& rhs)
-		: Super(rhs)
-	{
-
-	}
-
-	template <typename T>
-	T& NETIterator<T>::get()
-	{
-		T* nullpointer = NE_NULL;
-		if( ! _proxy.isBinded()		||
-			this == &_proxy.get()	)
-			return *nullpointer;
-
-		return *_proxy;
-	}
-
-	template <typename T>
-	NEObject& NETIterator<T>::clone() const
-	{
-		return *(new This(*this));
-	}
-
 	template <typename T>
 	NETIterator<T>& NETIterator<T>::operator++()
 	{
-		return static_cast<This&>(Super::operator++());
-	}
+		back();
 
-	template <typename T>
-	NETIterator<T>& NETIterator<T>::operator++()
-	{
-		return static_cast<This&>(Super::operator--());
+		return *this;
 	}
 
 	template <typename T>
 	NETIterator<T> NETIterator<T>::operator+(type_count step_for_next)
 	{
-		return static_cast<This&>(Super::operator+(step_for_next));
+		This copied(*this);
+		copied.next(step_for_next);
+
+		return copied;
 	}
 
 	template <typename T>
 	NETIterator<T> NETIterator<T>::operator-(type_count step_for_back)
 	{
-		return static_cast<This&>(Super::operator-(step_for_back));
+		This copied(*this);
+		copied.back(step_for_back);
+
+		return copied;
 	}
 
 	template <typename T>
--- Projects/Core/Sources/Kernal/Sources/Modules/NETIterator/NETIterator.inl
+++ Projects/Core/Sources/Kernal/Sources/Modules/NETIterator/NETIterator.inl
@@ -1,7 +1,6 @@
 #pragma once
 
 #include "../NEIteratorBase/NEIteratorBase.hpp"
-#include "../NETBinder/NETBinder.hpp"
 
 namespace NE
 {
@@ -19,51 +18,21 @@ namespace NE
 	template <typename T>
 	class NETCIterator : public NEIteratorBase 
 	{
-		//	we're declaring this as Interface class even though this can be Class:
-		//		There is no default constructor, so this can't be instantiated by 
-		//		NE_DECLARE_CLASS macro.
 		NE_DECLARE_INTERFACE_ONLY(NETCIterator, NEIteratorBase)
 
-	protected:
-		NETCIterator();
 	public:
-		NETCIterator(const NEIteratorBase& origin);	//	actually, we've to use NEBinder<This>& instead of this.
-		NETCIterator(const This& rhs);
-		virtual ~NETCIterator();
+		virtual type_result next(type_count step_for_next) = 0;
+		virtual type_result back(type_count step_for_back) = 0;
+		virtual const T& get() const = 0;
 
 	public:
-		virtual type_result next(type_count step_for_next);
-		virtual type_result back(type_count step_for_back);
-		virtual const T& get() const;
-		//	from NEObject:
-		virtual NEObject& clone() const;
-		virtual void release();
-		virtual NEBinaryFileSaver& serialize(NEBinaryFileSaver& saver) const;
-		virtual NEBinaryFileLoader& serialize(NEBinaryFileLoader& loader);
-		virtual type_result isValid() const;
-
-	public:
-		//	Method hiding:
-		This& operator=(const This& rhs);
-		type_bool operator==(const This& rhs) const;
-		type_bool operator!=(const This& rhs) const;
+		//	Method hiding:		
 		This& operator++();
 		This& operator--();
 		This operator+(type_count step_for_next);
 		This operator-(type_count step_for_back);
 		const T& operator*() const;
 		const T& operator->() const;
-
-	private:
-		void _release();
-		This& _assign(const This& rhs);
-
-	protected:
-		//	Why does this class use proxy?:
-		//		This'll be used at NETCollector hierarchy. And in order to return
-		//		Iterator instance by value at NETCollector class, we've to use
-		//		proxy which contains how iterate instance cursor.
-		NETBinder<NEIteratorBase> _proxy;	// TODO: this should be changed to NEBinder
 	};
 
 	template <typename T>
@@ -71,16 +40,8 @@ namespace NE
 	{
 		NE_DECLARE_INTERFACE_ONLY(NETIterator, NEIteratorBase)
 
-	protected:
-		NETIterator();
 	public:
-		NETIterator(const NEIteratorBase& origin);	//	actually, we've to use NEBinder<This>& instead of this.
-		NETIterator(const This& rhs);
-		virtual ~NETIterator();
-
-	public:
-		virtual T& get();
-		virtual NEObject& clone() const;
+		virtual T& get() = 0;
 
 	public:
 		//	Method hiding:
