2e9caef563469276ebdf0960765d78e140913cbf
kniz <kniz@kniz.net>
2016-09-30 23:37:01 +0900

    + add NETDeeper

--- Projects/Core/Sources/Kernal/Sources/Modules/NETDeeper/NETDeeper.hpp
+++ Projects/Core/Sources/Kernal/Sources/Modules/NETDeeper/NETDeeper.hpp
@@ -2,5 +2,290 @@
 
 namespace NE
 {
+#define _TEMPLATE   template <typename <typename> Template, typename T>
+#define _OWNERNAME  NETDeeper<Collector<T>>
+#define _NAME       _OWNERNAME##::Iterator
 
+	NE_DEFINE_CLASS_ONLY_2(_NAME, NE_MACRO_FUNNEL(_TEMPLATE));
+
+	_TEMPLATE
+	_NAME::Iterator(NETBinder<BeanIterator>& bean_iterator)
+		: _proxied(bean_iterator);
+	{        
+
+	}
+
+	_TEMPLATE
+	type_bool _NAME::operator==(const This& rhs) const
+	{
+		return  Super::operator==(rhs)  &&
+			_proxied == rhs._proxied;
+	}
+
+	_TEMPLATE
+	const T& _NAME::get() const
+	{
+		This* unconsted = const_cast<This*>(this);
+
+		return unconsted->get();
+	}
+
+	_TEMPLATE
+	T& _NAME::get() 
+	{
+		//  TODO: code refactorying needs with NETNuller
+		//  with NETNuller:
+		//      return ***_proxied;
+		T* nullpointer = NE_NULL;
+		BeanIterator& bean_e = *_proxied;
+		if( ! &bean_e) return *nullpointer;
+
+		BeanType& bean = bean_e->get();
+		if( ! &bean) return *nullpointer;
+
+		return *bean;
+	}
+
+	_TEMPLATE
+	type_bool _NAME::isEnd() const
+	{
+		return _proxied.isEnd();
+	}
+
+	_TEMPLATE
+	type_result _NAME::next(type_index step_for_next)
+	{
+		return _proxied.next(step_for_next);
+	}
+
+	_TEMPLATE
+	type_result _NAME::back(type_index step_for_back)
+	{
+		return _proxied.back(step_for_back);
+	}
+
+	_TEMPLATE
+	type_index getLength() const
+	{
+		return _proxied.getLength();
+	}
+
+	_TEMPLATE
+	void _NAME::release()
+	{
+		_proxied.release();
+	}
+
+	_TEMPLATE
+	NEBinaryFileSaver& _NAME::serialize(NEBinaryFileSaver& saver) const
+	{
+		Super::serialize(saver);
+
+		return saver << _proxied;
+	}
+
+	_TEMPLATE
+	NEBinaryFileLoader& _NAME::serialize(NEBinaryFileLoader& loader)
+	{
+		Super::serialize(loader);
+
+		return loader >> _proxied;
+	}
+
+	_TEMPLATE
+	type_result _NAME::isValid() const
+	{
+		type_result result = Super::isValid();
+		if(NEResult::hasError(result)) return RESULT_TYPE_ERROR;
+
+		return _proxied.isValid(); 
+	}
+
+#undef  _NAME
+#define _NAME   _OWNERNAME##::CIterator
+
+	NE_DEFINE_CLASS_ONLY_2(_NAME, NE_MACRO_FUNNEL(_TEMPLATE));
+
+	_TEMPLATE
+	_NAME::CIterator(NETBinder<BeanIterator>& bean_iterator)
+		: _proxied(bean_iterator)
+	{
+
+	}
+
+	_TEMPLATE
+	type_bool _NAME::operator==(const This& rhs) const
+	{
+		return  Super::operator==(rhs)  &&
+			_proxied == rhs._proxied;
+	}
+
+	_TEMPLATE
+	const T& _NAME::get() const
+	{
+		//  TODO: code refactorying needs with NETNuller
+		//  with NETNuller:
+		//      return ***_proxied;
+		const T* nullpointer = NE_NULL;
+		BeanCIterator& bean_e = *_proxied;
+		if( ! &bean_e) return *nullpointer;
+
+		const BeanType& bean = bean_e->get();
+		if( ! &bean) return *nullpointer;
+
+		return *bean;
+	}
+
+	_TEMPLATE
+	type_bool _NAME::isEnd() const
+	{
+		return _proxied.isEnd();
+	}
+
+	_TEMPLATE
+	type_result _NAME::next(type_index step_for_next)
+	{
+		return _proxied.next(step_for_next);
+	}
+
+	_TEMPLATE
+	type_result _NAME::back(type_index step_for_back)
+	{
+		return _proxied.back(step_for_back);
+	}
+
+	_TEMPLATE
+	type_index getLength() const
+	{
+		return _proxied.getLength();
+	}
+
+	_TEMPLATE
+	void _NAME::release()
+	{
+		_proxied.release();
+	}
+
+	_TEMPLATE
+	NEBinaryFileSaver& _NAME::serialize(NEBinaryFileSaver& saver) const
+	{
+		Super::serialize(saver);
+
+		return saver << _proxied;
+	}
+
+	_TEMPLATE
+	NEBinaryFileLoader& _NAME::serialize(NEBinaryFileLoader& loader)
+	{
+		Super::serialize(loader);
+
+		return loader >> _proxied;
+	}
+
+	_TEMPLATE
+	type_result _NAME::isValid() const
+	{
+		type_result result = Super::isValid();
+		if(NEResult::hasError(result)) return RESULT_TYPE_ERROR;
+
+		return _proxied.isValid(); 
+	}
+
+#undef  _NAME
+
+	NE_DEFINE_CLASS_ONLY_2(_OWNERNAME, NE_MACRO_FUNNEL(_TEMPLATE))
+
+	_TEMPLATE
+	NETBinder<NETCIterator<T>> _OWNERNAME::getIterator(type_index index) const
+	{
+		//  iterating:
+		NETBinder<BeanCIterator> iteratored = _proxy.getIterator(index);
+		iteratored += index;
+
+		//  packaging the return:
+		return NETBinder<NETCIterator<T>>( *(new CIterator(iteratored)) );
+	}
+
+	_TEMPLATE
+	NETBinder<NETIterator<T>> _OWNERNAME::getIterator(type_index index)
+	{
+		//  iterating:
+		NETBinder<BeanIterator> iteratored = _proxy.getIterator(index);
+		iteratored += index;
+
+		//  packaging the return:
+		return NETBinder<NETIterator<T>>( *(new Iterator(iteratored)) );
+	}
+
+	_TEMPLATE
+	type_index _OWNERNAME::insert(const NETCIterator<T>& where, const T& source)
+	{
+		return _proxy.insert(where, NETBinder<T>(source));
+	}
+
+	_TEMPLATE
+	type_index _OWNERNAME::insert(const NETCIterator<T>& where, const NETCIterator<T>& from, const NETCIterator<T>& end)
+	{
+		return _proxy.insert(where, from, end);
+	}
+
+	_TEMPLATE
+	type_index _OWNERNAME::remove(const NETCIterator<T>& from, const NETCIterator<T>& end)
+	{
+		return _proxy.remove(from, end);
+	}
+
+	_TEMPLATE
+	T& _OWNERNAME::getElement(type_index index)
+	{
+		return *_proxy.getElement(index);
+	}
+
+	_TEMPLATE
+	const T& _OWNERNAME::getElement(type_index index) const
+	{
+		return *_proxy.getElement(index);
+	}
+
+	_TEMPLATE
+	type_index _OWNERNAME::getLength() const 
+	{
+		return _proxy.getLength();
+	}
+
+	_TEMPLATE
+	void _OWNERNAME::release()
+	{
+		Super::release();
+
+		_proxy.release();
+	}
+
+	_TEMPLATE
+	type_result _OWNERNAME::isValid() const
+	{
+		type_result result = Super::isValid();
+		if(NEResult::hasError(result)) return result;
+
+		return _proxy.isValid();
+	}
+
+	_TEMPLATE
+	NEBinaryFileSaver& _OWNERNAME::serialize(NEBinaryFileSaver& saver) const
+	{
+		Super::serialize(saver);
+
+		return saver << _proxy;
+	}
+
+	_TEMPLATE
+	NEBinaryFileLoader& _OWNERNAME::serialize(NEBinaryFileLoader& loader)
+	{
+		Super::serialize(loader);
+
+		return loader >> _proxy;
+	}
+
+#undef _OWNERNAME
+#undef _TEMPLATE
 }
--- Projects/Core/Sources/Kernal/Sources/Modules/NETDeeper/NETDeeper.inl
+++ Projects/Core/Sources/Kernal/Sources/Modules/NETDeeper/NETDeeper.inl
@@ -5,15 +5,78 @@
 namespace NE
 {
 	template <template <typename> Collector, typename T>
-	class NE_DLL NETDeeper : public NETCollector<T>
+	class NETDeeper : public NETCollector<T>
 	{
-		NE_DECLARE_CLASS_ONLY(NETDeeper<Collector<T>>, NETCollector<T>)
+		typedef NETDeeper<Collector<T>> _This;
+
+		NE_DECLARE_CLASS_ONLY(_This, NETCollector<T>)
+
+	protected:
+		typedef NETBinder<T>			BeanType;
+		typedef NETIterator<BeanType>	BeanIterator;
+		typedef NETCIterator<BeanType>	BeanCIterator;
+
+
+	protected:
+		class Iterator : public NETIterator<T>
+		{
+			NE_DECLARE_CLASS_ONLY(Iterator, NETIterator<T>)
+
+		public:
+			Iterator(NETBinder<BeanIterator>& bean_iteratored);
+
+		public:
+			type_bool operator==(const This& rhs) const;			
+
+		public:
+			virtual const T& get() const;
+			virtual T& get();
+			virtual type_bool isEnd() const;
+			virtual type_result next(type_index step_for_next);
+			virtual type_result back(type_index step_for_back);
+			//		NELengthedObject:
+			virtual type_index getLength() const;
+			//		NEObject:
+			virtual void release();
+			virtual NEBinaryFileSaver& serialize(NEBinaryFileSaver& saver) const;
+			virtual NEBinaryFileLoader& serialize(NEBinaryFileLoader& loader);
+			virtual type_result isValid() const;
+
+		private:
+			NETBinder<BeanIterator> _proxied;
+		};
+		class CIterator : public NETCIterator<T>
+		{
+			NE_DECLARE_CLASS_ONLY(CIterator, NETCIterator<T>)
+
+		public:
+			CIterator(NETBinder<BeanCIterator>& bean_iteratored);
+
+		public:
+			type_bool operator==(const This& rhs) const;			
+
+		public:
+			virtual const T& get() const;
+			virtual type_bool isEnd() const;
+			virtual type_result next(type_index step_for_next);
+			virtual type_result back(type_index step_for_back);
+			//		NELengthedObject:
+			virtual type_index getLength() const;
+			//		NEObject:
+			virtual void release();
+			virtual NEBinaryFileSaver& serialize(NEBinaryFileSaver& saver) const;
+			virtual NEBinaryFileLoader& serialize(NEBinaryFileLoader& loader);
+			virtual type_result isValid() const;
+
+		private:
+			NETBinder<BeanCIterator> _proxyied;
+		};
 
 	public:
 		virtual NETBinder<NETCIterator<T>> getIterator(type_index index) const;
 		virtual NETBinder<NETIterator<T>> getIterator(type_index index);
 		virtual type_index insert(const NETCIterator<T>& where, const T& source);
-		virtual type_index insert(const NETCIterator<T>& where, NETCIterator<T> from, const NETCIterator<T>& end);
+		virtual type_index insert(const NETCIterator<T>& where, const NETCIterator<T>& from, const NETCIterator<T>& end);
 		virtual type_index remove(NETCIterator<T> from, const NETCIterator<T>& end);
 		virtual T& getElement(type_index index);
 		virtual const T& getElement(type_index index) const;
@@ -24,7 +87,6 @@ namespace NE
 		virtual type_result isValid() const;
 		virtual NEBinaryFileSaver& serialize(NEBinaryFileSaver& saver) const;
 		virtual NEBinaryFileLoader& serialize(NEBinaryFileLoader& loader);
-
 
 	private:
 		Collector<NETBinder<T>> _proxy;
