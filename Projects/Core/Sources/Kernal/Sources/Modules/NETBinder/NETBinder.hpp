8e89d0a57eb08b3a86c0add9d013a6d3fe0e5ba9
kniz <kniz@kniz.net>
2016-10-08 11:05:53 +0900

    + add comments for exceptions I found.

--- Projects/Core/Sources/Kernal/Sources/Modules/NETBinder/NETBinder.hpp
+++ Projects/Core/Sources/Kernal/Sources/Modules/NETBinder/NETBinder.hpp
@@ -51,8 +51,37 @@ namespace NE
 	type_result NETBinder<T>::_bind(const T& target)
 	{
 		if( ! &target) return RESULT_TYPE_WARNING;
-
+		
 		// TODO:
+		//	Remember. The sequence is the point:
+		//		Consider bind another target means "some object destroyed because 
+		//		of loosing".
+		//		Bind First, reduce refcount later. if you don't, there are some 
+		//		exceptions that instances were terminated cascade.
+		//
+		//		For example, there is a List.		
+		//
+		//			->	[A]	->	[B]	->	[C]
+		//
+		//		[A] means node of List, the arrow is binding.
+		//		In this case, we're trying to change the binder of A to C, not B.
+		//		
+		//			->	[A]	->	[C]
+		//						 ^
+		//					[B]	-|		
+		//
+		//		[B] will be destroyed. there is no binder who bind it.
+		//		Well, approximately this picture looks right. But how about [A]'s
+		//		binder detach binding to B first rather than binding to C?
+		//		
+		//			->	[A]		[C]							->	[A]		[C] 
+		//						 ^		--after a while-->				 ^
+		//					[B]	-|										 |- removing
+		//
+		//		As you can see above, B was removed simultaniously.
+		//		and because of removal of B, C has no other binder, will be destroyed 
+		//		too. After that, A tries to bind to C, but there is no C any more.
+
 	}
 
 	template <typename T>
