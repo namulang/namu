9e3dfc9011b8f40bd7a0126437c21389b7fbde72
kniz <kniz4we@gmail.com>
2017-07-23 13:16:43 +0900

    - [tester] modify testcase against changed concept of file-manipulator's APPENDABLE. + [tester] improve logger TC

--- projects/tester/tests/LoggerTest.hpp
+++ projects/tester/tests/LoggerTest.hpp
@@ -26,6 +26,31 @@ namespace NE
             NE_WARN("world!")
             NE_ERROR("this is not error. paradox?")
 
+
+            type_bool found = false;
+            Folder build(".", Folder::Option(false));            
+            while( found || ! build.next().isNull())
+                if(build.peek().getName().find(".log"))
+                    found = true;
+            NE_ASSERT_THEN_RETURN(found)
+
+            FileLogStream* fl = new FileLogStream("./LoggerTest.log");
+            NE_ASSERT_THEN_RETURN(logger.pushStream(fl));
+            NE_ASSERT_THEN_RETURN(logger.getStreamCount() < 2)
+            NE_ASSERT_THEN_RETURN(logger["FileLogStream"].isNull())
+            NE_ASSERT_THEN_RETURN( ! fl->isInitialized())
+
+            NE_ASSERT_THEN_RETURN(logger.dump("let's go on 2nd phase."));
+
+            BuildFeatures::Date date;
+            NE_INFO("today is %d-%d %d. %s.", date.getMonth(), date.getDay(), date.getYear(), "Nice start!")
+            NE_WARN("Fatal error! Fatal error! this PC should be infected to a virus immediatly!!!")
+            NE_ERROR("***backstabbing*** It's a joke. Carm down, my master.")
+
+            NE_ASSERT_THEN_RETURN(logger.release())
+            NE_ASSERT_THEN_RETURN(logger.getStreamCount() > 0)
+            NE_ASSERT_THEN_RETURN( ! logger["FileLogStream"].isNull())
+
             return "";
         }
     };
