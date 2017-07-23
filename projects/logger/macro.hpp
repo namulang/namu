3784668e9dc03d154a8c1b57da84786573496757
kniz <kniz4we@gmail.com>
2017-07-23 13:14:18 +0900

    - [logger] varidic arguments aren't working.

--- projects/logger/macro.hpp
+++ projects/logger/macro.hpp
@@ -4,12 +4,12 @@
 
 #define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
 
-#define NE_DUMP(level, msg) {                                               \
-        Logger::getInstance().dumpFormat("%s " NE_TAG " %s <%s::%s#%d> %s", \
-            PlatformAPI::createCurrentTime("%b %d %Y  %X").c_str(), level,  \
-            __FILENAME__, __func__, __LINE__, msg                           \
-        );                                                                  \
+#define NE_DUMP(level, msg, ...) {                                              \
+        Logger::getInstance().dumpFormat("%s " NE_TAG " %s <%s::%s#%d> " msg,   \
+            PlatformAPI::createCurrentTime("%b %d %Y  %X").c_str(), level,      \
+            __FILENAME__, __func__, __LINE__, ##__VA_ARGS__                     \
+        );                                                                      \
     }
-#define NE_INFO(msg) NE_DUMP("I", msg)
-#define NE_ERROR(msg) NE_DUMP("E", msg)
-#define NE_WARN(msg) NE_DUMP("W", msg)
+#define NE_INFO(msg, ...) NE_DUMP("I", msg, ##__VA_ARGS__)
+#define NE_ERROR(msg, ...) NE_DUMP("E", msg, ##__VA_ARGS__)
+#define NE_WARN(msg, ...) NE_DUMP("W", msg, ##__VA_ARGS__)
