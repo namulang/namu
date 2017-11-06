3ecf410d571220db6611cfc2a5a0bc7f6cc140a6
kniz <kniz4we@gmail.com>
2017-11-06 20:50:39 +0900

    + [class-design] WRD_DECL macro usage.

--- docs/protos/WorldPrototyping.cpp
+++ docs/protos/WorldPrototyping.cpp
@@ -95,73 +95,80 @@ namespace W
     };
 
 
-	class MyFirstWorldModule : public Object {
-		static const Result& _onInitializeMethods(List<Methods> tray) {
-			W_DECL_FUNC(String& addString(String& arg1, String/*by val is ok.*/ arg2))
-			//	RETURN_T=String& is by-ref and sharable.
-			/* So, Above macro will expand like this,
-			 *
-			 * RET_T = "String&"
-			 * NAME = "addString"
-			 * __VA_ARGS__ = rest of arguments.
-			 *
-			 * class addStringMethod : public Method {
-			 * 		W_DECL_CLASS(addStringMethod, Method)
-			 *
-			 * 		typedef Mashall<RET_T>::MashalledType ReturnType;
-			 * 		ReturnType _return;
-			 * 
-			 * public:
-			 *		addStringMethod() 
-			 *			// Mashall<RET_T>::RefindType is String
-			 *			// Mashall<RET_T>::MashalledType is Reference<String>
-			 *			// Mashall<RET_T>::isReference is 1
-			 *			// Mashall<RET_T>::isSharable is 1
- 			 *
-			 * 			// Occupiable = Reference<Occupiable> 은, rhs의 Sharable의 값이 Occupiable로 들어간다
-			 *			// 이는 Occupiable은 어떻게 해서라도 값vs값의 할당이 일어나야 하기 때문이다.
-			 *			// 물론 Reference<Occupiable> = Occupiable의 경우에는 Reference::operator=()가 된다.
-			 *			// 그래서 아래의 경우에는 원본이 Bind로 라이프사이클이 관리되고, 그것이 Sharable이든
-			 *			// Occupiable이든 값은 return으로 들어갈 수 있다.
-			 *			_return = Reference(Mashall<RET_T>::RefindType());
-			 *
-			 *			_arguments << Reference<MyFirstWorldModule>() // thisptr
-			 *			
-			 *			char* __VA_ARGS_를 split하고, loop를 돌면서 적합한 TClass<T>를 만들어서 arguments
-			 *			에 넣어야 한다. 이 것을 하는 도중에 T* arg,  T  *  args,  T  *args 같은 케이스들도
-			 *			문제없이 파싱할 수 있어야 한다.
-			 *
-			 *			어떻게?
-			 *
-			 *
-			 *			_fptr = &MyFirstWorldModule::addString;
-			 *
-			 *		}
-			 *
-			 *		String& (addStringMethod::*_fptr)(String&, __VA_ARGS_...);
-			 *
-			 * 		//	World에서 메소드를 실행하기 위한 가장 짦은 인터페이스이다.
-			 *		//	Native를 위한 인자의 가공이 시작된다.
-			 *		virtual Bind<Node> call(const Arguments& args) {
-			 *			if(Mashall<RET_T>::sharable)
-			 *				if(is_ref) // is_ref에는 Reference로 반환한경우도 포함이 되어야 한다.
-			 *					return _return = (me->*_fptr)(args[0].to<String>(), args[1].to.... 재귀 );
-			 *				else
-			 *					//	RET_T는 Sharable인데, 값으로 반환한 경우다. 불가피하게 퍼포먼스가 소모.
-			 *					return _return = (me->*_fptr)(args[0].to<String>(), args[1].to.... 재귀 ).clone();
-			 *		}
-			 * };
-			 */
+	//	RETURN_T=String& is by-ref and sharable.
+	/* So, Above macro will expand like this,
+	 *
+	 * RET_T = "String&"
+	 * NAME = "addString"
+	 * __VA_ARGS__ = rest of arguments.
+	 *
+	 * class addStringMethod : public Method {
+	 * 		W_DECL_CLASS(addStringMethod, Method)
+	 *
+	 * 		typedef Mashall<RET_T>::MashalledType ReturnType;
+	 * 		ReturnType _return;
+	 * 
+	 * public:
+	 *		addStringMethod() 
+	 *			// Mashall<RET_T>::RefindType is String
+	 *			// Mashall<RET_T>::MashalledType is Reference<String>
+	 *			// Mashall<RET_T>::isReference is 1
+	 *			// Mashall<RET_T>::isSharable is 1
+	 *
+	 * 			// Occupiable = Reference<Occupiable> 은, rhs의 Sharable의 값이 Occupiable로 들어간다
+	 *			// 이는 Occupiable은 어떻게 해서라도 값vs값의 할당이 일어나야 하기 때문이다.
+	 *			// 물론 Reference<Occupiable> = Occupiable의 경우에는 Reference::operator=()가 된다.
+	 *			// 그래서 아래의 경우에는 원본이 Bind로 라이프사이클이 관리되고, 그것이 Sharable이든
+	 *			// Occupiable이든 값은 return으로 들어갈 수 있다.
+	 *			_return = Reference(Mashall<RET_T>::RefindType());
+	 *
+	 *			_arguments << Reference<MyFirstWorldModule>() // thisptr
+	 *			
+	 *			char* __VA_ARGS_를 split하고, loop를 돌면서 적합한 TClass<T>를 만들어서 arguments
+	 *			에 넣어야 한다. 이 것을 하는 도중에 T* arg,  T  *  args,  T  *args 같은 케이스들도
+	 *			문제없이 파싱할 수 있어야 한다.
+	 *
+	 *			어떻게?
+	 *
+	 *
+	 *			_fptr = &MyFirstWorldModule::addString;
+	 *
+	 *		}
+	 *
+	 *		String& (addStringMethod::*_fptr)(String&, __VA_ARGS_...);
+	 *
+	 * 		//	World에서 메소드를 실행하기 위한 가장 짦은 인터페이스이다.
+	 *		//	Native를 위한 인자의 가공이 시작된다.
+	 *		virtual Bind<Node> call(const Arguments& args) {
+	 *			if(Mashall<RET_T>::sharable)
+	 *				if(is_ref) // is_ref에는 Reference로 반환한경우도 포함이 되어야 한다.
+	 *					return _return = (me->*_fptr)(args[0].to<String>(), args[1].to.... 재귀 );
+	 *				else
+	 *					//	RET_T는 Sharable인데, 값으로 반환한 경우다. 불가피하게 퍼포먼스가 소모.
+	 *					return _return = (me->*_fptr)(args[0].to<String>(), args[1].to.... 재귀 ).clone();
+	 *		}
+	 * };
+	 */
+	#define WRD_DECL_2(T, S)	.......
+	#define WRD_DECL_3(T, S, M)	\
+		WRD_DECL_2(T,S)	\
+	protected:	\
+		static const Result& _onInitializeMethods(List<Method>& tray) {	\
+			FOR_EACH()
 		}
 
-		//	사용자는 이걸 따로 만들어야 한다.
-		//	반환값은 동일하게
-		String& addString(String& arg1, String arg2/*값 복사가 1번더 일어난다*/) {
-			static String added;
-			added = arg1 + arg2;
-			return added;
-		}
-		Reference addString(String& arg1, String& arg2) {
+	//	사용법1:
+	//		.HPP에서
+	class MyFirstWorldModule : public Object {
+		WRD_DECL_ONLY(MyFirstWorldModule, Object, 
+			String, addString, (String&, String&)
+			//	조건1. ,로 구분한다.
+			//	조건2. 인자명 없다.
+		) // static onInitializeMethods() 구현된다.
+			//	조건3. 시작과 끝 ()를 넣는다.
+
+	public:
+		String addString(String& arg1, String& arg2) {
 			//	Reference의 사용방법1 : 
 			Reference<String> ret = new String(arg1 + arg2);
 			//	방법2:
@@ -175,6 +182,22 @@ namespace W
 			
 			return ret;
 		}
-	public:
 	};
+	//	.CPP 구현 없음.
+
+	//	사용법2:
+	//	.HPP에서
+	class MyFirstWorldModule : public Object {
+		WRD_DECL(MyFirstWorldModule, Object) // static onInitializeMethods(); 선언 된다.
+
+	public:
+		String addString(String& arg1, String& arg2); // 여기서 정의해도 상관없다.
+	};
+	//	.CPP에서
+	WRD_DEF(MyFirstWorldModule, 
+		(String, addString, String&, String & const) // static onInitializeMethods() 구현들어간다.
+	)
+	String MyFirstWorldModule::addString(String& arg1, String & const arg2) {
+		... dosomething ...
+	}
 }
