06ff0876f6b3f0abcaf993336bce25765a95a6df
kniz <kniz@kniz.net>
2018-03-23 00:21:16 +0900

    + [proto] add inline returning methods

--- docs/protos/worldfrw/pretype component.cpp
+++ docs/protos/worldfrw/pretype component.cpp
@@ -3,4 +3,17 @@ class Result {
 	wint err(....);
 	wint info(....);
 	wint dbg(..);
+
+	template <typename T>
+	T returns() {
+		return T();
+	}
+	template <typename T>
+	T returns(T value) {
+		return value;
+	}
+	template <typename T>
+	T& returnsNullref() {
+		return TNuller<T>::ref;
+	}
 };
