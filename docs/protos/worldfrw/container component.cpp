ba4b007ed1607e2091b4b5cfc95aa10053066f2a
kniz <kniz@kniz.net>
2018-03-31 17:28:13 +0900

    + [proto] modulize Containable and Iteratable.
    both classes are interface. but I added normal method in them.
    this can causes issue when a class uses multiple inheritance with above
    two classes. but normaly, it won't happen.

--- docs/protos/worldfrw/container component.cpp
+++ docs/protos/worldfrw/container component.cpp
@@ -1,15 +1,91 @@
+class Containable {
+	Node& operator[](windex n);
+	const Node& operator[](windex n) const;
+	operator+
+	operator-
+	operator<<
+	operator>>
+	virtual Result& insert(const Iterator& pos, const Node& it) = 0;
+	Result& insert(windex n, const Node& it);
+	Result& insert(const Iterator& pos, const Iterator& its_start, const Iterator& its_end);
+	Result& insert(windex n, const Iterator& its_start, const Iterator& its_end);
+	Result& insert(const Iterator& pos, <<<iterator from stl>>>);
+	Result& insert(windex n, <<<iterator from stl>>>);
+	template <typename... Ts>
+	Result& insert(const Iterator& pos, Ts... args);
+	template <typename... Ts>
+	Result& insert(windex n, Ts... args) {
+		return insert(getIterator(n), args...);
+	}
+	windex push(const Node& it);
+	windex push(const Iterator& its_start, const Iterator& its_end);
+	windex push(<<<iterator from stl>>>)
+	template <typename... Ts>
+	windex push(Ts... args) {
+		return insert(getTail(), args...);
+	}
+	Result& enq(const Node& it);
+	Result& enq(const Iterator& its_start, const Iterator& its_end);
+	Result& enq(<<<iterator from stl>>>);
+	template <typename... Ts>
+	Result& enq(Ts... args) {
+	}
+	
+	virtual Result& remove(const Iterator& start, const Iterator& end) = 0;
+	Result& remove(const Node& it);
+	Result& remove(windex n);
+	template <typename T>
+	Result& remove(const Iterator& pos, Ts... args) {
+	}
+	template <typename T>
+	Result& remove(windex n, Ts... args) {
+	}
+	windex pop();
+	Result& deq();
+	
+	//	사용자가 Iteration을 상속할 수 있도록 하기 위한 메소드.
+	virtual TStrong<Iteration> _onCreateIteration(windex n) = 0;
+
+	virtual wcount getSize() const = 0;
+
+	virtual Iterator getIterator(windex n) = 0;
+	virtual CIterator getIterator(windex n) const = 0;
+	Iterator getHead();
+	CIterator getHead() const;
+	Iterator getTail();
+	CIterator getTail() const;
+
+	template <typename T>
+	Result& each(T lambda);
+	Result& each(Method& lambda);
+
+	virtual const Class& getTrait() const = 0;
+};
+
+class Iteratable {
+	virtual Result& move(wcount step) = 0;
+	This& operator++(int);
+	This& operator++();
+	This& operator--(int);
+	This& operator--();
+	Result& next();
+	Result& prev();
+
+	virtual Node& get() = 0;
+	virtual const Node& get() = 0;
+	
+	operator wbool() const;
+	virtual wbool isTail() const = 0;
+	virtual wbool isHead() const = 0;
+};
+
 class Iteration : public ConstableInstance {
-	virtual result& move(wcount step) = 0;
-	virtual Node& getNode() = 0;
-	virtual const Node& getNode() = 0;
-	virtual wbool isEnd() const = 0;
 	Container& getOwner() {
 		return _getOwner();
 	}
 	const Container& getOwner() const {
 		return _getOwner();
 	}
-	virtual Container& _getOwner() = 0;
 	const Container& _getOwner() const {
 		This& unconst = const_cast<This&>(*this);
 		return unconst._getOwner();
@@ -32,12 +108,6 @@ class Iterator : public OccupiableObject {
 //	Object여야 World에서 다룰 수 있게 된다.
 	friend class Container;
 
-	operator wbool() const;
-	This& operator++(int);
-	This& operator++();
-	This& operator--(int);
-	This& operator--();
-
 	Result& move(wcount step) {
 		WRD_IS_NULL(_iteration)
 
@@ -56,7 +126,6 @@ class Iterator : public OccupiableObject {
 		
 		return _iteration.getNode();
 	}
-	wbool isEnd() const;
 	Container& getOwner() {
 		WRD_IS_THIS(Container)
 		WRD_IS_CONST(Nuller<Container>::ref)
@@ -92,87 +161,26 @@ class TIterator<T, true> : public Iterator {
 	const T& get() { return static_cast<const T&>(Super::get()); }
 };
 
-class Containable {
-	virtual Result& insert(const Iterator& pos, const Node& it) = 0;
-	virtual Result& remove(const Iterator& pos, const Node& trg) = 0;
-	virtual Iterator getIterator(windex n) = 0;
-	virtual CIterator getIterator(windex n) const = 0;
-	virtual wcount getSize() const = 0;
-};
 class Container : public Object, public Containable {
 	Container() : Super(), _trait(Node::getStaticClass()) {
 	}
 	Container(const Class& trait);
 
-	Node& operator[](windex n);
-	const Node& operator[](windex n) const;
-	operator+
-	operator-
-	operator<<
-	operator>>
-	//	World에서 Container로 사용해야 하므로 Object로부터 상속받아야 한다.
-	Result& insert(windex n, const Node& it);
-	Result& insert(const Iterator& pos, const Iterator& its_start, const Iterator& its_end);
-	Result& insert(windex n, const Iterator& its_start, const Iterator& its_end);
-	Result& insert(const Iterator& pos, <<<iterator from stl>>>);
-	Result& insert(windex n, <<<iterator from stl>>>);
-	template <typename... Ts>
-	Result& insert(const Iterator& pos, Ts... args);
-	template <typename... Ts>
-	Result& insert(windex n, Ts... args) {
-		return insert(getIterator(n), args...);
-	}
-	windex push(const Node& it);
-	windex push(const Iterator& its_start, const Iterator& its_end);
-	windex push(<<<iterator from stl>>>)
-	template <typename... Ts>
-	windex push(Ts... args) {
-		return insert(getTail(), args...);
-	}
-	Result& enq(const Node& it);
-	Result& enq(const Iterator& its_start, const Iterator& its_end);
-	Result& enq(<<<iterator from stl>>>);
-	template <typename... Ts>
-	Result& enq(Ts... args) {
-	}
-
-	Result& remove(const Node& it);
-	Result& remove(windex n);
-	Result& remove(const Iterator& start, const Iterator& end);
-	template <typename T>
-	Result& remove(const Iterator& pos, Ts... args) {
-	}
-	template <typename T>
-	Result& remove(windex n, Ts... args) {
-	}
-	windex pop();
-	Result& deq();
-
 	virtual Iterator getIterator(windex n) {
 		Iterator ret;
-		ret._setIteration(_onTakeIteration(n));
+		ret._setIteration(_onCreateInteration(n));
 		return ret;
 	}
 	virtual CIterator getIterator(windex n) const {
 		CIterator ret;
-		ret._setIteration(_onTakeIteration(n));
+		ret._setIteration(_onCreateIteration(n));
 		return ret;
 	}
-	virtual TStrong<Iteration> _onTakeIteration(windex n) = 0;
-	Iterator getHead();
-	CIterator getHead() const;
-	Iterator getTail();
-	CIterator getTail() const;
-
-	template <typename T>
-	Result& each(T lambda);
-	Result& each(Method& lambda);
-
 	virtual wcount getSize() const { return _size; }
 	Result& _setSize(wcount newone) { _size = newone; }
 	wcount _size;
-
-	const Class& getTrait() const {
+db
+	virtual const Class& getTrait() const {
 	//	삽입가능한 Class를 나타낸다.
 		return *_trait;
 	}
