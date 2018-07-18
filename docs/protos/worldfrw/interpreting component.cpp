c1821f9335d0c2965598c3a386daced5a81035eb
kniz <kniz@kniz.net>
2018-07-19 00:15:22 +0900

    + [prototyping] make class Origin be far specific.

--- docs/protos/worldfrw/interpreting component.cpp
+++ docs/protos/worldfrw/interpreting component.cpp
@@ -1,9 +1,31 @@
 //	Interpreting에 의해서 나오게 되는 모든 산물들.
-class Source : public CompositNode {
-	struct SrcPos {
-		int col;
-		int row;
+class Origin : public CompositNode { //	Origin은 또다른 Origin들을 composition으로 가질 수 있다.
+	class Pos : public Node { // Pos는 visible해야 한다. IDE에서도 사용해야 하니까.
+		Pos(wint row, wint col);
+
+		This& operator=(const This& rhs);
+		wbool operator==(const This& rhs) const;
+		wbool operator!=(const This& rhs) const;
+		wbool operator<(const This& rhs);
+		wbool operator<=(const This& rhs);
+		wbool operator>(const This& rhs);
+		wbool operator>=(const This& rhs);
+
+		wint getRow() const { return _row; }
+		wint getCol() const { return _col; }
+
+		wint _row;
+		wint _col;
 	};
-	SrcPos _start;
-	SrcPos _end;
+
+	Origin(const Pos& start = Pos(), const Pos& end = Pos());
+
+	const Pos& getStart() const;
+	const Pos& getEnd() const;
+	const Origin& getOwner() const { return _owner; }
+	Origin& getOwner() { return _owner; }
+
+	Pos _start;
+	Pos _end;
+	Origin& _owner;
 };
