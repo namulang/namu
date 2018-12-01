9abb8614cf10f2e12d6471bfd7c1479e33693850
kniz <kniz.private@gmail.com>
2018-12-01 15:36:32 +0900

    + [pref] put namespace on it.

--- projects/preferencer/Preferencer.cpp
+++ projects/preferencer/Preferencer.cpp
@@ -3,43 +3,46 @@
 
 namespace wrd
 {
-    #define THIS Preferencer
-    wbool THIS::parse(const std::string& contents) {
-        CharStream stream(contents, 1, 1);
-        PRParserTokenManager token_manager(&stream);
-        PRParser parser(&token_manager);
+	namespace pref
+	{
+		#define THIS Preferencer
+		wbool THIS::parse(const std::string& contents) {
+			CharStream stream(contents, 1, 1);
+			PRParserTokenManager token_manager(&stream);
+			PRParser parser(&token_manager);
 
-        WRD_INFO("parser.configure_file()");
-        parser.setPreferencer(this);
-        parser.setErrorHandler(new PRParserHandler());
-        parser.configure_file();
-        return true;
-    }
-    wbool THIS::onAddClass(const std::string& name) {
-        if( ! getClass(name).isNull())
-        {
-            WRD_ERROR("%s is duplicated.", name.c_str());
-            return false;
-        }
+			WRD_INFO("parser.configure_file()");
+			parser.setPreferencer(this);
+			parser.setErrorHandler(new PRParserHandler());
+			parser.configure_file();
+			return true;
+		}
+		wbool THIS::onAddClass(const std::string& name) {
+			if( ! getClass(name).isNull())
+			{
+				WRD_ERROR("%s is duplicated.", name.c_str());
+				return false;
+			}
 
-        _classes[name] = ClassType(name);
-        return true;
-    }
-    wbool THIS::onAddMember(const std::string& class_name, const std::string& key, const std::string& value) {
-        if(getClass(class_name).isNull())
-        {
-            WRD_ERROR("class %s is not defined.", class_name.c_str());
-            return false;
-        }
-       
-        ClassType& klass = _classes[class_name];           
-        if( ! klass.getMember(key).isNull())
-        {
-            WRD_ERROR("memberdata %s is duplicated.", key.c_str());
-            return false;
-        }
-       
-        klass._members[key] = MemberType(key, value);
-        return true;
-    }
+			_classes[name] = ClassType(name);
+			return true;
+		}
+		wbool THIS::onAddMember(const std::string& class_name, const std::string& key, const std::string& value) {
+			if(getClass(class_name).isNull())
+			{
+				WRD_ERROR("class %s is not defined.", class_name.c_str());
+				return false;
+			}
+		   
+			ClassType& klass = _classes[class_name];           
+			if( ! klass.getMember(key).isNull())
+			{
+				WRD_ERROR("memberdata %s is duplicated.", key.c_str());
+				return false;
+			}
+		   
+			klass._members[key] = MemberType(key, value);
+			return true;
+		}
+	}
 }
--- projects/preferencer/Preferencer.hpp
+++ projects/preferencer/Preferencer.hpp
@@ -4,141 +4,144 @@
 
 namespace wrd
 {
-    namespace
-    {
-        template <typename T>
-        T& nullreference() {
-            T* nulled = 0;
-            return *nulled;
-        }
-    }
+	namespace pref
+	{
+		namespace
+		{
+			template <typename T>
+			T& nullreference() {
+				T* nulled = 0;
+				return *nulled;
+			}
+		}
 
-    class Object
-    {
-    public:
-        virtual wbool release() = 0;
-        virtual Object& clone() const = 0;
-        wbool isNull() const { return ! this; }
-    };
-    class Type : public Object
-    {
-    public:
-        Type() {}
-        Type(const std::string& key) : _key(key) {}
-        const std::string& getKey() const { return _key; }
-        virtual wbool release() { 
-            _key.clear(); 
-            return true;
-        }
-        Object& clone() const { return *(new Type(*this)); }
+		class Object
+		{
+		public:
+			virtual wbool release() = 0;
+			virtual Object& clone() const = 0;
+			wbool isNull() const { return ! this; }
+		};
+		class Type : public Object
+		{
+		public:
+			Type() {}
+			Type(const std::string& key) : _key(key) {}
+			const std::string& getKey() const { return _key; }
+			virtual wbool release() { 
+				_key.clear(); 
+				return true;
+			}
+			Object& clone() const { return *(new Type(*this)); }
 
-    private:
-        std::string _key;
-    };
-    class MemberType : public Type
-    {
-    public:
-        MemberType() : Type() {}
-        MemberType(const std::string& key, const std::string& value) : Type(key), _value(value) {}
-        const std::string& getValue() const { return _value; }
-        virtual wint toInt() const { return std::stoi(_value); }
-        virtual wbool toBoolean() const {
-            return _toLowercase(_value) == "true" || toInt();
-        }
-        virtual wfloat toFloat() const { return std::stof(_value, 0); }
-        virtual wchar toChar() const { return _value[0]; }
-        virtual std::string toString() const { return _value; }
-        virtual wbool release() {
-            _value.clear();
-            return Type::release();
-        }
-        virtual Object& clone() const { return *(new MemberType(*this)); }
+		private:
+			std::string _key;
+		};
+		class MemberType : public Type
+		{
+		public:
+			MemberType() : Type() {}
+			MemberType(const std::string& key, const std::string& value) : Type(key), _value(value) {}
+			const std::string& getValue() const { return _value; }
+			virtual wint toInt() const { return std::stoi(_value); }
+			virtual wbool toBoolean() const {
+				return _toLowercase(_value) == "true" || toInt();
+			}
+			virtual wfloat toFloat() const { return std::stof(_value, 0); }
+			virtual wchar toChar() const { return _value[0]; }
+			virtual std::string toString() const { return _value; }
+			virtual wbool release() {
+				_value.clear();
+				return Type::release();
+			}
+			virtual Object& clone() const { return *(new MemberType(*this)); }
 
-    private:
-        static std::string _toLowercase(const std::string& str) {
-            std::string to_return = str;
-            std::transform(str.begin(), str.end(), to_return.begin(), ::tolower);
-            return to_return;
-        }
+		private:
+			static std::string _toLowercase(const std::string& str) {
+				std::string to_return = str;
+				std::transform(str.begin(), str.end(), to_return.begin(), ::tolower);
+				return to_return;
+			}
 
-    private:
-        std::string _key;
-        std::string _value;
-    };
+		private:
+			std::string _key;
+			std::string _value;
+		};
 
-    class ClassType : public Type
-    {
-    public:
-        friend class Preferencer;
+		class ClassType : public Type
+		{
+		public:
+			friend class Preferencer;
 
-    public:
-        ClassType() {}
-        ClassType(const std::string& key) : Type(key) {}
+		public:
+			ClassType() {}
+			ClassType(const std::string& key) : Type(key) {}
 
-    public:
-        wbool push(const MemberType& member) { 
-            _members[member.getKey()] = member;
-            
-            return true;
-        }
-        const MemberType& operator[](const std::string& key) const { return getMember(key); }
-        const MemberType& getMember(const std::string& key) const {
-            if(isNull())
-                return nullreference<MemberType>();
-            Members::const_iterator e = _members.find(key);
-            if(e == _members.end())
-                return nullreference<MemberType>();
+		public:
+			wbool push(const MemberType& member) { 
+				_members[member.getKey()] = member;
+				
+				return true;
+			}
+			const MemberType& operator[](const std::string& key) const { return getMember(key); }
+			const MemberType& getMember(const std::string& key) const {
+				if(isNull())
+					return nullreference<MemberType>();
+				Members::const_iterator e = _members.find(key);
+				if(e == _members.end())
+					return nullreference<MemberType>();
 
-            return e->second;
-        }
-        virtual wbool release() {
-            _members.clear();
-            
-            return true;
-        }
-        virtual Object& clone() const { return *(new ClassType(*this)); }
+				return e->second;
+			}
+			virtual wbool release() {
+				_members.clear();
+				
+				return true;
+			}
+			virtual Object& clone() const { return *(new ClassType(*this)); }
 
-    private:
-        typedef std::map<std::string, MemberType> Members;
-        Members _members;
-    };
+		private:
+			typedef std::map<std::string, MemberType> Members;
+			Members _members;
+		};
 
-    class Preferencer : public Object
-    {
-    public:
-        const ClassType& operator[](const std::string& key) const { return getClass(key); }
-        const ClassType& getClass(const std::string& key) const {
-            Classes::const_iterator e = _classes.find(key);
-            if(e == _classes.end())
-                return nullreference<ClassType>();
+		class Preferencer : public Object
+		{
+		public:
+			const ClassType& operator[](const std::string& key) const { return getClass(key); }
+			const ClassType& getClass(const std::string& key) const {
+				Classes::const_iterator e = _classes.find(key);
+				if(e == _classes.end())
+					return nullreference<ClassType>();
 
-            return e->second;
-        }
-        virtual wbool parse(const std::string& contents);
-        virtual wbool parse(const File& file) {
-            //  pre:
-            if(file.isNull())
-            {
-                WRD_WARN("file is null")
-                return false;
-            }
+				return e->second;
+			}
+			virtual wbool parse(const std::string& contents);
+			virtual wbool parse(const File& file) {
+				//  pre:
+				if(file.isNull())
+				{
+					WRD_WARN("file is null")
+					return false;
+				}
 
-            WRD_INFO("parse(%s)", file.getPath().c_str());
-            AsciiStream stream(file);
-            stream.initialize();
-            
-            return parse(stream.readWhole());
-        }
-        wbool release() {
-            _classes.clear();
-            return true;
-        }
-        virtual Object& clone() const { return *(new Preferencer(*this)); }
-        virtual wbool onAddClass(const std::string& name);
-        virtual wbool onAddMember(const std::string& class_name, const std::string& key, const std::string& value);
+				WRD_INFO("parse(%s)", file.getPath().c_str());
+				AsciiStream stream(file);
+				stream.initialize();
+				
+				return parse(stream.readWhole());
+			}
+			wbool release() {
+				_classes.clear();
+				return true;
+			}
+			virtual Object& clone() const { return *(new Preferencer(*this)); }
+			virtual wbool onAddClass(const std::string& name);
+			virtual wbool onAddMember(const std::string& class_name, const std::string& key, const std::string& value);
 
-    private:
-        typedef std::map<std::string, ClassType> Classes;
-        Classes _classes;
-    };
+		private:
+			typedef std::map<std::string, ClassType> Classes;
+			Classes _classes;
+		};
+	}
 }
