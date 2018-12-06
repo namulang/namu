28303370c8027320f3e81daa023180bd8734fb7d
kniz <kniz.private@gmail.com>
2018-12-06 21:54:19 +0900

    + [world] Add Composit class.

--- projects/world/bases/Composit.hpp
+++ projects/world/bases/Composit.hpp
@@ -1,11 +1,11 @@
 #pragma once
 
 #include "Node.hpp"
+#include "../binds.hpp"
 
 namespace wrd
 {
-    class Result;
-    class Container;
+	class Chain;
     class Composit : public Node
     {
     public: //    Node:
@@ -19,6 +19,6 @@ namespace wrd
     private:
         //    _nodes can't be declared with protected accessor:
         //        if we do that, module developers can use _nodes and remove or insert some Node at runtime.
-        Chain _nodes; // of Container.
+        TStrong<Chain> _nodes; // of Container.
     };
 }
