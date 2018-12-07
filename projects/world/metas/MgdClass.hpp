35203fbf9528f1b8d09ef855cab44304c023b4e2
kniz <kniz.private@gmail.com>
2018-12-07 22:52:53 +0900

    + [world] add MgdClass.

---
+++ projects/world/metas/MgdClass.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "TConcreteClass.inl"
+#include "ObjectClass.hpp"
+
+namespace wrd
+{
+	class MgdObject;
+    class MgdClass : public TConcreteClass<MgdObject, ObjectClass>
+    {
+    public:
+        virtual const Origin& getOrigin() const;
+        virtual TStrong<Instance> instance() const;
+
+    private:
+        TStrong<Origin> _org;
+    };
+}
