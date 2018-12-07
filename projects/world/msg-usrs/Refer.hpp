ed1fe14cec42d619bd8f6d5f22a63be8515d8985
kniz <kniz.private@gmail.com>
2018-12-07 22:45:05 +0900

    + [world] add Refer.

---
+++ projects/world/msg-usrs/Refer.hpp
@@ -0,0 +1,64 @@
+#pragma once
+
+#include "../binds/TBindable.inl"
+#include "../bases/Node.hpp"
+
+namespace wrd
+{
+	class Refer : public Node, public TBindable<Node>
+	{
+	public:
+		//TODO: uncomment this. Refer(const Class& cls = Node::getStaticClass(), wbool want_const = false);
+		Refer(Node& it);
+		Refer(const Node& it);
+		Refer(const Refer& it);
+		Refer(Refer& it);
+
+	public:
+		wbool operator==(const Refer& rhs) const;
+		wbool operator!=(const Refer& rhs) const;
+		Refer& operator=(const Refer& it);
+		Refer& operator=(Refer& it);
+		Refer& operator=(Node& it);
+		Refer& operator=(const Node& it);
+
+	public:	//	Node:
+		virtual Refer use(Msg& msg) const; 
+		virtual Refer use(Msg& msg); 
+		virtual const Origin& getOrigin() const; 
+		virtual wbool isConst() const; 
+		virtual const Container& getNodes() const;
+
+	public:	//	Bindable:
+		using TBindable<Node>::bind;
+		Result& bind(Refer& it);
+		Result& bind(const Refer& it);
+		virtual Result& bind(Instance& it);
+		Result& bind(const Instance& it);
+		virtual wbool doesBind() const;
+		Result& unbind();
+
+	public:	//	Thing:
+		virtual Refer to(const Class& cls);
+		virtual Result& assign(const Thing& it);
+		virtual const Class& getClass() const;
+
+	protected:
+		virtual Instance& _get();
+		//	TODO: const cast.
+		//		Refer는 알다시피 World의 const를 구현하는 핵심 클래스다.
+		//		Node& n = Refer(const T&..);
+		//		n.to<T>(); // REJECT. return Refer(Null)
+		//		T& cast = n.down<T>(); // REJECT. cast.isNull() == true.
+		//		const T& cast = n.down<const T>(); // OK.
+		//		Refer& cast1 = n.to<const T>(); // OK. cast1->isNull() != true
+		//	위의 코드가 가능하도록 해야 한다.
+		virtual Node& _cast(const Class& cls);
+		virtual const Node& _cast(const Class& cls) const;
+
+	private:
+		wbool _is_const;
+		Strong _bean;
+		const Class& _cls;
+	};
+}
