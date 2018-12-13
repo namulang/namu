198596ee03737fc113b54fec5674426398eca69f
kniz <kniz.private@gmail.com>
2018-12-13 22:32:36 +0900

    + [wrd] add World class at entrypoint.

--- projects/world/world.hpp
+++ projects/world/world.hpp
@@ -2,7 +2,28 @@
 
 #include "./wrd-commons.hpp"
 #include "./bases.hpp"
+#include "./memory.hpp"
 #include "./binds.hpp"
 #include "./metas.hpp"
 #include "./msg-usrs.hpp"
 #include "./pretypes.hpp"
+
+namespace wrd
+{
+    class World : public Manager
+    {
+        typedef World This;
+
+    private:
+        World();
+
+    public:
+        virtual ~World();
+
+    public:
+        static This& get() {
+            static This inner;
+            return inner;
+        }
+    };
+}
