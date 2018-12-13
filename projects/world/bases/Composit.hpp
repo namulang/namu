6da93f1def93988d2cd59537d1e4c2dfe543ee51
kniz <kniz.private@gmail.com>
2018-12-13 22:30:59 +0900

    + [wrd] implement init and release.

--- projects/world/bases/Composit.hpp
+++ projects/world/bases/Composit.hpp
@@ -9,10 +9,21 @@ namespace wrd
     class Composit : public Node
     {
     public: //    Node:
-        //        getMember(); 는 공개하지 않는다:
+        //        getNodes(); 는 공개하지 않는다:
         //        사용자는 Container채로 받게 되면 밖에서 remove, insert를 할 수 있게 된다.
         virtual const Container& getNodes() const;
-
+        //  State:
+        virtual Result& init()
+        {
+            WRD_IS_SUPER(init())
+            return _initNodes();
+        }
+        //  Thing:
+        virtual Result& release()
+        {
+            _nodes.release();
+            return Super::release();
+        }
     private: //    Node:
         virtual Result& _initNodes();
 
