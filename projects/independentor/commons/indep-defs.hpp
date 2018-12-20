a666102abcfe4f440df81cdb5821af637a8a0a56
kniz <kniz.private@gmail.com>
2018-12-20 22:21:51 +0900

    + [indep] split macros into seperated files.
    WRD_INHERIT, WRD_UNCONST, WRD_LAZY_METHOD.

--- projects/independentor/commons/indep-defs.hpp
+++ projects/independentor/commons/indep-defs.hpp
@@ -70,25 +70,3 @@
     #define fgetts fgets
     #define fputts fputs
 #endif
-
-//	macros:
-#define WRD_UNCONST_0()			WRD_UNCONST_1(unconst)
-#define WRD_UNCONST_1(value)	This* value = const_cast<This*>(this);
-#define WRD_UNCONST(...)		WRD_OVERLOAD(WRD_UNCONST, __VA_ARGS__)
-
-///	the macro for definining lazymethod which will be initialized when it's called.
-//	this's uses static variable inside of method.
-#define _LAZY_METHOD(ReturnType, Name, isConst, StaticType, initValue)	\
-	isConst ReturnType& Name() isConst { \
-		static StaticType inner initValue; \
-		return inner; \
-	}
-#define WRD_LAZY_METHOD_5(ReturnType, Name, isConst, StaticType, initValue) \
-	_LAZY_METHOD(ReturnType, Name, isConst, StaticType, = initValue)
-#define WRD_LAZY_METHOD_4(ReturnType, Name, isConst, StaticType) \
-    _LAZY_METHOD(ReturnType, Name, isConst, StaticType, WRD_VOID)
-#define WRD_LAZY_METHOD_3(StaticType, Name, isConst)  \
-	_LAZY_METHOD(StaticType, Name, isConst, StaticType, WRD_VOID)
-#define WRD_LAZY_METHOD_2(StaticType, Name)	\
-	_LAZY_METHOD(StaticType, Name, WRD_SPACE, StaticType, WRD_VOID)
-#define WRD_LAZY_METHOD(...)	WRD_OVERLOAD(WRD_LAZY_METHOD, __VA_ARGS__)
--- projects/independentor/macros/Helpers.hpp
+++ projects/independentor/macros/Helpers.hpp
@@ -2,7 +2,12 @@
 
 //		helpers:
 #define WRD_COMMA ,
-#define WRD_VOID
+#define WRD_VOID()
 #define WRD_SPACE WRD_VOID WRD_VOID
 #define WRD_CONSUME_ARGS(...)
 #define WRD_UNWRAP(...) __VA_ARGS__
+
+#define WRD_UNCONST_0()			WRD_UNCONST_1(unconst)
+#define WRD_UNCONST_1(value)	This* value = const_cast<This*>(this);
+#define WRD_UNCONST(...)		WRD_OVERLOAD(WRD_UNCONST, __VA_ARGS__)
+
