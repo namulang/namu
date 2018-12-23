4495bde108bea377fd447d60ad943bbab435d2f1
kniz <kniz.private@gmail.com>
2018-12-23 15:16:16 +0900

    + [wrd] add compiler component and Origin class.

---
+++ projects/world/compiler.hpp
@@ -0,0 +1,3 @@
+#pragma once
+
+#include "./compiler/Origin.hpp"
---
+++ projects/world/compiler/Origin.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace wrd
+{
+      class Origin : public Thing // ?
+      {      WRD_CLASS(Origin, Thing)
+      public:
+      };
+}
