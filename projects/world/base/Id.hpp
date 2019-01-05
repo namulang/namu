c1c5c973f5843e668162ef0d609f3e071e7c7f3a
kniz <kniz.private@gmail.com>
2019-01-06 00:45:02 +0900

    + [wrd] add serial value onto Id.

--- projects/world/base/Id.hpp
+++ projects/world/base/Id.hpp
@@ -8,13 +8,13 @@ namespace wrd
     {
 		struct SeperatedId
 		{
-		    widx cls; //    í´ëì¤ì ì¢ë¥
-		    widx idx; //    í´ë¹ í´ëì¤ ì¸ì¤í´ì¤ ë²¡í°ì ëªë²ì§¸ì¸ê°.typeê³¼ indexë¥¼ ì¡°í©íë©´ ì´ëìë  ê°ì²´ì ì ê·¼í  ì ìë¤.
+		    widx cls:21;
+		    widx idx:21;
+			wcnt serial:21;
 		} sep;
 		wint64 num;
 		
 		Id(wint64 it = WRD_INDEX_ERROR) : num(it) {}
-		Id(widx itscls, widx itsidx) : sep {itscls, itsidx} {}
-
+		Id(widx itscls, widx itsidx, wcnt itsserial) : sep {itscls, itsidx, itsserial} {}
     };
 }
