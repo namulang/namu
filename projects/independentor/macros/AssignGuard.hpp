bf1fa44a47309d78095338aafbecfc81007035a1
kniz <kniz.private@gmail.com>
2019-01-06 00:46:08 +0900

    + [indep] add variant version of ASSIGN_GUARD.

--- projects/independentor/macros/AssignGuard.hpp
+++ projects/independentor/macros/AssignGuard.hpp
@@ -2,6 +2,7 @@
 
 #include "Overload.hpp"
 
-#define WRD_ASSIGN_GUARD_0()	AGN_GUARD_1(rhs)
-#define WRD_ASSIGN_GUARD_1(rhs)	if(this == &rhs) return *this;
-#define WRD_ASSIGN_GUARD(...)	WRD_OVERLOAD(WRD_ASSIGN_GUARD, __VA_ARGS__)
+#define WRD_ASSIGN_GUARD_0()			WRD_ASSIGN_GUARD_1(rhs)
+#define WRD_ASSIGN_GUARD_1(rhs)			WRD_ASSIGN_GUARD_2(rhs, *this)
+#define WRD_ASSIGN_GUARD_2(rhs, ret)	if(this == &rhs) return ret;
+#define WRD_ASSIGN_GUARD(...)			WRD_OVERLOAD(WRD_ASSIGN_GUARD, __VA_ARGS__)
