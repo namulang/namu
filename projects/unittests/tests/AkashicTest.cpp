c58f6303b736f06015d613956f647830a99cbfa0
kniz <kniz@kniz.net>
2019-02-04 12:25:39 +0900

    - [wrd] add AkashicTest with more testcases.

--- projects/unittests/tests/AkashicTest.cpp
+++ projects/unittests/tests/AkashicTest.cpp
@@ -12,15 +12,43 @@ WRD_TEST_START(AkashicTest)
         float grade;
     };
 
-    //    basic:
+    //	basic:
     A a;
-    {
-        const Block& blk = a.getBlock();
-        T(blk.isExist())
-        T(blk.getSerial())
-        const Chunk& chk = blk.getChunk();
-        T( ! chk.isExist())
+	const Block& blk = a.getBlock();
+	T(blk.isExist())
+	T(blk.getSerial())
+	const Chunk& chk = blk.getChunk();
+	T( ! chk.isExist())
+
+	std::vector<A*> arr;
+	for(int n=0; n < 10; n++)
+	{
+		A* new1 = new A();
+		const Block& blk = new1->getBlock();
+		T(blk.isExist())
+		T(blk.getSerial() > 0)
+		T(blk.isBind())
+		//TODO: T(blk.getBindable() == TClass<Instance>())
+		//TOOD: T(blk.canBind(TClass<Instance>()))
+		Id id = blk.getId();
+		T(id.s.blk_n != WRD_INDEX_ERROR)
+		T(id.s.chk_n != WRD_INDEX_ERROR)
+		T(id.s.serial > 0)
+		T(blk.isHeap())
+		const Chunk& chk = blk.getChunk();
+		T(chk.isExist())
+		T(chk.has(*new1))
+		T(chk.getEOB())
+		T(chk.getHeap())
+		T(chk.getLen() > 0)
+		T(chk.getSize() > 0)
+		new1->age = n;
+		arr.push_back(new1);
     }
 
+	for(A* e : arr)
+		delete e;
+	arr.clear();
+
     return "";
-WRD_TEST_END(AkashicTest)
+ WRD_TEST_END(AkashicTest)
