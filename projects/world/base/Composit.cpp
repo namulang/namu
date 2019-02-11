cc5f3a3786aa0deaf76ff539a75d9fc43fdf2a76
kniz <kniz@kniz.net>
2019-02-11 11:55:41 +0900

    + [wrd] remove dependency to Binder of Composit.
    uses pointer instead of binder. it removed dependency and increased
    performance.
    I used binder because there is no possibility for chaing type or instance
    of _nodes if it's allocated once.

--- projects/world/base/Composit.cpp
+++ projects/world/base/Composit.cpp
@@ -8,6 +8,33 @@ namespace wrd
 #define THIS Composit
 	WRD_CLASS_DEF(THIS)
 
+	THIS::THIS() : _nodes(0) {}
+	THIS::THIS(const This& rhs) : Super(rhs) { _assign(rhs); }
+	THIS::~THIS() { _release(); }
+
+	THIS& THIS::_assign(const This& rhs)
+	{
+		_release();
+		//TODO: if(rhs._nodes)
+		//	_nodes = new Chain(*rhs._nodes);
+		return *this;
+	}
+
+	Res& THIS::_release()
+	{
+		if(_nodes)
+			_nodes->release();
+		_nodes = NULL;
+		return wasgood;
+	}
+
+	THIS& THIS::operator=(const This& rhs)
+	{
+		WRD_ASSIGN_GUARD()
+		Super::operator=(rhs);
+		return _assign(rhs);
+	}
+
 	Res& THIS::init()
 	{
 		WRD_IS_SUPER(init())
@@ -16,13 +43,15 @@ namespace wrd
 
 	Res& THIS::release()
 	{
-		_nodes.release();
+		_release();
 		return Super::release();
 	}
 
     const Container& THIS::getNodes() const
     {
 		//	TODO: 
+		//if( ! _nodes)
+		//	_nodes = new Chain();
         //if(_nodes.getLength() <= 0)
         //   _initNodes();
         return *_nodes;
@@ -31,7 +60,7 @@ namespace wrd
     Res& THIS::_initNodes()
     {
 		/* TODO:
-        _nodes.release();
+        _getNodes().release();
 
         const Chain& chain = WRD_GET(cls.getNodes().down<Chain>());
         if(_nodes.chain(chain.getControl()[0])) // first elem as a container owned by "chain"
