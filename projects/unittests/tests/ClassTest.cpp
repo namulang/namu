90af2d39e147b04093440ee483a2aebc4f408dcb
kniz <kniz@kniz.net>
2019-02-13 19:13:04 +0900

    + [ut] add ClassTest.

---
+++ projects/unittests/tests/ClassTest.cpp
@@ -0,0 +1,32 @@
+#include "../TestCase.hpp"
+
+WRD_TEST_START(ClassTest)
+	//	basic:
+	{
+		TClass<Thing> cls;
+		T( ! cls.isTemplate())
+		T(cls.isADT())
+		const Classes& subs = cls.getSubs();
+		T(subs.isExist())
+		T(subs.getLen())
+		T(subs.getSize())
+		T( ! cls.instance().isBind())
+		const Classes& sups = cls.getSupers();
+		T(sups.isExist())
+		T(sups.getLen() == 0)
+		T(sups.getSize() == 0)
+		T(cls == cls.getSuper())
+
+		TClass<Instance> ins;
+		T(ins.isSub(cls))
+		T(ins.getSuper().isExist())
+		T(ins.getSuper() == cls)
+		TStrong<TClass<Instance> > cloned = ins.clone();
+		T(cloned.isExist())
+		T(cloned.isBind())
+		T(*cloned == ins)
+		//TODO: cls.getLeafs().isExist())
+		// TODO: checks TClass<Instance> is one of subs using has() or check() API on Container.
+	}
+	return "";
+WRD_TEST_END(ClassTest)
