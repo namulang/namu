1fd2aefa24dfd5abd70c0decd47991b811090d45
kniz <kniz@kniz.net>
2019-02-14 21:50:49 +0900

    - [wrd] Crash bug. block refers _pt freed already.

--- projects/world/memory/Block.cpp
+++ projects/world/memory/Block.cpp
@@ -40,14 +40,7 @@ namespace wrd
 	wbool THIS::canBind(const Class& cls) const { return cls.isSub(getBindable()); }
 	Id THIS::getId() const { return _id; }
 
-	wbool THIS::isHeap() const
-	{
-		const Chunk& chk = getChunk();
-		WRD_IS_NULL(chk, wasnull, false)
-
-		return chk.has(*_pt);
-	}
-
+	wbool THIS::isHeap() const { return _id.s.chk_n != WRD_INDEX_ERROR; }
 	Strong THIS::toStrong() { return Strong((Node*)_pt); }
 	Weak THIS::toWeak() { return Weak((Node*)_pt); }
 	Res& THIS::release() { return unbind(); }
@@ -78,6 +71,7 @@ namespace wrd
 		//	complete mine:
 		Id mine = getId();
 		mine.s.chk_n = it._id.s.chk_n;
+		WRD_INFO("%x.bind(%d %d)", &it, mine.s.blk_n, mine.s.serial);
 		//	propagate it:
 		return _sync(mine);
 	}
