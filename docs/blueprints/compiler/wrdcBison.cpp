9ad82cd522676c466bb598699a440d13240b0399
kniz <kniz.official@gmail.com>
2020-02-19 13:02:53 +0900

    + [docs] now prototype can handle def syntax.

--- docs/blueprints/compiler/wrdcBison.cpp
+++ docs/blueprints/compiler/wrdcBison.cpp
@@ -48,6 +48,8 @@ void yyerror(const char* s)
 
 %type <node> tstmt tlhsexpr/*only lhs*/ trhsexpr tcast trhsargs tlhsargs tblock tindentBlock tfile tfuncCall telseBlock telifBlock tbranch termIf tseq tarray ttype tmap taccess treturn tlhslist trhslist ttuple ttuples tparam tparams tfunc
 
+%type <node> tdefOrigin tdefIndentBlock tdefexpr tdefStmt tdefBlock
+
 %type <nodes> telifBlocks
 
 
@@ -118,6 +120,7 @@ trhsexpr    : tinteger { $$ = new Int($1); }
             | tlhsexpr { $$ = $1; }
             | tcast %dprec 1 { $$ = $1; }
             | taccess { $$ = $1; }
+            | tdefOrigin { $$ = $1; }
             | trhsexpr '+' trhsexpr %dprec 2 {
                 $$ = new Plus($1, $3);
             }
@@ -165,12 +168,16 @@ trhsexpr    : tinteger { $$ = new Int($1); }
             }
             | termIf { $$ = $1; }
             | tfuncCall { $$ = $1; }
+            ;
+
+tdefexpr    : tlhslist topDefAssign trhslist { $$ = new DefAssign($1, $3); }
+            | tlhsexpr topDefAssign trhsexpr { $$ = new DefAssign($1, $3); }
             | tfunc { $$ = $1; }
             ;
 
 tlhsexpr    : tidentifier { $$ = new Id($1); }
+            | tdefexpr { $$ = $1; }
             | tlhslist '=' trhslist { $$ = new Assign($1, $3); }
-            | tlhslist topDefAssign trhslist { $$ = new DefAssign($1, $3); }
             | tlhslist topPlusAssign trhslist { $$ = new PlusAssign($1, $3); }
             | tlhslist topMinusAssign trhslist { $$ = new MinusAssign($1, $3); }
             | tlhslist topSquareAssign trhslist { $$ = new SquareAssign($1, $3); }
@@ -178,7 +185,6 @@ tlhsexpr    : tidentifier { $$ = new Id($1); }
             | tlhslist topModAssign trhslist { $$ = new ModulerAssign($1, $3); }
             | tlhslist topPowAssign trhslist { $$ = new PowAssign($1, $3); }
 
-            | tlhsexpr topDefAssign trhsexpr { $$ = new DefAssign($1, $3); }
             | tlhsexpr topPlusAssign trhsexpr { $$ = new PlusAssign($1, $3); }
             | tlhsexpr topMinusAssign trhsexpr { $$ = new MinusAssign($1, $3); }
             | tlhsexpr topSquareAssign trhsexpr { $$ = new SquareAssign($1, $3); }
@@ -294,6 +300,14 @@ tparams     : tparam {
             }
             ;
 
+tdefOrigin  : tdef tidentifier teol tdefIndentBlock {
+                $$ = new Def($2, 0, $4);
+            }
+            | tdef tidentifier tfrom tidentifier teol tdefIndentBlock {
+                $$ = new Def($2, new Id($4), $6);
+            }
+            ;
+
 tfunc       : ttype tfuncname '(' tparams ')' teol tindentBlock {
                 $$ = new Func($1, $2, $4, $7);
             }
@@ -306,19 +320,14 @@ tfuncCall   : tfuncname trhslist {
             }
             ;
 
-tstmt       : trhsexpr teol {
-                $$ = new Stmt($1);
-            }
-            | treturn teol {
-                $$ = new Stmt($1);
-            }
-            | teol {
-                $$ = new Stmt(new Str(""));
-            }
-            | trhsexpr teof {
-                $$ = new Stmt($1);
-            }
+tdefStmt    : tdefexpr teol { $$ = new Stmt($1); }
+            | teol { $$ = new Stmt(new Str("")); }
+            ;
 
+tstmt       : trhsexpr teol { $$ = new Stmt($1); }
+            | treturn teol { $$ = new Stmt($1); }
+            | trhsexpr teof { $$ = new Stmt($1); }
+            | teol { $$ = new Stmt(new Str("")); }
             ;
 
 tblock      : tstmt {
@@ -335,9 +344,23 @@ tblock      : tstmt {
             }
             ;
 
-tindentBlock: tindent tblock tdedent {
-                $$ = $2;
+tdefBlock   : tdefexpr {
+                Block* ret = new Block();
+                if ($1)
+                    ret->add($1);
+                $$ = ret;
             }
+            | tdefBlock tdefStmt {
+                Block* ret = (Block*) $1;
+                if ($2)
+                    ret->add($2);
+                $$ = ret;
+            }
+            ;
+
+tdefIndentBlock: tindent tdefBlock tdedent { $$ = $2; }
+
+tindentBlock: tindent tblock tdedent { $$ = $2; }
 
 tfile       : tblock teof {
                 Block* blk = (Block*) $1;
