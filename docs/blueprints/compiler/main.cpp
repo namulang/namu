480629152f38da1660220237065ce8aede340ee6
kniz <kniz@kniz.net>
2020-02-24 09:49:03 +0900

    + [docs] prototype now supports OperatorPrecedence mode. * it prints which expression will be evalutated first if they coupled among them by using paranthesis.

--- docs/blueprints/compiler/main.cpp
+++ docs/blueprints/compiler/main.cpp
@@ -10,6 +10,7 @@ int yyparse();
 
 extern FILE *yyin, *yyout;
 extern File* parsed;
+bool USE_OP = false;
 
 int main(int argc, char* argv[])
 {
@@ -17,23 +18,22 @@ int main(int argc, char* argv[])
 
     cout    << "\n"
             << " 2020 @ Worldlang interpreter. prototype ver. by kniz\n"
-            << " it's on github, github.com/kniz/worldlang with LGPL licensed.\n";
+            << " it's on github, github.com/kniz/worldlang with LGPL licensed.\n"
+            << " " << argv[0] << " [<option>] <filename> [\n"
+            << " options:\n"
+            << " \t-d\tuse Debug mode. prints all logs when it compiles.\n"
+            << " \t-op\tprint operator precedence by paranthesis.\n";
 
     if (argc > 1)
     {
-        if (argc > 3) {
-            cout    << " " << argv[0] << " [<option>] <filename> [\n"
-                    << " options:\n"
-                    << " \t-d\tuse Debug mode. prints all logs when it compiles.\n";
-            exit(-1);
-        }
-
         for (int n=1; n < argc ;n++) {
             if (argv[n] == string("-d")) {
                 cout << " - use debug mode. \n";
 #ifdef YYDEBUG
                 yydebug = 1;
 #endif
+            } else if (argv[n] == string("-op")) {
+                USE_OP = true;
             } else {
                 yyin = fopen(argv[n], "r");
                 cout << " opening " << argv[n] << " file...\n";
