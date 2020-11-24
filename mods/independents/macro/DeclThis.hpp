422b91ba127f8f1ae5d8528bfe595960ef968b23
kniz <kniz@kniz.net>
2020-11-24 19:34:19 +0900

    - [indep] replace tab to spaces.

--- mods/independents/macro/DeclThis.hpp
+++ mods/independents/macro/DeclThis.hpp
@@ -3,13 +3,13 @@
 #include "DefThis.hpp"
 
 #define WRD_DECL_THIS_2(THIS, SUPER) \
-	public:	\
+    public: \
         WRD_DEF_THIS_2(THIS, SUPER) \
-	private:
+    private:
 
 #define WRD_DECL_THIS_1(THIS) \
-	public:	\
+    public: \
         WRD_DEF_THIS_1(THIS) \
-	private:
+    private:
 
 #define WRD_DECL_THIS(...)  WRD_OVERLOAD(WRD_DECL_THIS, __VA_ARGS__)
