e275ab9950ffda9b9d72f0ce7d3d10606f035b02
kniz <kniz.official@gmail.com>
2020-11-26 20:30:35 +0900

    - [indep] replace tab to spaces.

--- mods/indep/file/stream/BinaryStream.hpp
+++ mods/indep/file/stream/BinaryStream.hpp
@@ -3,45 +3,45 @@
 #include "FileStream.hpp"
 
 namespace wrd {
-	namespace fm {
+    namespace fm {
 
-		class File;
+        class File;
 
-		class BinaryStream : public FileStream {
+        class BinaryStream : public FileStream {
             WRD_DECL_THIS(BinaryStream, FileStream)
 
-		public:
-			BinaryStream();
-			BinaryStream(const std::string& new_path);
-			BinaryStream(const File& file);
-			virtual ~BinaryStream();
+        public:
+            BinaryStream();
+            BinaryStream(const std::string& new_path);
+            BinaryStream(const File& file);
+            virtual ~BinaryStream();
 
-		public:
-			using FileStream::operator=;
-			using FileStream::read;
-			using FileStream::write;
+        public:
+            using FileStream::operator=;
+            using FileStream::read;
+            using FileStream::write;
 
-			virtual wbool init();
+            virtual wbool init();
 
-			template <typename T>
-			wcnt write(const T& datum) { return write(&datum, sizeof(T)); }
+            template <typename T>
+            wcnt write(const T& datum) { return write(&datum, sizeof(T)); }
 
-			wcnt write(const char* str);
-			wcnt write(const std::string& str);
-			virtual wcnt write(const void* chunks, wcnt bytes);
-			virtual wcnt read(void* target, wcnt bytes);
+            wcnt write(const char* str);
+            wcnt write(const std::string& str);
+            virtual wcnt write(const void* chunks, wcnt bytes);
+            virtual wcnt read(void* target, wcnt bytes);
 
-			template <typename T>
-			T read() {
-				T to_return;
-				read(&to_return, sizeof(T));
-				return to_return;
-			}
+            template <typename T>
+            T read() {
+                T to_return;
+                read(&to_return, sizeof(T));
+                return to_return;
+            }
 
-			std::string read(wcnt bytes);
+            std::string read(wcnt bytes);
 
-		private:
-			BinaryStream(const PathedObject& object);
-		};
-	}
+        private:
+            BinaryStream(const PathedObject& object);
+        };
+    }
 }
--- mods/indep/file/structure/Folder.cpp
+++ mods/indep/file/structure/Folder.cpp
@@ -1,104 +1,104 @@
 #include "Folder.hpp"
 
 namespace wrd {
-	namespace fm {
+    namespace fm {
 
-		using namespace std;
+        using namespace std;
         WRD_DEF_THIS(Folder)
 
-		This::Option::Option(wbool is_reculsive) : _is_reculsive(is_reculsive) {}
-		wbool This::Option::isReculsive() const { return _is_reculsive; }
+        This::Option::Option(wbool is_reculsive) : _is_reculsive(is_reculsive) {}
+        wbool This::Option::isReculsive() const { return _is_reculsive; }
 
-		This::Folder(const std::string& new_path) : Super(new_path), _iterator(0), _sub_file(0) {}
-		This::Folder(const File* owner, const string& path, Option option) : Super(owner, path), _iterator(0), _sub_file(0), _option(option) {}
-		This::Folder(const string& path, Option option) : Super(0, path), _iterator(0), _sub_file(0), _option(option) {}
-		This::~Folder() { _release(); }
+        This::Folder(const std::string& new_path) : Super(new_path), _iterator(0), _sub_file(0) {}
+        This::Folder(const File* owner, const string& path, Option option) : Super(owner, path), _iterator(0), _sub_file(0), _option(option) {}
+        This::Folder(const string& path, Option option) : Super(0, path), _iterator(0), _sub_file(0), _option(option) {}
+        This::~Folder() { _release(); }
 
-		wbool This::init() {
-			if(isInit())
-				_release();
+        wbool This::init() {
+            if(isInit())
+                _release();
 
-			_iterator = opendir(getPath().c_str());
-			if( ! _iterator)
-				std::cout << strerror(errno) << "=opendir(" << getPath().c_str() << ")\n";
-			return ! isInit();
-		}
+            _iterator = opendir(getPath().c_str());
+            if( ! _iterator)
+                std::cout << strerror(errno) << "=opendir(" << getPath().c_str() << ")\n";
+            return ! isInit();
+        }
 
-		wbool This::isInit() const { return _iterator; }
-		const This::Option& This::getOption() const { return _option; }
+        wbool This::isInit() const { return _iterator; }
+        const This::Option& This::getOption() const { return _option; }
 
-		const File& This::next() {
-			//  main:
-			//      prepare start_iterator just after returned before:
-			//          if _sub_file is not a folder, we should not call its next():
-			//              because File.next() always returns valid value. we'll fall into infinite loops.
-			const File* to_return = _prepare();
-			while(_isFiltered(*to_return)) {
-				if(_sub_file)
-					delete _sub_file;
-				_sub_file = _createSubFile(_iterator);
-				if(!_sub_file)
-					release();
+        const File& This::next() {
+            //  main:
+            //      prepare start_iterator just after returned before:
+            //          if _sub_file is not a folder, we should not call its next():
+            //              because File.next() always returns valid value. we'll fall into infinite loops.
+            const File* to_return = _prepare();
+            while(_isFiltered(*to_return)) {
+                if(_sub_file)
+                    delete _sub_file;
+                _sub_file = _createSubFile(_iterator);
+                if(!_sub_file)
+                    release();
 
-				to_return = _next();
-			}
+                to_return = _next();
+            }
 
-			return *to_return;
-		}
+            return *to_return;
+        }
 
-		const File& This::peek() const { return _sub_file ? _sub_file->peek() : *this; }
+        const File& This::peek() const { return _sub_file ? _sub_file->peek() : *this; }
 
-		wbool This::release() {
-			_release();
-			return Super::release();
-		}
+        wbool This::release() {
+            _release();
+            return Super::release();
+        }
 
-		const File* This::_prepare() const {
-			if(_sub_file && getOption().isReculsive() && _sub_file->isFolder())
-				return &_sub_file->next();
-			return 0;
-		}
+        const File* This::_prepare() const {
+            if(_sub_file && getOption().isReculsive() && _sub_file->isFolder())
+                return &_sub_file->next();
+            return 0;
+        }
 
-		const File* This::_next() const { return _sub_file && getOption().isReculsive() ? &_sub_file->next() : _sub_file; }
-		/// @remark not allowed.
-		This::Folder(const Folder& rhs) : File(rhs), _iterator(0) { }
-		Folder& This::operator=(const Folder& rhs) { return *this; }
+        const File* This::_next() const { return _sub_file && getOption().isReculsive() ? &_sub_file->next() : _sub_file; }
+        /// @remark not allowed.
+        This::Folder(const Folder& rhs) : File(rhs), _iterator(0) { }
+        Folder& This::operator=(const Folder& rhs) { return *this; }
 
-		wbool This::_isFiltered(const File& target) const {
-			if(target.isNull()) return _iterator != 0; // if next() called at least once and there is no target.
+        wbool This::_isFiltered(const File& target) const {
+            if(target.isNull()) return _iterator != 0; // if next() called at least once and there is no target.
 
-			return false; // okay. seems that we found it.
-		}
+            return false; // okay. seems that we found it.
+        }
 
-		File* This::_createSubFile(DIR* e) {
-			//  pre:
-			if(!e) return 0;
+        File* This::_createSubFile(DIR* e) {
+            //  pre:
+            if(!e) return 0;
 
 
-			//  main
-			string name;
-			do
-			{
-				struct dirent* file = readdir(e);
-				if( ! file)
-					return 0;
-				name = file->d_name;
-			} while(name == "." || name == "..");
-			//      gathering info:
-			if(_isFolder(_getInfo(getPath() + "/" + name)))
-				return new Folder(this, name, _option);
-			return new File(this, name);
-		}
+            //  main
+            string name;
+            do
+            {
+                struct dirent* file = readdir(e);
+                if( ! file)
+                    return 0;
+                name = file->d_name;
+            } while(name == "." || name == "..");
+            //      gathering info:
+            if(_isFolder(_getInfo(getPath() + "/" + name)))
+                return new Folder(this, name, _option);
+            return new File(this, name);
+        }
 
-		void This::_release() {
-			if(_sub_file)
-				_sub_file->release();
-			delete _sub_file;
-			_sub_file = 0;
+        void This::_release() {
+            if(_sub_file)
+                _sub_file->release();
+            delete _sub_file;
+            _sub_file = 0;
 
-			if(_iterator)
-				closedir(_iterator);
-			_iterator = 0;
-		}
-	}
+            if(_iterator)
+                closedir(_iterator);
+            _iterator = 0;
+        }
+    }
 }
