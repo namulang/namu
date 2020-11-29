7947159a2f4d71d15349b8e754fb126a49fa61e9
kniz <kniz.official@gmail.com>
2020-11-29 10:02:11 +0900

    + [indep] add first unit test of indep.

---
+++ mods/indep/test/common/indep-test-dep.hpp
@@ -0,0 +1,4 @@
+#pragma once
+
+#include <gtest/gtest.h>
+#include <indep.hpp>
--- mods/indep/test/main.cc
+++ mods/indep/test/main.cc
@@ -1,4 +1,4 @@
-#include <gtest/gtest.h>
+#include "common/indep-test-dep.hpp"
 
 int main(int argc, char** argv) {
     ::testing::InitGoogleTest(&argc, argv);
