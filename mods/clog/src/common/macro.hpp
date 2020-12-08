83304b9796d5c9ce3466f2b38b0c238fd027e9fc
kniz <kniz@kniz.net>
2020-12-08 21:05:55 +0900

    - [clog] can't use WRD_LOG macro with variable.

--- mods/clog/src/common/macro.hpp
+++ mods/clog/src/common/macro.hpp
@@ -1,11 +1,11 @@
 #pragma once
 
-#define WRD_LOG(level, msg, ...) { \
-	wrd::clog::Logger::getInstance().dumpFormat("%s " WRD_TAG " %s <%s::%s#%d> " msg, \
-            wrd::indep::PlatformAPI::createCurrentTime("%b %d %Y  %X").c_str(), level, \
-            __FILENAME__, __func__, __LINE__, ##__VA_ARGS__ \
-        ); \
+#define WRD_LOG(level, msg) { \
+	wrd::clog::Logger::getInstance().dumpFormat(\
+        "%s " WRD_TAG " %s <%s::%s#%d> %s", \
+        wrd::indep::PlatformAPI::createCurrentTime("%b %d %Y  %X").c_str(), \
+        level, __FILENAME__, __func__, __LINE__, msg); \
     }
-#define WRD_I(msg, ...) WRD_LOG("I", msg, ##__VA_ARGS__)
-#define WRD_E(msg, ...) WRD_LOG("E", msg, ##__VA_ARGS__)
-#define WRD_W(msg, ...) WRD_LOG("W", msg, ##__VA_ARGS__)
+#define WRD_I(msg) WRD_LOG("I", msg)
+#define WRD_E(msg) WRD_LOG("E", msg)
+#define WRD_W(msg) WRD_LOG("W", msg)
