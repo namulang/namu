82665937a739d82b281618046caa49ea9e2b81d6
kniz <kniz.official@gmail.com>
2021-02-08 13:36:36 +0900

    - [clog] replace tab into 4 width spaces.

--- mod/clog/src/common/macro.hpp
+++ mod/clog/src/common/macro.hpp
@@ -1,7 +1,7 @@
 #pragma once
 
 #define _LOG(func, level, fmt, ...) { \
-	wrd::Logger::get().func(\
+    wrd::Logger::get().func(\
         "%s " WRD_TAG " %s <%s::%s#%d> " fmt, \
         wrd::PlatformAPI::createCurrentTime("%b %d %Y  %X").c_str(), \
         level, __FILENAME__, __func__, __LINE__, ## __VA_ARGS__); \
