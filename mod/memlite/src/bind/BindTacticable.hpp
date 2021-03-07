6720ed800b30d357f4cef35a420df5b12ea1523e
kniz <kniz@kniz.net>
2021-03-07 12:19:25 +0900

    - [memlite] when a binder got assigned from another bind obj, it doesn't unbind.

--- mod/memlite/src/bind/BindTacticable.hpp
+++ mod/memlite/src/bind/BindTacticable.hpp
@@ -8,7 +8,6 @@ namespace wrd {
     class Instance;
     class BindTacticable {
     public:
-        virtual wbool assign(Bind& me, const Bind& rhs) = 0;
         virtual wbool unbind(Bind& me) = 0;
         virtual wbool bind(Bind& me, const Instance& it) = 0;
     };
