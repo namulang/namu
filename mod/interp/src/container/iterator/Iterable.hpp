c51cc3c449aaf009d14833171280a8c4d0f24193
kniz <kniz.official@gmail.com>
2021-03-08 12:35:14 +0900

    - [interp] Iter::next() now can be used to proceed multiple times at a time.

--- mod/interp/src/container/iterator/Iterable.hpp
+++ mod/interp/src/container/iterator/Iterable.hpp
@@ -14,7 +14,13 @@ namespace wrd {
         virtual ~Iterable() {}
 
         virtual wbool isEnd() const = 0;
-        virtual wbool next() = 0;
+        /// proceed Iteration obj to point next element by given step.
+        /// @param  step    how much it proceeds.
+        ///                 positive value only.
+        /// @return how many step it proceeded. if it tries to proceed further when it reached to End()
+        ///         it won't be counted as returning value.
+        ///         it'll return '0' if it already reached to the end.
+        virtual wcnt next(wcnt step) = 0;
         virtual Node& get() = 0;
         const Node& get() const WRD_UNCONST_FUNC(get())
         virtual wbool isFrom(const NContainer& it) const = 0;
