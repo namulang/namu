725781c8bb1da59f8e1a54e7b481c1bd2c262467
kniz <kniz@kniz.net>
2021-03-10 13:10:15 +0900

    - [interp] NChainTest failed to when calling NContainer API.

--- mod/interp/test/case/NChainTest.cc
+++ mod/interp/test/case/NChainTest.cc
@@ -53,3 +53,130 @@ TEST(NChainFixture, simpleAddDelTest1) {
 TEST(NChainFixture, simpleAddDelTest1) {
     simpleAddDelTest(10000);
 }
+
+class MyMyNode : public MyNode {
+    WRD_CLASS(MyMyNode, MyNode)
+
+public:
+    MyMyNode(int num): Super(num) {}
+};
+TEST(NChainFixture, testContainableAPI) {
+    //  initial state:
+    TStr<NChain> arr = new NChain();
+    Containable* con = &arr.get();
+    ASSERT_EQ(con->getLen(), 0);
+
+    Iter head = arr->head();
+    ASSERT_TRUE(head.isEnd());
+    Iter tail = con->tail();
+    ASSERT_TRUE(tail.isEnd());
+
+    ASSERT_TRUE(con->add(con->head(), new MyNode(0)));
+    ASSERT_TRUE(con->add(con->tail(), new MyMyNode(1)));
+    ASSERT_EQ(con->getLen(), 2);
+
+    //  add:
+    int expectVal = 0;
+    for(Iter e=con->head(); e != con->tail() ;e++) {
+        MyNode& elem = e->cast<MyNode>();
+        ASSERT_FALSE(nul(elem));
+        ASSERT_EQ(elem.number, expectVal++);
+    }
+
+    //  get & each:
+    NArr tray = arr->get<MyNode>([](const MyNode& elem) {
+        return true;
+    });
+    ASSERT_EQ(tray.getLen(), 2);
+
+    int cnt = 0;
+    tray = arr->get<MyNode>([&cnt](const MyNode& elem) {
+        if(cnt >= 1) return false;
+        cnt++;
+        return true;
+    });
+    ASSERT_EQ(tray.getLen(), 1);
+
+    tray = arr->get<MyMyNode>([](const MyMyNode& elem) {
+        if(elem.number == 1) return true;
+        return false;
+    });
+    ASSERT_EQ(tray.getLen(), 1);
+
+    //  del:
+    ASSERT_TRUE(con->del());
+    ASSERT_EQ(con->getLen(), 1);
+    ASSERT_EQ(con->head()->cast<MyNode>().number, 0);
+
+    //  add with element:
+    NArr arr2;
+    ASSERT_EQ(arr2.add(*con), 1);
+    ASSERT_TRUE(arr2.add(new MyNode(1)));
+    ASSERT_TRUE(arr2.add(new MyMyNode(2)));
+    ASSERT_TRUE(arr2.add(new MyNode(3)));
+    ASSERT_EQ(arr2[2].cast<MyNode>().number, 2);
+    ASSERT_EQ(arr2[3].cast<MyNode>().number, 3);
+    ASSERT_EQ(arr2.getLen(), 4);
+
+    Iter e = arr2.head();
+    e = e + 2;
+    ASSERT_EQ(e->cast<MyNode>().number, 2);
+    ASSERT_TRUE(arr2.add(e, new MyNode(5)));
+    ASSERT_TRUE(arr2.add(2, new MyNode(6)));
+
+    ASSERT_EQ(arr2[0].cast<MyNode>().number, 0);
+    ASSERT_EQ(arr2[1].cast<MyNode>().number, 1);
+    ASSERT_EQ(arr2[2].cast<MyNode>().number, 6);
+    ASSERT_EQ(arr2[3].cast<MyNode>().number, 5);
+    ASSERT_EQ(arr2[4].cast<MyNode>().number, 2);
+    ASSERT_EQ(arr2[5].cast<MyNode>().number, 3);
+
+    ASSERT_EQ(con->getLen(), 1);
+    ASSERT_EQ(con->add(arr2.iter(1), arr2.iter(3)), 2);
+    ASSERT_EQ(con->getLen(), 3);
+    e=con->head();
+    MyNode* elem = &e->cast<MyNode>();
+    ASSERT_FALSE(nul(elem));
+    ASSERT_EQ(elem->number, 0);
+
+    elem = &(++e)->cast<MyNode>();
+    ASSERT_FALSE(nul(elem));
+    ASSERT_EQ(elem->number, 1);
+
+    elem = &(++e)->cast<MyNode>();
+    ASSERT_FALSE(nul(elem));
+    ASSERT_EQ(elem->number, 6);
+    ASSERT_FALSE(++e);
+
+    ASSERT_TRUE(con->getLen() > 0);
+    con->empty();
+    ASSERT_TRUE(con->getLen() == 0);
+
+    ASSERT_EQ(con->add(arr2.head() + 2, arr2.tail()), 4);
+    e = con->head();
+    elem = &e->cast<MyNode>();
+    ASSERT_FALSE(nul(elem));
+    ASSERT_EQ(elem->number, 6);
+
+    elem = &(++e)->cast<MyNode>();
+    ASSERT_FALSE(nul(elem));
+    ASSERT_EQ(elem->number, 5);
+
+    elem = &(++e)->cast<MyNode>();
+    ASSERT_FALSE(nul(elem));
+    ASSERT_EQ(elem->number, 2);
+
+    elem = &(++e)->cast<MyNode>();
+    ASSERT_FALSE(nul(elem));
+    ASSERT_EQ(elem->number, 3);
+
+    ASSERT_EQ(con->del(con->head() + 1, con->head() + 3), 2);
+    e = con->head();
+    elem = &e->cast<MyNode>();
+    ASSERT_FALSE(nul(elem));
+    ASSERT_EQ(elem->number, 6);
+
+    elem = &(++e)->cast<MyNode>();
+    ASSERT_FALSE(nul(elem));
+    ASSERT_EQ(elem->number, 3);
+}
