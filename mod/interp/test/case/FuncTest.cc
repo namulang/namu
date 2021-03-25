5b9e38b63b691b14f3230ce4572e31691cf49512
kniz <kniz@kniz.net>
2021-03-26 00:06:58 +0900

    + [interp] add TC for nested function which interacts with stackframe.

--- mod/interp/test/case/FuncTest.cc
+++ mod/interp/test/case/FuncTest.cc
@@ -8,7 +8,7 @@ class MyFunc : public Func {
     WRD_CLASS(MyFunc, Func)
 
 public:
-    MyFunc(): Super("MyFunc") {}
+    MyFunc(std::string name = "MyFunc"): Super(name) {}
 
     void setUp() {
         _executed = false;
@@ -52,6 +52,31 @@ struct FuncFixture : public ::testing::Test {
     MyFunc func;
 };
 
+wbool checkFrameHasFuncAndObjScope(const Frame& fr, const Func& func, const Obj& obj) {
+    if(nul(fr)) return false;
+
+    int n = 0;
+    WRD_E("fr.len=%d", fr.subs().getLen());
+    for(Iter e=fr.subs().head(); e ;e++)
+        WRD_E(" - Func(\"%s\") calls: fr[%d]=%s", func.getName().c_str(), n++, e->getType().getName().c_str());
+
+    const NChain& funcScope = fr.subs().cast<NChain>();
+    if(nul(funcScope)) return WRD_E("nul(funcScope)"), false;
+    if(&func.subs() != &funcScope.getContainer())
+        return WRD_E("func.subs(%x) != funcScope(%x)", &func.subs(), &funcScope), false;
+
+    const NChain& objScope = funcScope.getNext();
+    if(nul(objScope)) return WRD_E("nul(objScope)"), false;
+    if(&obj.subs() != &objScope)
+        return WRD_E("obj.subs(%x) != objScope(%x)", &obj.subs(), &objScope), false;
+
+    NArr foundFunc = fr.sub(func.getName());
+    if(foundFunc.getLen() != 1)
+        return WRD_E("couldn't find %s func on frame(%x)", func.getName().c_str(), &fr), false;
+
+    return true;
+}
+
 TEST_F(FuncFixture, testFuncConstructNewFrame) {
     Obj obj;
     obj.subs().add(func);
@@ -67,28 +92,59 @@ TEST_F(FuncFixture, testFuncConstructNewFrame) {
     func.setLambda([this, &obj](const StackFrame& sf) {
         if(sf.getLen() != 1) return false;
 
-        const Frame& fr = sf[0];
-        if(nul(fr)) return false;
-
-        const NChain& funcScope = fr.subs().cast<NChain>();
-        if(nul(funcScope)) return WRD_E("nul(funcScope)"), false;
-        const NChain& objScope = funcScope.getNext();
-        if(nul(objScope)) return WRD_E("nul(objScope)"), false;
-        if(&obj.subs() != &objScope) return false;
-
-        int n = 0;
-        WRD_E("fr.len=%d", fr.subs().getLen());
-        for(Iter e=fr.subs().head(); e ;e++) {
-            WRD_E(" - fr[%d]=%s", n++, e->getType().getName().c_str());
-        }
-
-        NArr foundFunc = fr.sub(func.getName());
-        if(foundFunc.getLen() != 1) return false;
-
-        return true;
+        return checkFrameHasFuncAndObjScope(sf[0], this->func, obj);
     });
+
     func.run(args);
     ASSERT_TRUE(func.isRun());
     ASSERT_TRUE(func.isSuccess());
     ASSERT_TRUE(func.isSuccess());
+}
+
+TEST_F(FuncFixture, testCallFuncInsideFunc) {
+    Obj obj1;
+    MyFunc obj1func1("obj1func1");
+    MyFunc obj1func2("obj1func2");
+    obj1.subs().add(obj1func1);
+    obj1.subs().add(obj1func2);
+
+    Obj obj2;
+    MyFunc obj2func1("obj2func1");
+    obj2.subs().add(obj2func1);
+
+    obj1func1.setLambda([&obj1, &obj1func1, &obj1func2](const StackFrame& sf) {
+        if(sf.getLen() != 1) return WRD_E("obj1func1: sf.getLen() != 1"), false;
+        if(!checkFrameHasFuncAndObjScope(sf[0], obj1func1, obj1)) return false;
+
+        NArr args;
+        args.add(obj1);
+        obj1func2.run(args);
+        if(sf.getLen() != 1)
+            return WRD_E("return of obj1func1: sf.getLen() != 1"), false;
+        return true;
+    });
+    obj1func2.setLambda([&obj2, &obj1func2, &obj1, &obj2func1](const StackFrame& sf) {
+        if(sf.getLen() != 2) return WRD_E("obj1func2: sf.getLen() != 2"), false;
+
+        if(!checkFrameHasFuncAndObjScope(sf[1], obj1func2, obj1)) return false;
+
+        NArr args;
+        args.add(obj2);
+
+        obj2func1.run(args);
+        if(sf.getLen() != 2)
+            return WRD_E("return of obj1func2: sf.getLen() != 2"), false;
+        return true;
+    });
+    obj2func1.setLambda([&obj2, &obj2func1](const StackFrame& sf) {
+        if(sf.getLen() != 3) return false;
+
+        if(!checkFrameHasFuncAndObjScope(sf[2], obj2func1, obj2)) return false;
+        return true;
+    });
+
+    NArr args;
+    args.add(obj1);
+    obj1func1.run(args);
+    ASSERT_TRUE(obj1func1.isSuccess());
 }
