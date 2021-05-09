9837ab12551e44db369f1bba22febdf25a9793bb
kniz <kniz@kniz.net>
2021-05-09 20:39:37 +0900

    + [swrd] swrd always make a 'file' root obj.

--- mod/swrd/src/interp/smallWorld.cpp
+++ mod/swrd/src/interp/smallWorld.cpp
@@ -4,6 +4,10 @@
 
 namespace wrd { namespace swrd {
 
+    namespace {
+        std::string _fileName;
+    }
+
     WRD_DEF_ME(smallWorld)
 
     tstr<obj> me::interp(const wchar* script) {
@@ -20,6 +24,8 @@ namespace wrd { namespace swrd {
         if(!yyin)
             return WRD_E("invalid file path %s.", path), tstr<obj>();
 
+        _fileName = _extractFileName(path);
+        WRD_I("interpreting file '%s'...", _fileName.c_str());
         return _runParser();
     }
     tstr<obj> me::interpFile(const std::string& path) {
@@ -34,5 +40,16 @@ namespace wrd { namespace swrd {
             WRD_E("nothing interpreted.");
 
         return tstr<obj>(root);
+    }
+
+    const std::string& me::getFileName() {
+        return _fileName;
+    }
+
+    std::string me::_extractFileName(const std::string& path) {
+        size_t atSlash = path.find_last_of("\\/");
+        if(atSlash < 0) return "";
+
+        return path.substr(atSlash + 1);
     }
 }}
--- mod/swrd/test/case/helloProgrammerTest.cc
+++ mod/swrd/test/case/helloProgrammerTest.cc
@@ -10,10 +10,13 @@ TEST(helloProgrammerTest, testScript) {
         "   say := 'html is not a programming language!'\n"
         "";
 
-    tstr<swrd::obj> root = swrd::smallWorld::interp(script);
-    ASSERT_TRUE(root);
+    tstr<swrd::obj> file = swrd::smallWorld::interp(script);
+    ASSERT_TRUE(file);
 
-    obj& say = root->sub("say");
+    swrd::obj& programmer = file->sub("programmer");
+    ASSERT_FALSE(nul(programmer));
+
+    swrd::obj& say = programmer.sub("say");
     ASSERT_FALSE(nul(say));
 
     ASSERT_STREQ(say.asStr().c_str(), "html is not a programming language!");
@@ -22,8 +25,20 @@ TEST(helloProgrammerTest, testScript) {
 TEST(helloProgrammerTest, testScriptEndsWithEOF) {
     const std::string script =
         "def programmer\n"
-        "   say := 'html is not a programming language!'";
+        "   say := 'html is not a programming language!'\n"
+        "   age := 12";
 
-    tstr<swrd::obj> root = swrd::smallWorld::interp(script);
-    ASSERT_TRUE(root);
+    tstr<swrd::obj> file = swrd::smallWorld::interp(script);
+    ASSERT_TRUE(file);
+
+    swrd::obj& programmer = file->sub("programmer");
+    ASSERT_FALSE(nul(programmer));
+
+    swrd::obj& say = programmer.sub("say");
+    ASSERT_FALSE(nul(say));
+    ASSERT_STREQ(say.asStr().c_str(), "html is not a programming language!");
+
+    swrd::obj& age = programmer.sub("age");
+    ASSERT_FALSE(nul(age));
+    ASSERT_EQ(age.asInt(), 12);
 }
