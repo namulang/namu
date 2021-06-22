bd679d93e5ad247a90a45e0b2682e8e47cc7d140
kniz <kniz.official@gmail.com>
2021-06-22 22:20:09 +0900

    interp: test: frame should preserves chaining on add()

--- mod/interp/test/case/frameTest.cc
+++ mod/interp/test/case/frameTest.cc
@@ -2,14 +2,71 @@
 
 using namespace wrd;
 
+namespace {
+    struct myNode : public node {
+        WRD_CLASS(myNode, node)
+
+    public:
+        myNode(int n): num(n) {}
+
+        ncontainer& subs() override { return nulOf<ncontainer>(); }
+        wbool canRun(const wtypes& types) const override { return false; }
+        str run(const ncontainer& args) override { return str(); }
+
+        int num;
+    };
+}
+
 struct frameTest : public ::testing::Test {
+
+    stackFrame& getStackFrame() {
+        return (stackFrame&) thread::get().getStackFrame();
+    }
+
+    void SetUp() {
+        getStackFrame().add(new frame());
+    }
 
     void TearDown() {
         thread::get().rel();
     }
+
+    nchain& getLinks(frame& fr) {
+        return *fr._links;
+    }
 };
 
 TEST_F(frameTest, testAccessFrame) {
-    stackFrame& sf = (stackFrame&) thread::get().getStackFrame();
-    sf.add(new frame);
+    getStackFrame().add(new frame);
+}
+
+TEST_F(frameTest, testFrameManipulateChainObjNegative) {
+    frame& fr = getStackFrame().getCurrentFrame();
+
+    nchain chnBase;
+    chnBase.add(new myNode(1));
+    chnBase.add(new myNode(2));
+    ASSERT_EQ(chnBase.len(), 2);
+    fr.add(chnBase);
+
+    nchain shares;
+    shares.add(new myNode(3));
+    nchain owns;
+    owns.add(new myNode(4));
+    owns.link(shares);
+    ASSERT_EQ(owns.len(), 2);
+    auto lambda = [](const myNode& elem) {
+        return true;
+    };
+    ASSERT_EQ(owns.get<myNode>(lambda).len(), 2);
+
+    fr.add(owns);
+    ASSERT_EQ(fr.sub<myNode>(lambda).len(), 4);
+    ASSERT_EQ(owns.get<myNode>(lambda).len(), 2);
+
+    int expects[] = {4, 3, 1, 2};
+    titer<myNode> e = getLinks(fr).begin<myNode>();
+    for (int expect : expects) {
+        ASSERT_EQ(expect, e->num);
+    }
 }
