92974c82090803d40c861fa1f77a5c1465b0fa0f
kniz <kniz.official@gmail.com>
2021-06-26 21:01:29 +0900

    interp: feat: entrypoint of pack

--- mod/interp/test/samplePack/entrypoint.cpp
+++ mod/interp/test/samplePack/entrypoint.cpp
@@ -1,3 +1,17 @@
+#include <interp.hpp>
+#include <iostream>
+using namespace wrd;
 
-extern "C" void wrd_module_entrypoint() {
+struct helloWorld {
+    void say() {
+        std::cout << "hello world!\n";
+    }
+};
+
+extern "C" struct wrd_bridge_cpp_origins* wrd_module_entrypoint(struct wrd_bridge_cpp_origins* packs) {
+
+    packs->arr[packs->size++] = tcppBridge<helloWorld>::def()
+        ->func("say", &helloWorld::say);
+
+    return packs;
 }
