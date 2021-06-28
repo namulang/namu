11ad2dc3258eedb9974b1c14ff4109e966d99a5f
kniz <kniz@kniz.net>
2021-06-28 21:13:06 +0900

    swrd: feat: null this protection.

--- mod/swrd/src/ast/sobj.cpp
+++ mod/swrd/src/ast/sobj.cpp
@@ -6,6 +6,11 @@ namespace wrd {
     WRD_DEF_ME(sobj)
 
     me& me::sub(const std::string& name) {
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wtautological-undefined-compare"
+        if (this == nullptr)
+            return nulOf<me>();
+#pragma clang diagnostic pop
         tstr<me>& ret = _subs[name];
         if(!ret)
                 return nullSobj::get();
