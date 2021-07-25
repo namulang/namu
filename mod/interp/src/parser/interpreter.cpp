7e5909bdd1aeb0b4e9f927521dbfc0e9c597b6f4
kniz <kniz.official@gmail.com>
2021-07-25 23:49:07 +0900

    interp: feat: add 'interpreter' class interp: fix: glr-parser should be pull parser

---
+++ mod/interp/src/parser/interpreter.cpp
@@ -0,0 +1,30 @@
+#include "interpreter.hpp"
+#include "wrdScanner.hpp"
+
+namespace wrd {
+
+    WRD_DEF_ME(interpreter)
+
+    wbool test(const wchar* msg) {
+        yyscan_t scanner;
+        yylex_init(&scanner);
+
+        //TODO: read from file: yyset_in(input_file, scanner);
+        std::string str(msg);
+        YY_BUFFER_STATE buffer = yy_scan_buffer((wchar*) str.c_str(), str.size() + 2, scanner); // + 2 for marking END_OF_BUFFER, nullptr.
+        if (buffer == nullptr) {
+            return WRD_E("buffer == null"), false;
+        }
+
+        // TODO: provide API to manipulate these variable
+        yyset_debug(1, scanner); // For Flex (no longer a global, but rather a member of yyguts_t)
+#if YYDEBUG
+        yydebug = 1;             // For Bison (still global, even in a reentrant parser)
+#endif
+
+        int ret = yyparse(scanner);
+
+        yylex_destroy(scanner);
+        return ret;
+    }
+}
