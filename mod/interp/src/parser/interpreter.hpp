e6c7145b53e820100e27487a46ff1500cad89e7d
kniz <kniz@kniz.net>
2021-07-25 23:49:07 +0900

    interp: feat: add 'interpreter' class interp: fix: glr-parser should be pull parser

---
+++ mod/interp/src/parser/interpreter.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "../common.hpp"
+
+namespace wrd {
+
+    class interpreter {
+        //TODO: WRD_CLASS(interpreter)
+
+    public:
+        void test(const wchar* msg);
+    };
+
+}
