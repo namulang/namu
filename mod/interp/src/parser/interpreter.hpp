6e410b6d44a887115c00fb5ec1ae8b1da389157c
kniz <kniz.official@gmail.com>
2021-07-31 00:07:55 +0900

    interp: feat: add interpretable
    we have 2 generated parser by bison, one for swrd and one for wrd.
    however each generated parser's implementation file puts same define
    name as an include guard to distinguish condtional compilation,
    when we build both of them, one of guard always make that can't enter
    inside of the guard area.
    
    in order to prevent, we must not include parser header at swrd from parser
    header from wrd module.

--- mod/interp/src/parser/interpreter.hpp
+++ mod/interp/src/parser/interpreter.hpp
@@ -1,14 +1,15 @@
 #pragma once
 
 #include "../common.hpp"
+#include "interpretable.hpp"
 
 namespace wrd {
 
-    class interpreter {
+    class interpreter : public interpretable {
         //TODO: WRD_CLASS(interpreter)
 
     public:
-        wbool test(const wchar* msg);
+        wbool test(const wchar* msg) override;
     };
 
 }
