64a6d59b54b9266cf517092f984b56643abada06
kniz <kniz.official@gmail.com>
2021-09-12 22:56:59 +0900

    interp: bug: tpair compile error if L is a refenence type

--- mod/interp/src/builtin/res/tpair.hpp
+++ mod/interp/src/builtin/res/tpair.hpp
@@ -6,10 +6,37 @@ namespace wrd {
 
     template <typename L, typename R>
     struct tpair {
-        tpair(): l(L()), r(R()) {}
-        tpair(L newL, R newR): l(newL), r(newR) {}
+        tpair() {}
+        tpair(L& newL, R& newR): l(newL), r(newR) {}
 
         L l;
         R r;
+    };
+
+    template <typename L, typename R>
+    struct tpair<L&, R> {
+        tpair(): l(nulOf<L>()) {}
+        tpair(L& newL, R newR): l(newL), r(newR) {}
+
+        L& l;
+        R r;
+    };
+
+    template <typename L, typename R>
+    struct tpair<L, R&> {
+        tpair(): r(nulOf<R>()) {}
+        tpair(L newL, R& newR): l(newL), r(newR) {}
+
+        L l;
+        R& r;
+    };
+
+    template <typename L, typename R>
+    struct tpair<L&, R&> {
+        tpair(): l(nulOf<L>()), r(nulOf<R>()) {}
+        tpair(L& newL, R& newR): l(newL), r(newR) {}
+
+        L& l;
+        R& r;
     };
 }
