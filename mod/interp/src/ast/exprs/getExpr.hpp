92552764142d40d4e10d302d224ef073553fdb12
kniz <kniz.official@gmail.com>
2021-09-13 23:04:55 +0900

    interp: feat: expr now should take src info on creation

--- mod/interp/src/ast/exprs/getExpr.hpp
+++ mod/interp/src/ast/exprs/getExpr.hpp
@@ -8,10 +8,10 @@ namespace wrd {
         WRD(CLASS(getExpr, expr))
 
     public:
-        getExpr(const Ref& it, const wStr& newName, const arr& args)
-            : super(&target, &newName, &args), _it(it), _name(newName), _args(args) {}
-        getExpr(const Ref& target, const wStr& newName)
-            : super(&target, &newName), _it(it), _name(newName), _args(nulOf<arr>()) {}
+        getExpr(const src& s, wcnt lineNum, const Ref& it, const wStr& newName, const arr& args)
+            : super(s, lineNum, &target, &newName, &args), _it(it), _name(newName), _args(args) {}
+        getExpr(const src& s, wcnt lineNum, const Ref& target, const wStr& newName)
+            : super(s, lineNum, &target, &newName), _it(it), _name(newName), _args(nulOf<arr>()) {}
 
     public:
         str run(const ncontainer& args) {
