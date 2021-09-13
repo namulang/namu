a9de2505a799545d2923169f5c09d67526bb8c31
kniz <kniz.private@gmail.com>
2021-09-13 23:04:55 +0900

    interp: feat: expr now should take src info on creation

--- mod/interp/src/ast/expr.hpp
+++ mod/interp/src/ast/expr.hpp
@@ -8,7 +8,8 @@ namespace wrd {
         WRD(INTERFACE(expr, node))
 
     public:
-        expr(std::initializer_list<const node*> newTerms): _terms(newTerms) {}
+        expr(const src& s, wcnt lineNum, std::initializer_list<const node*> newTerms)
+            : _terms(newTerms), _src(s), _lineNum(lineNum) {}
 
     public:
         ncontainer& subs() override {
@@ -27,7 +28,17 @@ namespace wrd {
             return true;
         }
 
+        const src& getSrc() const {
+            return *_src;
+        }
+
+        wcnt getLineNum() const {
+            return _lineNum;
+        }
+
     private:
         narr _terms;
+        tstr<src> _src;
+        wcnt _lineNum;
     };
 }
