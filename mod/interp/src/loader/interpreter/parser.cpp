868cc8d019a37d0a202bcc625dd763db62058027
kniz <kniz.official@gmail.com>
2021-09-15 23:59:15 +0900

    interp: feat: parser can accept normal c-string
    yy_scan_buffer takes 2 argument.
    * 1 for pointer to string (const char*)
    * 1 for length to the string pointed by pointer.
    
    however yy_scan_buffer expects that string pointer has 2 null character
    at the end of string message.
    
    instead we use yy_scan_string. it only requires single null character.

--- mod/interp/src/loader/interpreter/parser.cpp
+++ mod/interp/src/loader/interpreter/parser.cpp
@@ -5,11 +5,11 @@ namespace wrd {
 
     WRD_DEF_ME(parser)
 
-    str me::parseBuffer(const std::string& buffer) {
+    str me::parse(const wchar* script) {
         yyscan_t scanner;
         yylex_init_extra(this, &scanner);
 
-        YY_BUFFER_STATE bufState = yy_scan_buffer((wchar*) buffer.c_str(), buffer.size() + 2, scanner); // +2 is for space of END_OF_BUFFER, nullptr.
+        YY_BUFFER_STATE bufState = yy_scan_string((wchar*) script, scanner); // +2 is for space of END_OF_BUFFER, nullptr.
         if(!bufState)
             return WRD_E("bufState == nullptr"), str();
 
--- mod/interp/test/case/parserTest.cc
+++ mod/interp/test/case/parserTest.cc
@@ -4,6 +4,12 @@ using namespace wrd;
 
 TEST(parserTest, testHelloWorld) {
     parser p;
-    str rootBinder = p.parseBuffer("123 ;");
-    // TODO: check with rootBinder.
+    const wchar* stringScript = "123 ;";
+    std::string script(stringScript);
+
+    str rootBinder = p.parse(script.c_str());
+    //TODO: ASSERT_TRUE(rootBinder.isBind());
+
+    rootBinder = p.parse(stringScript);
+    //TODO: ASSERT_TRUE(rootBinder.isBind());
 }
