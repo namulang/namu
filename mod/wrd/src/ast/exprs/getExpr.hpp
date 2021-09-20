9874dcdb7c813136845f839e103719c2b7e80c06
kniz <kniz.private@gmail.com>
2021-09-20 17:25:03 +0900

    wrd: refactor: expr don't provide terms as its sub node

--- mod/wrd/src/ast/exprs/getExpr.hpp
+++ mod/wrd/src/ast/exprs/getExpr.hpp
@@ -8,27 +8,26 @@ namespace wrd {
         WRD(CLASS(getExpr, expr))
 
     public:
-        getExpr(const src& s, wcnt lineNum, const Ref& it, const wStr& newName, const arr& args)
-            : super(s, lineNum, &target, &newName, &args), _it(it), _name(newName), _args(args) {}
-        getExpr(const src& s, wcnt lineNum, const Ref& target, const wStr& newName)
-            : super(s, lineNum, &target, &newName), _it(it), _name(newName), _args(nulOf<arr>()) {}
+        getExpr(Ref* from, const std::string& name, narr* args = nullptr)
+            : _from(from), _name(name), _args(args) {}
 
     public:
         str run(const containable& args) {
             // believe that this expression was checked to be valid.
-            return str(_it->sub(_name, _args));
+            return str(_from->sub(_name, _args));
         }
 
         wbool isValid() const override {
-            if(!_it) return false;
+            if(!super::isValid()) return false;
+            if(!_from) return false;
 
-            narr subs = _it->subAll(_name, _args);
+            narr subs = _from->subAll(_name, _args);
             return subs.len() == 1;
         }
 
     private:
-        const Ref& _it;
-        const wStr& _name;
-        const arr& _args;
+        tstr<Ref> _from;
+        std::string _name;
+        tstr<narr> _args;
     };
 }
