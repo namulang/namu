65f5268af43ae1f84afac98d72cc482944c032c1
kniz <kniz.private@gmail.com>
2021-09-23 18:15:09 +0900

    wrd: refactor: procedure changed for calling funcs
    * remove redundant class or funcs
    * class 'func' doesn't need to care for interaction to scope.
      because native funcs won't interact to scope.

--- mod/wrd/src/ast/mgd/mgdFunc.hpp
+++ mod/wrd/src/ast/mgd/mgdFunc.hpp
@@ -1,20 +1,27 @@
 #pragma once
 
 #include "../func.hpp"
+#include "../../frame/frameInteractable.hpp"
+#include "../exprs/blockExpr.hpp"
 
 namespace wrd {
 
-    class mgdFunc : public func {
+    class mgdFunc : public func, public frameInteractable {
         WRD(INTERFACE(mgdFunc, func))
 
     public:
-        explicit mgdFunc(const std::string& name): super(name) {}
+        explicit mgdFunc(const std::string& name, const blockExpr* newBlock = new blockExpr()): super(name), _blk(newBlock) {}
+
+    public:
+        blockExpr& getBlock() { return *_blk; }
+        const blockExpr& getBlock() const { return *_blk; }
 
     protected:
         str _onCast(narr& castedArgs) override;
-        virtual str _onRun(narr& args) = 0;
-
         wbool _onInFrame(frame& sf, const ncontainer& args) override;
         wbool _onOutFrame(frame& sf, const ncontainer& args) override;
+
+    private:
+        tstr<blockExpr> _blk;
     };
 }
