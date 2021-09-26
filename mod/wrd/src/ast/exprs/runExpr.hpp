1904da75477f052ad3ee92e0cfc16259c9a90922
kniz <kniz.private@gmail.com>
2021-09-26 18:57:16 +0900

    wrd: feat: add runExpr

--- mod/wrd/src/ast/exprs/runExpr.hpp
+++ mod/wrd/src/ast/exprs/runExpr.hpp
@@ -4,9 +4,20 @@
 
 namespace wrd {
 
-    /*class runExpr : public expr {
+    class runExpr : public expr {
         WRD(CLASS(runExpr, expr))
 
     public:
-    };*/
+        runExpr(node* meObj, narr* args): _me(meObj), _args(args) {}
+
+    public:
+        str run(const containable& args) override {
+            if(!_me) return WRD_E("_me == null"), str();
+
+            return _me->
+
+    private:
+        str _me;
+        scope _args;
+    };
 }
--- mod/wrd/src/builtin/primitive/wInt.hpp
+++ mod/wrd/src/builtin/primitive/wInt.hpp
@@ -6,26 +6,26 @@ namespace wrd {
 
     class wInt : public primitiveObj<wint> {
 
-		class wIntType : public wtype {
-		public:
-			wbool isImmutable() const override { return true; }
+        class wIntType : public wtype {
+        public:
+            wbool isImmutable() const override { return true; }
 
-		protected:
-			const ases& _getImpliAses() const override {
-				static ases inner;
-				if(inner.len() <= 0) {
-					struct toWstr: public tas<wStr> {
-						str as(const node& wInt, const type& to) const override {
-							// TODO:
-							return str();
-						}
-					};
-					inner.add(new toWstr());
-				}
+        protected:
+            const ases& _getImpliAses() const override {
+                static ases inner;
+                if(inner.len() <= 0) {
+                    struct toWstr: public tas<wStr> {
+                        str as(const node& wInt, const type& to) const override {
+                            // TODO:
+                            return str();
+                        }
+                    };
+                    inner.add(new toWstr());
+                }
 
-				return inner;
-			}
-		};
+                return inner;
+            }
+        };
         WRD(CLASS(wInt, primitiveObj, wIntType))
 
     public:
