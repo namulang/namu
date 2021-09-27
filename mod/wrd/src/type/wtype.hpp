5cb485e2f62bee2f2938072495ad2d6e7cc5e1d9
kniz <kniz.official@gmail.com>
2021-09-27 19:01:36 +0900

    wrd: test: add wtypeTest

--- mod/wrd/src/type/wtype.hpp
+++ mod/wrd/src/type/wtype.hpp
@@ -14,40 +14,40 @@ namespace wrd {
         // wtype:
         template <typename T>
         wbool isImpli() const {
-			return this->isImpli(ttype<T>::get());
-		}
+            return this->isImpli(ttype<T>::get());
+        }
         wbool isImpli(const type& to) const {
             return _getImpliAses().is(*this, to);
         }
 
         template <typename T>
         tstr<T> asImpli(const node& it) const {
-			return this->asImpli(ttype<T>::get());
-		}
+            return this->asImpli(ttype<T>::get());
+        }
         str asImpli(const node& from, const type& to) const {
-			return _getImpliAses().as(from, to);
-		}
+            return _getImpliAses().as(from, to);
+        }
 
         template <typename T>
         wbool is() const {
-			return this->is(ttype<T>::get());
-		}
+            return this->is(ttype<T>::get());
+        }
         wbool is(const type& to) const {
-			if(isImpli(to)) return true;
+            if(isImpli(to)) return true;
 
             return _getAses().is(*this, to);
         }
 
         template <typename T>
         tstr<T> as(const node& it) const {
-			return this->as(it, ttype<T>::get());
-		}
+            return this->as(it, ttype<T>::get());
+        }
         str as(const node& from, const type& to) const {
-			if(isImpli(to))
-				return asImpli(from, to);
+            if(isImpli(to))
+                return asImpli(from, to);
 
-			return _getAses().as(from, to);
-		}
+            return _getAses().as(from, to);
+        }
 
         virtual wbool isImmutable() const {
             return false;
@@ -61,9 +61,9 @@ namespace wrd {
         }
 
         virtual const ases& _getAses() const {
-			static ases inner;
-			return inner;
-		}
+            static ases inner;
+            return inner;
+        }
     };
 
     typedef std::vector<const wtype*> wtypes;
