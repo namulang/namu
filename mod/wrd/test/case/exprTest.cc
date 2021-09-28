ee94dd4086bcdebbb44d7115cfc2d2e6ada729ee
kniz <kniz.official@gmail.com>
2021-09-28 19:04:04 +0900

    wrd: test: add TC for expr

---
+++ mod/wrd/test/case/exprTest.cc
@@ -0,0 +1,45 @@
+#include "../common/dep.hpp"
+
+using namespace wrd;
+using namespace std;
+
+namespace {
+    struct helloWorld {
+        void main(const string msg) {
+            cout << "wow, how do you feel? ==> " << msg << "\n";
+            isRun = true;
+        }
+
+        static inline wbool isRun = false;
+    };
+
+    struct exprTest : public ::testing::Test {
+        void SetUp() {
+            bridge.bind(tcppBridge<helloWorld>::def()
+                ->func("main", &helloWorld::main));
+        }
+
+        void TearDown() { helloWorld::isRun = false; }
+
+        tstr<obj> bridge;
+    };
+}
+
+TEST_F(exprTest, constructExprsInManual) {
+    ASSERT_TRUE(bridge.isBind());
+
+    tstr<wStr> msg(new wStr());
+    narr args {&bridge.get(), &msg.get()};
+
+    node& mainFunc = bridge->sub("main", args);
+    ASSERT_FALSE(nul(mainFunc));
+    ASSERT_TRUE(mainFunc.canRun(args));
+    ASSERT_FALSE(mainFunc.canRun(wtypes({&msg->getType()})));
+    ASSERT_TRUE(mainFunc.canRun(wtypes({&bridge->getType(), &msg->getType()})));
+    ASSERT_FALSE(helloWorld::isRun);
+    str res = mainFunc.run(args);
+    ASSERT_TRUE(res.isBind());
+    ASSERT_TRUE(res.getType() == ttype<node>::get());
+    ASSERT_TRUE(res->getType() == ttype<wVoid>::get());
+    ASSERT_TRUE(helloWorld::isRun);
+}
