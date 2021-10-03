6b3a28b0a8bf2ad6d8f63687c847e495c1a6b95c
kniz <kniz.official@gmail.com>
2021-10-03 17:17:06 +0900

    wrd: feat: add dummyErrReport

--- mod/wrd/src/loader/errReport.hpp
+++ mod/wrd/src/loader/errReport.hpp
@@ -1,14 +1,16 @@
 #pragma once
 
 #include "err.hpp"
+#include "../type/wtype.hpp"
+#include "../ast/clonable.hpp"
 
 namespace wrd {
 
-    class errReport : public typeProvidable, clonable {
-        WRD(CLASS(errReport, std::vector<err>))
+    class errReport : public typeProvidable, public clonable {
+        WRD(CLASS(errReport))
 
 	public:
-		const err& operator[](widx n) const { return _errs[n]; }
+		const err& operator[](widx n) const { return get(n); }
 
     public:
         wbool hasErr() const {
@@ -26,7 +28,7 @@ namespace wrd {
 
 		wcnt len() const { return _errs.size(); }
 
-		void add(const err& new1) {
+		virtual void add(const err& new1) {
 			_errs.push_back(new1);
 		}
 
@@ -45,5 +47,13 @@ namespace wrd {
 	private:
 		std::vector<err> _errs;
     };
-}
 
+	class dummyErrReport : public errReport {
+		WRD(CLASS(dummyErrReport, errReport))
+
+	public:
+		void add(const err& new1) override {}
+
+		static me singletone;
+	};
+}
