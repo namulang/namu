a899320135fa08b27c76da8c979d1dc1612e7197
kniz <kniz.private@gmail.com>
2021-10-07 13:21:57 +0900

    wrd: refactor: check validness for runExpr

--- mod/wrd/src/loader/err.cpp
+++ mod/wrd/src/loader/err.cpp
@@ -12,8 +12,16 @@ namespace wrd {
         static msgMap inner {
         WRD_EACH(_ON_MSG,
             "unknown", /* is 0 */
-            "this expr doesn't have line number info." /* is 1 */
+            "this expr doesn't have row info.", /* == 1 */
+            "this expr evaludated as nulled type",
+            "obj to be accessed was null.",
+            "obj can't be cast into node class.",
+            "obj can't execute with this argument types.", /* == 5 */
+            "this expr doesn't have col info.",
+
             /* to be continue... */
+
+            "end of errors"
         )
 
 #undef _ON_MSG
