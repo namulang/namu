fcca3cd4b0080f52df34d07d9bdc91ac7236ce8f
kniz <kniz.official@gmail.com>
2021-10-09 12:56:34 +0900

    wrd: doc: comment for new classes
    * documents why it has been created and which role it has.

--- mod/wrd/src/loader/interpreter/parser.hpp
+++ mod/wrd/src/loader/interpreter/parser.hpp
@@ -6,6 +6,16 @@
 
 namespace wrd {
 
+    /// parser is a class that finally parses the wrd file and creates an ast structure.
+    /// this has a parser and a scanner created from a parser generator such as bison
+    /// internally, and these are called low-level parsers and low-level scanners.
+    /// low-level parser/scanner focuses on whether the string given as input matches a
+    /// specific grammar, and passes it to the parser when matching.
+    /// this 'parser' class creates and returns an appropriate AST structure if it is
+    /// matched.
+    ///
+    /// the reason for configuring this structure is to make it easy to replace other
+    /// parser generators.
     class parser : public parserable, typeProvidable {
         WRD(INTERFACE(parser, parserable))
 
