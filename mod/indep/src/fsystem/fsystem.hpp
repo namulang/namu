79a0adaae8e2fb99abc140760680f9f537b310cd
kniz <kniz.official@gmail.com>
2021-10-11 23:22:51 +0900

    indep: feat: add file search iterator on fsystem
    this will be replaced to filesystem of standard lib in future.

--- mod/indep/src/fsystem/fsystem.hpp
+++ mod/indep/src/fsystem/fsystem.hpp
@@ -2,11 +2,79 @@
 
 #include "../macro.hpp"
 #include "../common.hpp"
+#include <dirent.h>
+#include <vector>
 
 namespace wrd {
 
+    /// fsystem is going to be used before filesystem library fully supported.
+    /// to apply fsystem on clang++ is quite hard now.
     class fsystem {
         WRD_DECL_ME(fsystem)
+        WRD(ME(fsystem))
+
+        struct entry {
+            DIR* dir;
+            std::string path;
+        };
+        typedef std::vector<entry> entries;
+
+        class iterator {
+            WRD(ME(iterator))
+            friend class fsystem;
+
+        private:
+            iterator(const std::string& path) {
+                _addDir(path);
+            }
+
+        public:
+            ~iterator() {
+                rel();
+            }
+
+        public:
+            const std::string& operator*() {
+                return get();
+            }
+            operator wbool() const {
+                return !isEnd();
+            }
+
+        public:
+            void rel() {
+                while(!isEnd())
+                    _popDir();
+            }
+
+            const std::string& get();
+
+            wbool isEnd() const {
+                return _entries.size() == 0;
+            }
+
+        private:
+            void _addDir(const std::string& dirPath) {
+                DIR* newDir = opendir(dirPath.c_str());
+                if(!newDir) return;
+
+                _entries.push_back(entry {newDir, dirPath});
+            }
+            void _popDir() {
+                entry& e = _entries[_entries.size()-1];
+                closedir(e.dir);
+                _entries.pop_back();
+            }
+
+        private:
+            entries _entries;
+            std::string _nowPath;
+        };
+
+    public:
+        static iterator find(const std::string& path) {
+            return iterator(path);
+        }
 
     public:
         static std::string getCurrentDir() {
---
+++ mod/indep/test/case/fsystemTest.cc
@@ -0,0 +1,18 @@
+#include "../common/dep.hpp"
+
+using namespace wrd;
+
+TEST(fsystemTest, recursiveDirSearchAtBin) {
+    // expect that workdirectory of this executable is at 'bin'.
+    auto root = fsystem::find("..");
+    wbool buildFound = false;
+
+    while(root) {
+        if(*root == "../build/CMakeLists.txt") {
+            buildFound = true;
+            break;
+        }
+    }
+    root.rel();
+    ASSERT_TRUE(buildFound);
+}
