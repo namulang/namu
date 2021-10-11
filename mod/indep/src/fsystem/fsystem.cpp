a0a4843d302672675fddaf19919b1718361cf0be
kniz <kniz.private@gmail.com>
2021-10-11 23:22:51 +0900

    indep: feat: add file search iterator on fsystem
    this will be replaced to filesystem of standard lib in future.

---
+++ mod/indep/src/fsystem/fsystem.cpp
@@ -0,0 +1,32 @@
+#include "fsystem.hpp"
+
+namespace wrd {
+
+    WRD_DEF_ME(fsystem)
+
+    const std::string& me::iterator::get() {
+        // assume that all of data to _entries was valid:
+        //  which means, if returning value from readdir() was null,
+        //  then the most top element of entries should be removed.
+        while(!isEnd()) {
+            entry& e = _entries[_entries.size()-1];
+            struct dirent* file = readdir(e.dir);
+            if(!file) {
+                _popDir();
+                continue;
+            }
+            std::string name = file->d_name;
+            if(name == ".." || name == ".")
+                continue;
+            std::string path = e.path + "/" + name;
+            if(file->d_type == DT_DIR) {
+                _addDir(path);
+                continue;
+            }
+
+            return _nowPath = path;
+        }
+
+        return _nowPath = "";
+    }
+}
