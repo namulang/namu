f9ff62e09d4e99496cb2dcd433caace8cedaab8b
kniz <kniz.official@gmail.com>
2021-12-19 13:50:42 +0900

    wrd: refactor: optimization to reuse scanable object
    previsouly I used to create scanable instance every time.

--- mod/wrd/src/loader/interpreter/loweventer.hpp
+++ mod/wrd/src/loader/interpreter/loweventer.hpp
@@ -4,31 +4,40 @@
 #include <functional>
 #include "../../ast/node.hpp"
 #include "tokenDispatcher.hpp"
-#include "bison/scanMode.hpp"
+#include "bison/tokenScanable.hpp"
 
 namespace wrd {
 
-    class loweventer : public typeProvidable, public clonable {
+    typedef union YYSTYPE YYSTYPE;
+    typedef struct lloc YYLTYPE;
+    typedef void* yyscan_t;
+
+    class loweventer : public typeProvidable, public clonable, public tokenScanable {
         WRD(CLASS(loweventer))
         friend class tokenScanModable;
 
     public:
-        loweventer(): _mode(nullptr) {}
-        virtual ~loweventer();
+        loweventer(): _mode(nullptr) {
+            _indents.push_back(0);
+        }
 
     public:
         str& getRoot() { return _root; }
         tokenDispatcher& getDispatcher() { return _dispatcher; }
-        std::vector<wcnt>& getIndents() { return _indents; }
-        scanMode& getScanMode() const { return *_mode; }
-        void setScanMode(scanMode* new1) {
-            if(_mode)
-                delete _mode;
-            _mode = new1;
+        std::vector<wcnt> getIndents() { return _indents; }
+        wbool isInit() const { return _mode; }
+
+        template <typename T>
+        void setScan() {
+            _mode = T::_instance;
         }
 
+    public:
+        // events:
+        wint onScan(loweventer& eventer, YYSTYPE* yylval, YYLTYPE* yylloc, yyscan_t scanner) override;
+
     private:
-        scanMode* _mode;
+        tokenScanable* _mode;
         str _root;
         tokenDispatcher _dispatcher;
         std::vector<wcnt> _indents;
