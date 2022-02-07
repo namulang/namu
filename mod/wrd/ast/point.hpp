f4956d475a335a0379459095140327fe16d41819
kniz <kniz.private@gmail.com>
2022-02-07 19:03:08 +0900

    wrd: feat: add rel() on 'point' class

--- mod/wrd/ast/point.hpp
+++ mod/wrd/ast/point.hpp
@@ -6,10 +6,19 @@ namespace wrd {
 	struct point {
 		wcnt row;
 		wcnt col;
+
+        void rel() {
+            row = col = 0;
+        }
 	};
 
 	struct area {
 		point start;
 		point end;
+
+        void rel() {
+            start.rel();
+            end.rel();
+        }
 	};
 }
