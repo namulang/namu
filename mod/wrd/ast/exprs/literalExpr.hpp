2ce96c590d3a0575af1528810a5a5f3e22393a39
kniz <kniz.private@gmail.com>
2022-02-23 17:44:03 +0900

    wrd: feat: add literalExpr
    in wrdlang, literal constants in the middle of block stmt
    can be evaluated and returned outside of block. including a func.

---
+++ mod/wrd/ast/exprs/literalExpr.hpp
@@ -0,0 +1,35 @@
+#pragma once
+
+#include "../expr.hpp"
+
+namespace wrd {
+
+    class literalExpr : public expr {
+        WRD(CLASS(literalExpr, expr, expr::exprType))
+        friend class mgdFunc;
+
+    public:
+        literalExpr(const node& n): _str(n) {}
+
+    public:
+        using super::run;
+        str run(const containable& args) override {
+            return _str;
+        }
+
+        const wtype& getEvalType() const override {
+            // _str always binds to some object.
+            return _str->getType();
+        }
+
+        str& getStr() { return _str; }
+        const str& getStr() const { return _str; }
+
+    private:
+        str _str;
+    };
+
+    /*WRD_VERIFY(
+     * if _str.type is immutable, _str should not bind any object.
+     */
+}
