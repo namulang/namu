c3ff21afabab3ba6b31272d4141c90e3fc7ee97e
kniz <kniz.private@gmail.com>
2022-04-27 20:50:58 +0900

    indep: feat: compatible for windows

--- mod/indep/fsystem/fsystem.cpp
+++ mod/indep/fsystem/fsystem.cpp
@@ -14,16 +14,31 @@ namespace wrd {
         //  then the most top element of entries should be removed.
         while(!isEnd()) {
             entry& e = _entries[_entries.size()-1];
+#ifdef WRD_BUILD_PLATFORM_IS_WINDOWS
+            int res = _findnext(e.dir, &e.file);
+            if(res == -1) {
+#else
             struct dirent* file = readdir(e.dir);
             if(!file) {
+#endif
                 _popDir();
                 continue;
             }
+#ifdef WRD_BUILD_PLATFORM_IS_WINDOWS
+            std::string name = e.file.name;
+            char delimiter = '\\';
+#else
             std::string name = file->d_name;
+            char delimiter = '/';
+#endif
             if(name == ".." || name == ".")
                 continue;
             std::string path = e.path + "/" + name;
+#ifdef WRD_BUILD_PLATFORM_IS_WINDOWS
+            if(e.file.attrib & _A_SUBDIR) {
+#else
             if(file->d_type == DT_DIR) {
+#endif
                 _addDir(path);
                 continue;
             }
