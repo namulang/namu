1b477dfa777ef929f6c80ae8b25b2d00e2561970
kniz <kniz.official@gmail.com>
2022-05-15 12:23:48 +0900

    wrd: feat: add tndumMap

--- mod/wrd/builtin/container/native/tndumArr.hpp
+++ mod/wrd/builtin/container/native/tndumArr.hpp
@@ -6,14 +6,15 @@ namespace wrd {
 
     template <typename T, typename WRAPPER = str>
     class tndumArr: public tnarr<T, WRAPPER> {
-        WRD(CLASS(tndumArr, tnarr))
+        typedef tnarr<T, WRAPPER> __super;
+        WRD(CLASS(tndumArr, __super))
 
     public:
         tndumArr() {}
 
         // add:
         using super::add;
-        wbool add(const wrd::iter& e, const node& new1) override { return false; }
+        wbool add(const typename super::iter& e, const node& new1) override { return false; }
         wbool add(widx n, const node& new1) override { return false; }
 
     public:
