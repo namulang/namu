f84afae1bf91820cc800d8e5b9b7b504a94d36ea
kniz <kniz.official@gmail.com>
2022-05-25 08:42:45 +0900

    indep: fix: console fore and back color can't be applied to

--- mod/indep/helper/platformAPI.cpp
+++ mod/indep/helper/platformAPI.cpp
@@ -27,28 +27,12 @@ namespace wrd {
                 return use.find(e) != string::npos;
             }) != samples.end();
         }
-
-        void _printColorCharacter(me::consoleColor fore, me::consoleColor back) {
-            static vector<string> fores = {
-                "\x1B[0;30m", "\x1B[0;34m", "\x1B[0;32m", "\x1B[0;36m", // black, blue, green, cyan
-                "\x1B[0;31m", "\x1B[0;35m", "\x1B[0;33m", "\x1B[0;37m", // red, purple, yellow, white
-                "\x1B[1;30m", "\x1B[1;34m", "\x1B[1;32m", "\x1B[1;36m", // same ones but more lighter than above.
-                "\x1B[1;31m", "\x1B[1;35m", "\x1B[1;33m", "\x1B[1;37m"
-            };
-            static vector<string> backs = {
-                "\x1B[0;40m", "\x1B[0;44m", "\x1B[0;42m", "\x1B[0;46m", // black, blue, green, cyan
-                "\x1B[0;41m", "\x1B[0;45m", "\x1B[0;43m", "\x1B[0;47m", // red, purple, yellow, white
-                "\x1B[1;40m", "\x1B[1;44m", "\x1B[1;42m", "\x1B[1;46m", // black, blue, green, cyan
-                "\x1B[1;41m", "\x1B[1;45m", "\x1B[1;43m", "\x1B[1;47m", // red, purple, yellow, white
-            };
-            cout << fores[fore] << backs[back];
-        }
     }
 #endif
 
-    void me::updateConsoleColor(consoleColor fore, consoleColor back) {
+    void me::updateConsoleFore(consoleColor fore) {
 #if WRD_BUILD_PLATFORM == WRD_TYPE_WINDOWS
-        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), back << 4 | fore);
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), platformAPI::BLACK << 4 | fore);
 #elif WRD_BUILD_PLATFORM == WRD_TYPE_LINUX
         static bool is_terminal_supporting = _isAnsiColorTerminal();
         if( ! is_terminal_supporting) {
@@ -56,7 +40,35 @@ namespace wrd {
             cout << "ERROR!";
             return;
         }
-        _printColorCharacter(fore, back);
+
+        static vector<string> fores = {
+            "\x1B[0;30m", "\x1B[0;34m", "\x1B[0;32m", "\x1B[0;36m", // black, blue, green, cyan
+            "\x1B[0;31m", "\x1B[0;35m", "\x1B[0;33m", "\x1B[0;37m", // red, purple, yellow, white
+            "\x1B[1;30m", "\x1B[1;34m", "\x1B[1;32m", "\x1B[1;36m", // same ones but more lighter than above.
+            "\x1B[1;31m", "\x1B[1;35m", "\x1B[1;33m", "\x1B[1;37m"
+        };
+        cout << fores[fore];
+    }
+#endif
+
+    void me::updateConsoleBack(consoleColor back) {
+#if WRD_BUILD_PLATFORM == WRD_TYPE_WINDOWS
+        SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), back << 4 | WHITE);
+#elif WRD_BUILD_PLATFORM == WRD_TYPE_LINUX
+        static bool is_terminal_supporting = _isAnsiColorTerminal();
+        if( ! is_terminal_supporting) {
+            // TODO: may need to print some error.
+            cout << "ERROR!";
+            return;
+        }
+
+        static vector<string> backs = {
+            "\x1B[0;40m", "\x1B[0;44m", "\x1B[0;42m", "\x1B[0;46m", // black, blue, green, cyan
+            "\x1B[0;41m", "\x1B[0;45m", "\x1B[0;43m", "\x1B[0;47m", // red, purple, yellow, white
+            "\x1B[1;40m", "\x1B[1;44m", "\x1B[1;42m", "\x1B[1;46m", // black, blue, green, cyan
+            "\x1B[1;41m", "\x1B[1;45m", "\x1B[1;43m", "\x1B[1;47m", // red, purple, yellow, white
+        };
+        cout << backs[back];
 #endif
     }
 
