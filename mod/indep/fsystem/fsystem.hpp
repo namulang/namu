c384d56122725f52ead3a42164742a6ac4f98322
kniz <kniz.private@gmail.com>
2022-05-28 08:26:19 +0900

    fsystem: style: move prototype def into implement file

--- mod/indep/fsystem/fsystem.hpp
+++ mod/indep/fsystem/fsystem.hpp
@@ -35,92 +35,26 @@ namespace wrd {
             friend class fsystem;
 
         private:
-            iterator(const std::string& path) {
-                _addDir(path);
-            }
+            iterator(const std::string& path);
+            ~iterator();
 
         public:
-            ~iterator() {
-                rel();
-            }
+            const std::string& operator*();
+            me& operator++(int);
+            operator wbool() const;
 
         public:
-            const std::string& operator*() {
-                return get();
-            }
-
-            me& operator++(int) {
-                next();
-                return *this;
-            }
-
-            operator wbool() const {
-                return !isEnd();
-            }
-
-        public:
-            void rel() {
-                while(!isEnd())
-                    _popDir();
-            }
-
+            void rel();
             wbool next();
-
             const std::string& get() const;
-
-            std::string getName() const {
-                const std::string& path = get();
-                widx slash = path.rfind('/');
-                return path.substr(slash + 1);
-            }
-
-            std::string getDir() const {
-                const std::string& path = get();
-                widx slash = path.rfind('/');
-                return path.substr(0, slash);
-            }
-
-            wbool isEnd() const {
-                return _entries.size() == 0;
-            }
+            std::string getName() const;
+            std::string getDir() const;
+            wbool isEnd() const;
 
         private:
-            void _addDir(const std::string& dirPath) {
-                if(dirPath.empty()) return;
-                std::string path = _filterPath(dirPath);
-#ifdef WRD_BUILD_PLATFORM_IS_WINDOWS
-                _finddata_t file;
-                intptr_t newDir = _findfirst((path + "\\*.*").c_str(), &file);
-                if (newDir == -1) return;
-                _entries.push_back(entry {file, newDir, path});
-#else
-                DIR* newDir = opendir(path.c_str());
-                if(!newDir) return;
-                _entries.push_back(entry {newDir, path});
-#endif
-            }
-            void _popDir() {
-                entry& e = _entries[_entries.size()-1];
-#ifdef WRD_BUILD_PLATFORM_IS_WINDOWS
-                _findclose(e.dir);
-#else
-                closedir(e.dir);
-#endif
-                _entries.pop_back();
-            }
-
-            std::string _filterPath(const std::string& org) {
-                int idx = org.length() - 1;
-                char last = org[idx];
-#ifdef WRD_BUILD_PLATFORM_IS_WINDOWS
-                if (last == '\\' || last == '/')
-#else
-                if (last == '/')
-#endif
-                    return org.substr(0, idx);
-
-                return org;
-            }
+            void _addDir(const std::string& dirPath);
+            void _popDir();
+            std::string _filterPath(const std::string& org);
 
         private:
             entries _entries;
@@ -128,14 +62,7 @@ namespace wrd {
         };
 
     public:
-        static iterator find(const std::string& path) {
-            return iterator(path);
-        }
-
-    public:
-        static std::string getCurrentDir() {
-            char buf[256] = {};
-            return getcwd(buf, 256);
-        }
+        static iterator find(const std::string& path);
+        static std::string getCurrentDir();
     };
 }
