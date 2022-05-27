8dc03e0e075cfb1f2dc1c3d8bf32dd5078924a17
kniz <kniz.official@gmail.com>
2022-05-28 08:26:19 +0900

    fsystem: style: move prototype def into implement file

--- mod/indep/fsystem/fsystem.cpp
+++ mod/indep/fsystem/fsystem.cpp
@@ -50,4 +50,93 @@ namespace wrd {
         _nowPath = "";
         return false;
     }
+
+    me::iterator::iterator(const std::string& path) {
+        _addDir(path);
+    }
+
+    me::iterator::~iterator() {
+        rel();
+    }
+
+    const std::string& me::iterator::operator*() {
+        return get();
+    }
+
+    me::iterator& me::iterator::operator++(int) {
+        next();
+        return *this;
+    }
+
+    me::iterator::operator wbool() const {
+        return !isEnd();
+    }
+
+    void me::iterator::rel() {
+        while(!isEnd())
+            _popDir();
+    }
+
+    std::string me::iterator::getName() const {
+        const std::string& path = get();
+        widx slash = path.rfind('/');
+        return path.substr(slash + 1);
+    }
+
+    std::string me::iterator::getDir() const {
+        const std::string& path = get();
+        widx slash = path.rfind('/');
+        return path.substr(0, slash);
+    }
+
+    wbool me::iterator::isEnd() const {
+        return _entries.size() == 0;
+    }
+
+    void me::iterator::_addDir(const std::string& dirPath) {
+        if(dirPath.empty()) return;
+        std::string path = _filterPath(dirPath);
+#ifdef WRD_BUILD_PLATFORM_IS_WINDOWS
+        _finddata_t file;
+        intptr_t newDir = _findfirst((path + "\\*.*").c_str(), &file);
+        if (newDir == -1) return;
+        _entries.push_back(entry {file, newDir, path});
+#else
+        DIR* newDir = opendir(path.c_str());
+        if(!newDir) return;
+        _entries.push_back(entry {newDir, path});
+#endif
+    }
+
+    void me::iterator::_popDir() {
+        entry& e = _entries[_entries.size()-1];
+#ifdef WRD_BUILD_PLATFORM_IS_WINDOWS
+        _findclose(e.dir);
+#else
+        closedir(e.dir);
+#endif
+        _entries.pop_back();
+    }
+
+    std::string me::iterator::_filterPath(const std::string& org) {
+        int idx = org.length() - 1;
+        char last = org[idx];
+#ifdef WRD_BUILD_PLATFORM_IS_WINDOWS
+        if (last == '\\' || last == '/')
+#else
+        if (last == '/')
+#endif
+            return org.substr(0, idx);
+
+        return org;
+    }
+
+    me::iterator me::find(const std::string& path) {
+        return iterator(path);
+    }
+
+    std::string me::getCurrentDir() {
+        char buf[256] = {};
+        return getcwd(buf, 256);
+    }
 }
