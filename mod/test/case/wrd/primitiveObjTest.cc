8eb355797ecd7b0170bc23721bdc1c7c4e56cdc5
kniz <kniz.official@gmail.com>
2022-06-08 22:38:27 +0900

    wrd: test: TC modification to apply new subs() of primitives

--- mod/test/case/wrd/primitiveObjTest.cc
+++ mod/test/case/wrd/primitiveObjTest.cc
@@ -8,7 +8,7 @@ struct primitiveObjTest : public wrdTest {};
 TEST_F(primitiveObjTest, testCreateWIntInMgd) {
     wInt origin(1);
 
-    tstr<wInt> inst = origin.run(narr({origin}));
+    tstr<wInt> inst = origin.run(obj::CTOR_NAME, narr(origin));
     ASSERT_TRUE(inst);
     ASSERT_EQ(origin.get(), inst->get());
     ASSERT_NE(&origin, &(*inst));
@@ -26,19 +26,19 @@ TEST_F(primitiveObjTest, testCloneWIntInMgd) {
 TEST_F(primitiveObjTest, testDefaultCtor) {
     wInt origin(1);
 
-    tstr<wInt> inst = origin.run(narr());
+    tstr<wInt> inst = origin.run(obj::CTOR_NAME, narr());
     ASSERT_TRUE(inst);
     ASSERT_EQ(inst->get(), 0);
 }
 
-TEST_F(primitiveObjTest, subsIsDummy) {
+TEST_F(primitiveObjTest, subsIsNotDummy) {
     wInt val(2);
-    ASSERT_EQ(val.subs().len(), 0);
+    int initial = val.subs().len();
 
     bicontainable& subs = val.subs();
     ASSERT_FALSE(nul(subs));
     subs.add("not work", new wInt(3));
-    ASSERT_EQ(subs.len(), 0);
-    ASSERT_FALSE(subs.has("not work"));
-    ASSERT_TRUE(subs.begin().isEnd());
+    ASSERT_EQ(subs.len(), initial+1);
+    ASSERT_TRUE(subs.has("not work"));
+    ASSERT_FALSE(subs.begin().isEnd());
 }
