c70cbe1e5aed4f9ac180694d02014e45fd4b3c4e
kniz <kniz.official@gmail.com>
2022-09-02 21:03:23 +0900

    core: feat: add more classes for visitor to support

--- mod/core/ast/visitor/visitor.hpp
+++ mod/core/ast/visitor/visitor.hpp
@@ -24,12 +24,15 @@ namespace namu {
         //      generalized-way:
         virtual void onTraverse(const std::string& name, node& t);
         //      specific-way:
+        virtual void onTraverse(const std::string& name, asExpr& a);
+        virtual void onTraverse(const std::string& name, assignExpr& a);
+        virtual void onTraverse(const std::string& name, blockExpr& b);
+        virtual void onTraverse(const std::string& name, defAssignExpr& d);
+        virtual void onTraverse(const std::string& name, FAOExpr& f);
         virtual void onTraverse(const std::string& name, getExpr& e);
-        virtual void onTraverse(const std::string& name, frame& f);
+        virtual void onTraverse(const std::string& name, returnExpr& b);
         virtual void onTraverse(const std::string& name, runExpr& e);
         virtual void onTraverse(const std::string& name, mgdFunc& f);
-        virtual void onTraverse(const std::string& name, blockExpr& b);
-        virtual void onTraverse(const std::string& name, returnExpr& b);
-        virtual void onTraverse(const std::string& name, asExpr& a);
+        virtual void onTraverse(const std::string& name, frame& f);
     };
 }
