aae2690d001a03873d9a37f5fa835b33963bfb07
kniz <kniz.private@gmail.com>
2022-09-15 23:33:45 +0900

    core: feat: generalize getExpr & defVarExpr

--- mod/test/case/core/syntax/genericsTest.cc
+++ mod/test/case/core/syntax/genericsTest.cc
@@ -10,11 +10,53 @@ namespace {
 TEST_F(genericsTest, simpleDefineGenerics) {
     make().parse(R"SRC(
         def object<T>
+            foo() str
+                sys.con.print(1 as T)
+
+        main() str
+            a := object<str>()
+            return a.foo()
+    )SRC").shouldVerified(true);
+    ASSERT_EQ(run().cast<std::string>(), "1");
+}
+
+TEST_F(genericsTest, defineGenerics) {
+    make().parse(R"SRC(
+        def object<e>
+            age e
+            foo() str
+                return age + 1 as str
+
+        main() str
+            a := object<int>()
+            return a.foo()
+    )SRC").shouldVerified(true);
+    str ret = run();
+    ASSERT_EQ(ret.cast<std::string>(), "01");
+}
+
+TEST_F(genericsTest, defineGenerics1) {
+    make().parse(R"SRC(
+        def object<T>
+            age T
+            foo(a T) T
+                return a + age
+
+        main() void
+            a := object<int>()
+            sys.con.print(a.foo(2) as str)
+    )SRC").shouldVerified(true);
+}
+
+TEST_F(genericsTest, makeGenericTwice) { // need deepClone()
+    make().parse(R"SRC(
+        def object<T>
             foo() void
                 sys.con.print(1 as T)
 
         main() void
             a := object<str>()
+            b := object<flt>() // run 'b.foo()' occurs F/C
             a.foo()
     )SRC").shouldVerified(true);
 }
