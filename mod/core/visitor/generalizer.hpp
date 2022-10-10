1424b66915f81634b21cfa1043a636643fdff634
kniz <kniz.official@gmail.com>
2022-10-10 12:06:32 +0900

    core: fix: FAOExpr not processed to be generic

--- mod/core/visitor/generalizer.hpp
+++ mod/core/visitor/generalizer.hpp
@@ -21,6 +21,7 @@ namespace namu {
         void onVisit(visitInfo i, runExpr& me) override;
         void onVisit(visitInfo i, func& me) override;
         void onVisit(visitInfo i, params& me);
+        void onVisit(visitInfo i, FAOExpr& me) override;
         void onVisit(visitInfo i, baseObj& me) override;
 
     private:
