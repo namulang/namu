270738f5db5eeda222314c27e710d62ffe83f61c
kniz <kniz.private@gmail.com>
2022-11-04 18:36:23 +0900

    core: feat: supports seq as a container of 'for' expr

--- mod/core/builtin/container/mgd/seq.cpp
+++ mod/core/builtin/container/mgd/seq.cpp
@@ -10,6 +10,78 @@ namespace namu {
     me::seq(nint start, nint end): super(new nseq(start, end)) {}
     me::seq(nint start, nint end, nint step): super(new nseq(start, end, step)) {}
 
+    namespace {
+        typedef tucontainable<nint>::iter niter;
+        typedef tcppBridge<niter> __superMgdIter;
+        class _nout mgdIter : public __superMgdIter {
+            NAMU(CLASS(mgdIter, __superMgdIter))
+
+        public:
+            mgdIter(niter* real): super(real) {}
+
+        public:
+            using super::subs;
+            nbicontainer& subs() override {
+                static super* inner = nullptr;
+                if(nul(inner)) {
+                    inner = new super();
+                    inner->func("isEnd", &niter::isEnd);
+                    inner->func("next", &niter::next);
+                    inner->func<nint&>("get", &niter::get);
+                }
+
+                return inner->subs();
+            }
+        };
+
+        class iterateFunc : public func {
+            NAMU(CLASS(iterateFunc, func))
+
+        public:
+            const node& getRet() const override {
+                static mgdIter inner(nullptr);
+                return inner;
+            }
+
+            const params& getParams() const override {
+                static params inner;
+                if(inner.len() <= 0)
+                    inner.add(new param("step", *new nInt()));
+
+                return inner;
+            }
+
+            str run(const args& a) override {
+                const params& ps = getParams();
+                if(a.len() != ps.len()) return NAMU_W("a.len(%d) != ps.len(%d)", a.len(), ps.len()), str();
+                seq& meObj = a.getMe().cast<seq>();
+                if(nul(meObj)) return NAMU_E("meObj as arr == null"), str();
+
+                str eval = a[0].as(ps[0].getOrigin().as<node>());
+                if(!eval)
+                    return NAMU_E("evaluation of arg[%s] -> param[%s] has been failed", a[0].getType().getName().c_str(), ps[0].getType().getName().c_str()), str();
+
+                nint step = eval->cast<nint>();
+                static tucontainable<nint>::iter (tucontainable<nint>::*specifier)(ncnt) const = &seq::iterate;
+                return new mgdIter(new niter((meObj.get().*specifier)(step)));
+            }
+        };
+
+        class getElemTypeFunc : public func {
+            NAMU(CLASS(getElemTypeFunc, func))
+
+        public:
+            const node& getRet() const override {
+                static nInt inner;
+                return inner;
+            }
+
+            str run(const args& a) override {
+                return getRet();
+            }
+        };
+    }
+
     nbicontainer& me::subs() {
         static super* inner;
         if(nul(inner)) {
@@ -17,6 +89,8 @@ namespace namu {
             inner->func("len", &nseq::len);
             inner->func("get", &nseq::get);
             inner->func("has", &nseq::has);
+            inner->subs().add("iterate", new iterateFunc());
+            inner->subs().add("getElemType", new getElemTypeFunc());
         }
 
         return inner->subs();
