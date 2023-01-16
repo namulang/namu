35aa3a3b7ad6a4f3449aad8896e2d7f7dcb0be0e
kniz <kniz.private@gmail.com>
2023-01-17 00:21:23 +0900

    core: refactor: remove duplication to arithmetic

--- mod/core/builtin/primitive/arithmeticObj.cpp
+++ mod/core/builtin/primitive/arithmeticObj.cpp
@@ -5,31 +5,15 @@ namespace namu {
     NAMU_DEF_ME(arithmeticObj)
 
 #define _X(FUNC) \
+    tstr<arithmeticObj> me::FUNC(const arithmeticObj& rhs) { \
         const ntype& deduced = getType().deduce(rhs); \
         nbool normalOrder = getType() == deduced; \
         const arithmeticObj& winner = getType() == deduced ? *this : rhs; \
         const arithmeticObj& loser = getType() == deduced ? rhs : *this; \
+        return winner._ ## FUNC(loser, !normalOrder); \
+    }
 
-    tstr<arithmeticObj> me::add(const arithmeticObj& rhs) {
-        _X(_add)
-        return winner._add(loser, !normalOrder);
-    }
-    tstr<arithmeticObj> me::sub(const arithmeticObj& rhs) {
-        _X(_sub)
-        return winner._sub(loser, !normalOrder);
-    }
-    tstr<arithmeticObj> me::mul(const arithmeticObj& rhs) {
-        _X(_mul)
-        return winner._mul(loser, !normalOrder);
-    }
-    tstr<arithmeticObj> me::div(const arithmeticObj& rhs) {
-        _X(_div)
-        return winner._div(loser, !normalOrder);
-    }
-    tstr<arithmeticObj> me::mod(const arithmeticObj& rhs) {
-        _X(_mod)
-        return winner._mod(loser, !normalOrder);
-    }
+    NAMU_EACH(_X, add, sub, mul, div, mod)
 
 #undef _X
 }
