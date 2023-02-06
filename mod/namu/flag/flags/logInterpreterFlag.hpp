15d1ffdb7902058e92bf68868829b97a7ae1c22c
kniz <kniz.private@gmail.com>
2023-02-06 13:02:04 +0900

    namu: style: fix some typo

--- mod/namu/flag/flags/logInterpreterFlag.hpp
+++ mod/namu/flag/flags/logInterpreterFlag.hpp
@@ -11,7 +11,7 @@ namespace namu {
         const nchar* getName() const override { return "-li"; }
         const nchar* getDescription() const override {
             return R"DESC(
-        prints all logs of the interpreter.  these are mostly about parser and verifier logs.
+        prints all logs of the interpreter. these are mostly about parser and verifier logs.
         it includes all of process to tokenizing, parsing, verification.
         this info will be useful if you can knew how src to be interpreted.)DESC";
         }
