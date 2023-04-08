a07863758898e8641542785f9cbd12ae41633f7c
kniz <kniz.private@gmail.com>
2023-04-08 13:14:41 +0900

    indep: fix: windows compatible for crtdbg.h
    * memlite module overrides operator new().
      because of indep's crtdbg.h, operator new got errors.

--- mod/indep/macro.hpp
+++ mod/indep/macro.hpp
@@ -2,7 +2,6 @@
 
 #include "common.hpp"
 #include "def.hpp"
-#include "macro/memLeak.hpp" // 메모리누수 검출 매크로.
 #include "macro/argCounter.hpp"
 #include "macro/argGetter.hpp"
 #include "macro/namuMeta.hpp"
