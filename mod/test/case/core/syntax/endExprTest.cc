51238434926d6f597bf01490259acd8784ad8e11
kniz <kniz.official@gmail.com>
2024-09-09 20:40:44 +0900

    core: test: add more TC for 'end'

--- mod/test/case/core/syntax/endExprTest.cc
+++ mod/test/case/core/syntax/endExprTest.cc
@@ -8,12 +8,53 @@ namespace {
 }
 
 TEST_F(endExprTest, simpleEnd) {
-    make().parse(R"SRC(
+    make()
+        .parse(R"SRC(
         age := 33
         foo() int
             end: age++
             ret 5
         main() int: foo() + age
+    )SRC")
+        .shouldVerified(true);
+
+    str res = run();
+    ASSERT_TRUE(res);
+    ASSERT_EQ(res.cast<nint>(), 39);
+}
+
+TEST_F(endExprTest, simpleEndNegative) {
+    make()
+        .negative()
+        .parse(R"SRC(
+        def obj
+            age := 22
+            end
+                age++
+        main() int
+            obj.age
+    )SRC")
+        .shouldParsed(false);
+    shouldVerified(false);
+}
+
+TEST_F(endExprTest, endUsesCapture) {
+    make().parse(R"SRC(
+        def person
+            age := 0
+            foo() void
+                end: me.age++
+
+                age := 33
+                end: me.age += age
+
+                if true
+                    age := 5
+                    end: me.age += age
+
+        main() int
+            person.foo()
+            person.age
     )SRC").shouldVerified(true);
 
     str res = run();
