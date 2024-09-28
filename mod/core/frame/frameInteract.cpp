05be574a4485fd94584a3ec417d2c3ad7164d14e
kniz <kniz.official@gmail.com>
2024-09-28 22:12:31 +0900

    core: fix: prevent app crash when interacting obj is null

--- mod/core/frame/frameInteract.cpp
+++ mod/core/frame/frameInteract.cpp
@@ -5,11 +5,11 @@ namespace nm {
     NM(DEF_ME(frameInteract))
 
     me::frameInteract(const frameInteractable& it, const bicontainable& args): _it(it), _args(args) {
-        _it.inFrame(_args);
+        if(!nul(it)) _it.inFrame(_args);
     }
 
     me::frameInteract(const frameInteractable& it): _it(it), _args(nulOf<bicontainable>()) {
-        _it.inFrame();
+        if(!nul(it)) _it.inFrame();
     }
 
     me::~frameInteract() { _it.outFrame(_args); }
