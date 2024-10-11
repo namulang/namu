c3539938e273c38a793aab571817789a0cda7d83
kniz <kniz.official@gmail.com>
2024-10-12 01:10:38 +0900

    core: fix: @expand funct should not be located in 'owns'

--- mod/core/worker/parser.cpp
+++ mod/core/worker/parser.cpp
@@ -624,9 +624,9 @@ namespace nm {
 
         // add postpones & common:
         //  if there is no postpones, add() will just return false.
-        auto& subs = it.subs();
-        subs.add(baseObj::EXPAND_NAME, _maker.makeExpandFunc(blk));
-        subs.add(baseObj::COMMON_NAME, _maker.makeCommonFunc(blk));
+        auto& shares = it.getShares();
+        shares.add(baseObj::EXPAND_NAME, _maker.makeExpandFunc(blk));
+        shares.add(baseObj::COMMON_NAME, _maker.makeCommonFunc(blk));
         return true;
     }
 
