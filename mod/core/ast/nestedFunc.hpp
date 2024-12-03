e68669c525510686e2d8fe9fac6a791a065003a3
kniz <kniz.official@gmail.com>
2024-12-03 22:07:20 +0900

    core: fix: nestedFunc ignores inherited mdgType

--- mod/core/ast/nestedFunc.hpp
+++ mod/core/ast/nestedFunc.hpp
@@ -10,7 +10,7 @@ namespace nm {
     ///     it doesn't know about local variables after you define it.
     ///     it contains copied block scope. and will inject just before runs block stmt.
     class _nout nestedFunc: public func {
-        NM(CLASS(nestedFunc, func))
+        NM(ME(nestedFunc, func), CLONE(nestedFunc))
 
     public:
         explicit nestedFunc(const func& org, const scope& subs);
