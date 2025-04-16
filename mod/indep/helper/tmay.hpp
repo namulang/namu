18d23228840aea665d85d1f69d69c186cf064312
kniz <kniz.official@gmail.com>
2025-04-16 18:28:07 +0900

    indep: feat: add `may` class

---
+++ mod/indep/helper/tmay.hpp
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "../common.hpp"
+#include "../macro.hpp"
+#include <optional>
+
+namespace nm {
+
+    /// this is a wrapper class for std::optional.
+    /// improved `optional`'s func name and signature because they are somewhat inconsistent
+    /// from the perspective of namu and convention rules.
+    template <typename T> class tmay {
+        NM(ME(tmay))
+
+    public:
+        tmay() = default;
+
+        explicit tmay(const T& value): _subj(value) {}
+
+    public:
+        T* operator->() { return &get(); }
+
+        const T* operator->() const NM_CONST_FUNC(operator->());
+
+        T& operator*() { return get(); }
+
+        const T& operator*() const NM_CONST_FUNC(operator*());
+
+        operator nbool() const { return has(); }
+
+    public:
+        nbool has() const { return _subj.has_value(); }
+
+        T& get() {
+            WHEN(!has()).retNul<T>();
+            return _subj.value();
+        }
+
+        void rel() { _subj.reset(); }
+
+        template <typename... Ts> void set(Ts... args) { _subj.emplace(args...); }
+
+    private:
+        std::optional<T> _subj;
+    };
+} // namespace nm
